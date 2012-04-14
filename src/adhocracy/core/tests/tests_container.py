78d42a78eac7c48590d6bf119926ebdda74bed9f
Nicolas Dietrich <nicodietrich@gmail.com>
2012-04-14 22:54:08 +0200

    make child relation test more explicit to expose bug in build_path in bulbs/util.py

--- src/adhocracy/core/tests/tests_container.py
+++ src/adhocracy/core/tests/tests_container.py
@@ -10,7 +10,6 @@ class ModelTests(unittest.TestCase):
         self.config = testing.setUp(settings={'rexster_uri':"http://localhost:8182/graphs/testgraph"})
         self.config.include('pyramid_zcml')
         self.config.load_zcml('adhocracy.core.models:utilities.zcml')
-        #TODO create test database
 
     def tearDown(self):
         registry = get_current_registry()
@@ -27,7 +26,7 @@ class ModelTests(unittest.TestCase):
         self.assert_(verifyObject(IContainer, container0))
         self.assert_(container0.name == "g0")
 
-    def create_child(self):
+    def create_two_nodes(self):
         #create a container
         from adhocracy.core.models.interfaces import IContainer
         from repoze.lemonade.content import create_content
@@ -39,21 +38,30 @@ class ModelTests(unittest.TestCase):
         container2 = create_content(IContainer, name=u"g2")
         container2.text = u"text2"
         container2.save()
-        container1["g2"] = container2
         return (container1, container2)
 
+    def test_child_factory(self):
+        container1, container2 = self.create_two_nodes()
+        from repoze.lemonade.content import create_content
+        from adhocracy.core.models.interfaces import IChild
+        child = create_content(IChild, parent=container1, child=container2, child_name=u"g2")
+        self.assert_(child.outV().eid == container2.eid)
+        self.assert_(child.inV().eid == container1.eid)
+
     def test_set_item(self):
-        container1, container2 = self.create_child()
+        container1, container2 = self.create_two_nodes()
+        container1["g2"] = container2
         self.assert_(container1.has_key("g2"))
         self.assert_(container2.eid == container1["g2"].eid)
         self.assert_(not container1.has_key("not_available"))
         self.assertRaises(KeyError, container1.__getitem__, ("not_available"))
         #it can be a children
         self.assert_(container2.__parent__ is not None)
-        self.assert_(container2.__parent__ == container1)
+        self.assert_(container2.__parent__.eid == container1.eid)
 
     def test_del_item(self):
-        container1, container2 = self.create_child()
+        container1, container2 = self.create_two_nodes()
+        container1["g2"] = container2
         del container1["g2"]
         self.assert_(not container1.has_key("g2"))
         self.assertRaises(KeyError, container1.__delitem__, ("g2"))
