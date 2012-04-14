f15c4f5b29ad5b5fe2c3363606bb0c3190e73760
Joscha Krutzki <joka@jokasis.de>
2012-04-14 10:40:09 +0200

    updated doc

--- src/adhocracy/core/__init__.py
+++ src/adhocracy/core/__init__.py
@@ -1,6 +1,5 @@
 from pyramid.config import Configurator
 
-from adhocracy.core.models import utilities
 from adhocracy.core.models.adhocracyroot import appmaker
 from adhocracy.core.models.interfaces import IGraphConnection
 
@@ -15,19 +14,19 @@ def root_factory(request):
 def main(global_config, **settings):
     """ This function returns a WSGI application.
     """
-    #load application settings
+    #load application settings (rootfactory == try to use object traversal)
     config = Configurator(root_factory=root_factory, settings=settings)
     #hook zope global component registry into the pyramid application registry
     #http://docs.pylonsproject.org/projects/pyramid/en/1.3-branch/narr/zca.html?highlight=utility#using-the-zca-global-registry
     config.hook_zca()
     #add views (TODO: use zcml or imperative?)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    #scan modules for configurations decorators
+    #scan modules for configuration decorators
     config.scan()
     #load zcml configuration files
     #http://readthedocs.org/docs/pyramid_zcml/en/latest/narr.html
-    config.include('pyramid_zcml')
-    config.load_zcml('configure.zcml')
+    config.include('pyramid_zcml') #imperative configuration: run pyramid_zcml.includeme()
+    config.load_zcml('configure.zcml') #declerative configuration: load central zcml file
 
     return config.make_wsgi_app()
 
