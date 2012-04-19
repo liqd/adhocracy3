d2c70c74514f87eec573d52c6493efe69bf521a4
Soenke Hahn <shahn@cs.tu-berlin.de>
2012-04-19 15:22:05 +0200

    testsuite: added doctest for utils.to_list

--- src/adhocracy/core/tests/test.rst
+++ src/adhocracy/core/tests/test.rst
@@ -4,7 +4,6 @@ Doctest test
 we test is test::
 
     >>> a = 1
-    >>> import ipdb; ipdb.set_trace()
     >>> a
     1
 
--- src/adhocracy/core/utils.py
+++ src/adhocracy/core/utils.py
@@ -1,6 +1,12 @@
 
 # collection of general convenience functions
 
-# Converts an iterable to a list.
 def to_list(iterable):
+    """
+    Converts an iterable to a list::
+
+    >>> from adhocracy.core.utils import to_list
+    >>> to_list((1, 2, 3))
+    [1, 2, 3]
+    """
     return [x for x in iterable]
