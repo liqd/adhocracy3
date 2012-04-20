e5bbbc11924d9e547c8b6ac73d6f6ff466a8a227
Joscha Krutzki <joka@jokasis.de>
2012-04-20 19:13:26 +0200

    interlude ipdb sessions for doctests

--- src/adhocracy/core/tests/tests_doctest_files.py
+++ src/adhocracy/core/tests/tests_doctest_files.py
@@ -4,6 +4,15 @@ import unittest
 from adhocracy.core.testing import ADHOCRACY_LAYER_FUNCTIONAL
 from adhocracy.core.testing import Browser
 
+try:
+    import interlude
+    """To use ipython shell in doctests: >>> interact( locals() )
+        mind bug: https://github.com/collective/interlude/issues/2
+    """
+    interact = interlude.interact
+except ImportError:
+    interact = None
+
 
 flags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
 
@@ -13,6 +22,9 @@ def globs():
               "app"     : app,
               "app_url" : "http://localhost",
              }
+    if interact:
+        globs["interact"] = interact
+
     return globs
 
 
