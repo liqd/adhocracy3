1ca076286626ce6eb8a96fe3a466051286837e97
Joscha Krutzki <joka@jokasis.de>
2012-04-20 10:29:38 +0200

    use only zcml declaration or python decorator configuration

--- src/adhocracy/core/__init__.py
+++ src/adhocracy/core/__init__.py
@@ -12,18 +12,15 @@ def root_factory(request):
 def main(global_config, **settings):
     """ This function returns a WSGI application.
     """
-    #load application settings (rootfactory == try to use object traversal)
+    # load application settings (rootfactory == try to use object traversal)
     config = Configurator(root_factory=root_factory, settings=settings)
-    #hook zope global component registry into the pyramid application registry
-    #http://docs.pylonsproject.org/projects/pyramid/en/1.3-branch/narr/zca.html?highlight=utility#using-the-zca-global-registry
+    # hook zope global component registry into the pyramid application registry
+    # http://docs.pylonsproject.org/projects/pyramid/en/1.3-branch/narr/zca.html?highlight=utility#using-the-zca-global-registry
     #config.hook_zca()
-    #add views (TODO: use zcml or imperative?)
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    #scan modules for configuration decorators
-    config.scan()
-    #load zcml configuration files
-    #http://readthedocs.org/docs/pyramid_zcml/en/latest/narr.html
+    # load zcml configuration files
+    # http://readthedocs.org/docs/pyramid_zcml/en/latest/narr.html
+    zcml_config_file = settings.get('configure_zcml', 'configure.zcml')
     config.include('pyramid_zcml') #imperative configuration: run pyramid_zcml.includeme()
-    config.load_zcml('configure.zcml') #declerative configuration: load central zcml file
+    config.load_zcml(zcml_config_file) #declerative configuration: load central zcml file
 
     return config.make_wsgi_app()
