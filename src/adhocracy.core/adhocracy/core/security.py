86fb5b06cffbd9057312c7e06e800494f00fd9ea
Joscha Krutzki <joka@jokasis.de>
2013-08-21 16:50:09 +0200

    remove all old files

--- src/adhocracy.core/adhocracy/core/security.py
+++
@@ -1,5 +0,0 @@
-SITE_ACL = [[u'Allow', u'system.Everyone', [u'view']],
-            [u'Allow', u'role:viewer', [u'view']],
-            [u'Allow', u'role:editor', [u'view', u'add', u'edit']],
-            [u'Allow', u'role:owner', [u'view', u'add', u'edit', u'manage']],
-           ]
