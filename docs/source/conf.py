a10571b24f6cdd2399fe37dcd1e6ded0e93f3bff
Joscha Krutzki <joka@jokasis.de>
2013-08-22 00:36:14 +0200

    build html documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,8 @@ import sys, os
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
               'sphinx.ext.intersphinx', 'sphinx.ext.coverage',
-              'sphinx.ext.viewcode', 'sphinxcontrib.blockdiag',
-              'repoze.sphinx.autointerface','sphinx.ext.pngmath',
-              'sphinx.ext.graphviz']
+              'sphinx.ext.viewcode', 'repoze.sphinx.autointerface',
+              'sphinx.ext.pngmath', 'sphinx.ext.graphviz']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
