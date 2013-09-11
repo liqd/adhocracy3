7f0f617d435bafc7378c5d574319bc87b357e99d
Joscha Krutzki <joka@jokasis.de>
2013-09-11 16:31:28 +0200

    move rest_api doctest to the adhocarcy. This allows to use py.test fixtures

---
+++ src/adhocracy/adhocracy/testing.py
@@ -0,0 +1,14 @@
+"""Helper to run tests"""
+
+
+config = {"pyramid.includes": ["pyramid_zodbconn", "pyramid_tm"],
+          "zodbconn.uri": "memory://",
+          "substanced.secret": "seekri1",
+          "substanced.initial_login": "admin",
+          "substanced.initial_password": "admin",
+          "substanced.initial_email": "admin@example.com",
+          "substanced.autosync_catalogs": "true",
+          "substanced.statsd.enabled": "false ",
+    }
+
+
