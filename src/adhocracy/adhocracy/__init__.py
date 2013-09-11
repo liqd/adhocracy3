7da0f5c8dcbee4be99a973978af41f1da7b3e56b
Joscha Krutzki <joka@jokasis.de>
2013-09-11 22:27:33 +0200

    Auto run migration scripts (evolve) on start up

--- src/adhocracy/adhocracy/__init__.py
+++ src/adhocracy/adhocracy/__init__.py
@@ -1,6 +1,28 @@
+import transaction
+from pyramid_zodbconn import get_connection
 from pyramid.config import Configurator
 from substanced import root_factory
+from substanced.stats import statsd_incr
 
+
+def root_factory(request, t=transaction, g=get_connection,
+                 mark_unfinished_as_finished=False):
+    """ A function which can be used as a Pyramid ``root_factory``.  It
+    accepts a request and returns an instance of the ``Root`` content type."""
+    # accepts "t", "g", and "mark_unfinished_as_finished" for unit testing
+    # purposes only
+    conn = g(request)
+    zodb_root = conn.root()
+    if not 'app_root' in zodb_root:
+        registry = request.registry
+        app_root = registry.content.create('Root')
+        zodb_root['app_root'] = app_root
+        t.savepoint() # give app_root a _p_jar
+        if mark_unfinished_as_finished:
+            mark_unfinished_as_finished(app_root, registry, t)
+        t.commit()
+    statsd_incr('root_factory', rate=.1)
+    return zodb_root['app_root']
 
 
 def main(global_config, **settings):
