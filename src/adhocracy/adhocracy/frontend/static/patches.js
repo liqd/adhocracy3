009c7856a13d8defdf104af8e579adbd6fc70201
Matthias Fischmann <mf@zerobuzz.net>
2013-09-12 15:22:02 +0200

    patches.js: d3js version history chart (dysfunctional)

--- src/adhocracy/adhocracy/frontend/static/patches.js
+++ src/adhocracy/adhocracy/frontend/static/patches.js
@@ -1,6 +1,7 @@
 (function($, obviel) {
 
-    // set up static, local proposal db
+    // set up static, local proposal db (this is only interesting to
+    // get a clean, self-contained test environment)
 
     var cache = {
           ahezwipfjfcj: { title: 'title', follows: null,           text: 'ahezwipfjfcj' },
@@ -65,7 +66,7 @@
     initCache(cache);
 
 
-    // views
+    // obviel views
 
     obviel.view({
         iface: 'proposal',
@@ -127,6 +128,9 @@
 
             // show new version
             refresh(new_obj.version);
+
+
+            // XXX: make title editable.
         }
     });
 
@@ -139,10 +143,91 @@
     });
 
 
-    // refresh page
+    // d3js history
+
+    var versionChart = (function() {
+        // cloned from http://bl.ocks.org/mbostock/4062045
+
+	var width = 960;
+	var height = 500;
+        var color = d3.scale.category20();
+
+        var force = d3.layout.force()
+            .charge(-120)
+            .linkDistance(30)
+            .size([width, height]);
+
+        var svg;
+        var link;
+        var node;
+
+        force.on("tick", function() {
+          link.attr("x1", function(d) { return d.source.x; })
+              .attr("y1", function(d) { return d.source.y; })
+              .attr("x2", function(d) { return d.target.x; })
+              .attr("y2", function(d) { return d.target.y; });
+
+          node.attr("cx", function(d) { return d.x; })
+              .attr("cy", function(d) { return d.y; });
+        });
+
+        var init = function() {
+            svg = d3.select("#version_chart").append("svg")
+                .attr("width", width)
+                .attr("height", height);
+	};
+
+        var refresh = function() {
+            console.assert(svg !== undefined);
+
+            svg.selectAll(".node").data([]).exit().remove();
+            svg.selectAll(".link").data([]).exit().remove();
+
+            var nodes = Object.keys(cache);
+            var links = [];
+            nodes.forEach(function(n) {
+                console.assert(typeof cache[n] !== 'undefined', "n is undefined");
+                console.assert(typeof cache[n].follows !== 'undefined', "n.follows is undefined");
+
+                if (cache[n].follows !== null) {
+                    var source = nodes.indexOf(cache[cache[n].follows].version);
+                    var target = nodes.indexOf(n);
+                    links.push({ source: source, target: target });
+		}
+	    });
+
+            link = svg.selectAll(".link")
+                .data(links)
+                .enter().append("line")
+                .attr("class", "link")
+                .style("stroke-width", 3);
+
+            node = svg.selectAll(".node")
+                .data(nodes)
+                .enter().append("circle")
+                .attr("class", "node")
+                .attr("r", 5)
+                .style("fill", 4)
+                .call(force.drag);
+
+            node.append("title")
+                .text(function(d) { return d.name; });
+
+            force
+                .nodes(nodes)
+                .links(links)
+                .start();
+        };
+
+        return { init: init, refresh: refresh };
+    })();
+
+
+    // main
 
     var refresh = function(path) {
         $('#main').render(cache[path], cache[path].render_state);
+        versionChart.refresh(path);
     };
 
     $(window).bind('hashchange', function(ev) {
@@ -152,6 +237,7 @@
     });
 
     $(document).ready(function() {
+        versionChart.init();
         refresh(prop_initial_version);
     });
 
