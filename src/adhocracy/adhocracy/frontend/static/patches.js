622db3fd68b91f048fb741fc5fa0f81cf887497a
Matthias Fischmann <mf@zerobuzz.net>
2013-09-12 17:20:44 +0200

    patches.js: ...

--- src/adhocracy/adhocracy/frontend/static/patches.js
+++ src/adhocracy/adhocracy/frontend/static/patches.js
@@ -162,13 +162,13 @@
         var node;
 
         force.on("tick", function() {
-          link.attr("x1", function(d) { return d.source.x; })
-              .attr("y1", function(d) { return d.source.y; })
-              .attr("x2", function(d) { return d.target.x; })
-              .attr("y2", function(d) { return d.target.y; });
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
 
-          node.attr("cx", function(d) { return d.x; })
-              .attr("cy", function(d) { return d.y; });
+            node.attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
         });
 
         var init = function() {
