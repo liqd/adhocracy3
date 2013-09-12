db57f7bd367b66106f49e22ecb586b4d91bfe617
Matthias Fischmann <mf@zerobuzz.net>
2013-09-12 14:01:32 +0200

    patches.js: edit; commit new versions

--- src/adhocracy/adhocracy/frontend/static/patches.js
+++ src/adhocracy/adhocracy/frontend/static/patches.js
@@ -15,7 +15,11 @@
     // path to initial version.  this is displayed after page load.
     var prop_initial_version = 'ahezwipfjfcj';
 
-    var init_cache = function(cache) {
+    // call initCache every time new objects have been added to update
+    // internal the data structure.  in particular, call this function
+    // from createNewVersion.  this is not very efficient, but makes
+    // the source easier to read, which is the priority for now.
+    var initCache = function(cache) {
         // inject version strings and followed_by edges
 	Object.keys(cache).forEach(function(k) {
             cache[k].version = k;
@@ -37,9 +41,28 @@
 	Object.keys(cache).forEach(function(k) {
             cache[k].render_state = 'display';
         });
-    }
+    };
 
-    init_cache(cache);
+    var commitNewVersion = function(obj) {
+        var new_ver = createNewVersion();
+        var new_obj = { title: obj.title, follows: obj.version, text: obj.text };
+
+        cache[new_ver] = new_obj;
+        initCache(cache);
+
+        return new_obj;
+    };
+
+    var createNewVersion = function() {
+        var v = '';
+        for (i = 0; i < 12; i++) {
+            c = String.fromCharCode(Math.floor(Math.random() * 26) + 'a'.charCodeAt(0));
+            v = v + c.toString();
+	};
+        return v;
+    };
+
+    initCache(cache);
 
 
     // views
@@ -74,7 +97,37 @@
                // attr>' is equivalent to '<elem attr="">'.
 
                '<hr>' +
-               '[editbutton]'
+               '<button data-on="click|startEdit">edit</button>',
+
+        startEdit: function(ev) {
+            this.obj.render_state = 'edit';
+            refresh(this.obj.version);
+        }
+    });
+
+    obviel.view({
+        iface: 'proposal',
+        name:  'edit',
+        obvt:  '<h1>{title}</h1>' +
+               '<ul>' +
+               '<li>this version: <a href="#{version}"><tt>{version}</tt></a></li>' +
+               '<li>follows version: <a href="#{follows}"><tt>{follows}</tt></a></li>' +
+               '<li>followed_by versions:<span data-repeat="followed_by"><tt> <a href="#{@.}">{@.}</a></tt></span></li>' +
+               '</ul>' +
+               '<textarea id="proposal_text">{text}</textarea>' +
+               '<hr>' +
+               '<button data-on="click|commit">create new version</button>',
+
+        commit: function(ev) {
+            this.obj.render_state = 'display';
+            var new_obj = commitNewVersion(this.obj);
+
+            // update new object from dom
+            new_obj.text = document.getElementById("proposal_text").value;
+
+            // show new version
+            refresh(new_obj.version);
+        }
     });
 
 
