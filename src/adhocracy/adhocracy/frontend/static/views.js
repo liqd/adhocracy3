bea8c3f46f750088b7e128ae74f0a9f5d601bae7
Matthias Fischmann <mf@zerobuzz.net>
2013-09-16 22:34:28 +0200

    frontend: added notes from dinner discussion

--- src/adhocracy/adhocracy/frontend/static/views.js
+++ src/adhocracy/adhocracy/frontend/static/views.js
@@ -1,3 +1,40 @@
+/* notes from dinner discussion today (are these worth keeping?  where
+ * should i move them?  -mf):
+
+there should be a way to keep changes locally without posting them
+right away.  something like a button that toggles between "edit" and
+"display", and if there are local changes, the background colour
+changes to "not saved", and two further buttons "submit changes" and
+"discard changes" appear next to the "edit/display" button.  "submit"
+and "discard" buttons also appear on super-structures; if the user has
+edited a number of paragraphs, she can post a new version of the
+entire document with one click.
+
+there must be a way for the server to receive a batch of post requests
+that are processed in one database "transaction" (not to be confused
+with rest api transactions that need to be handled by client and
+server cooperatively and are a total mess).
+
+if the server receives a paragraph update (or several ones), it
+performs the update to the containing document triggered by the
+essence edge from paragraph to document, and possibly further updates.
+all objects updated implicitly by the paragraph update are collected
+into a dict that maps old versions (that are still up to date for all
+the client knows) to new ones (that the client should know about).
+this dict is sent to the client in the http response to the paragraph
+update.
+
+this does not address the problem that the server wants to tell the
+client about changes by other users in real-time.  but that's an
+unrelated problem: if a user changes a paragraph, she wants to go on
+working on the changed version right away; if other users change
+stuff, she wants to be notified as soon as possible, but (1) delays
+are acceptable, and (2) she doesn't want the notifications to
+interfere with her work.
+
+*/
+
+
 (function($, obviel) {
 
     // for editing
