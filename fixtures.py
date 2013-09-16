0589750b1a438ad04b02212c465c0a99ce6bf4d0
Matthias Fischmann <mf@zerobuzz.net>
2013-09-16 11:11:46 +0200

    fixtures.py

--- fixtures.py
+++ fixtures.py
@@ -4,47 +4,102 @@ import requests
 import json
 from pprint import pprint
 
-root_url = 'http://localhost:6541/adhocracy/'
+
+root_url = 'http://localhost:6541'
+prop_name = 'no_more_mosquitos'
 
 
-do_everything = False
+def run(request, *params):
+    verbose = True
+
+    response = request(*params)
+    if verbose:
+        pprint((request.__name__, params))
+        pprint(response)
+        if (response.ok):
+            pprint(response.json())
+    return response
 
 
-if do_everything:
-    # 1st prop container: no_more_mosquitos
-    data = { 'content_type': 'adhocracy.interfaces.IProposalContainer',
-             'data': { 'adhocracy.interfaces.IName': { 'name': 'no_more_mosquitos' } }
-           }
-    resp = requests.post(root_url, data=json.dumps(data))
-    pprint(resp)
-    pprint(resp.json())
+# produces internal error on clean zodb state (git version
+# ffcf498c9820ef22ce7b8c3399330157e17459a8).  (there is probably an
+# error in this script, but it should not make the server crash,
+# right?)
+def utest1():
+    data = {'content_type': 'adhocracy.interfaces.IProposalContainer',
+            'data': {'adhocracy.interfaces.IName': {'name': prop_name}}}
+    r = run(requests.post, root_url + '/adhocracy', json.dumps(data))
+
+    r = run(requests.get, root_url + '/adhocracy/' + prop_name + '/_versions')
+    children = r.json()["children"]
+
+    url = children[0]['path']
+
+    r = run(requests.get, root_url + url)
+
+    obj = r.json()["data"]
+    meta = r.json()["meta"]
+
+    obj['adhocracy.interfaces.IDocument']['title'] = 'No More Mosquitos!'
+    obj['adhocracy.interfaces.IVersionable'] = {'follows': [url]}
+
+    r = run(requests.put, root_url + url, json.dumps({ 'content_type': 'adhocracy.interfaces.IProposal', 'data': obj }))
+
+    # ...  now do the same thing once more.
+    data = {'content_type': 'adhocracy.interfaces.IProposalContainer',
+            'data': {'adhocracy.interfaces.IName': {'name': prop_name}}}
+    r = run(requests.post, root_url + '/adhocracy', json.dumps(data))      # i think this should yield a clean error response from backend.  -mf
+
+    r = run(requests.get, root_url + '/adhocracy/' + prop_name + '/_versions')
+    children = r.json()["children"]
+
+    url = children[0]['path']
+
+    r = run(requests.get, root_url + url)
+
+    obj = r.json()["data"]
+    meta = r.json()["meta"]
+
+    obj['adhocracy.interfaces.IDocument']['title'] = 'No More Mosquitos!'
+    obj['adhocracy.interfaces.IVersionable'] = {'follows': [url]}
+
+    r = run(requests.put, root_url + url, json.dumps({ 'content_type': 'adhocracy.interfaces.IProposal', 'data': obj }))
 
 
-if do_everything or True:
-    resp = requests.get(root_url + '/no_more_mosquitos/_versions')
-    predecessor = resp.json()['children'][0]  # just take the first one (if we really just created it, that's the only one)
+def buildFixtures():
+    data = {'content_type': 'adhocracy.interfaces.IProposalContainer',
+            'data': {'adhocracy.interfaces.IName': {'name': prop_name}}}
+    r = run(requests.post, root_url + '/adhocracy', json.dumps(data))
 
-    # v2.1, follows v1
-    data = { 'content_type': 'adhocracy.interfaces.IProposal',
-             'data': { 'adhocracy.interfaces.IProposal': { 'title': 'v2.1', 'description': 'nulleinsneunzwei' },
-                       'adhocracy.interfaces.IVersionable': { 'follows': [predecessor['oid']] },
-                     }
-           }
-    resp = requests.put(root_url + '/no_more_mosquitos/_versions/' + str(predecessor['name']), data=json.dumps(data))
+    r = run(requests.get, root_url + '/adhocracy/' + prop_name + '/_versions')
+    children = r.json()["children"]
+    #pprint(children)
 
-    pprint(resp)
-    pprint(resp.json())
-    exit(0)  # this is still crashing.
+    url = children[0]['path']
+    #pprint(url)
 
-    v21 = resp.json()
+    r = run(requests.get, root_url + url)
 
-    # v2.2, follows v1
-    # v2.3, follows v1
-    # v3.1, follows v2.3
-    # v3.2, follows v2.3
-    # v4.1, follows v2.2
+    obj = r.json()["data"]
+    meta = r.json()["meta"]
+
+    obj['adhocracy.interfaces.IDocument']['title'] = 'No More Mosquitos!'
+    obj['adhocracy.interfaces.IVersionable'] = {'follows': [url]}
+
+    pprint(obj)
+
+    r = run(requests.put, root_url + url, json.dumps({ 'content_type': 'adhocracy.interfaces.IProposal', 'data': obj }))
+    url = r.json()['path']
+
+    obj['adhocracy.interfaces.IDocument']['paragraphs'] = ['wef', 'ofq']
+    r = run(requests.put, root_url + url, json.dumps({ 'content_type': 'adhocracy.interfaces.IProposal', 'data': obj }))
 
 
+#buildFixtures()
+utest1()
 
-print "all done!"
-exit(0)
+#r = run(requests.options, root_url + '/adhocracy');
+#r = run(requests.get, root_url + '/adhocracy');
+#r = run(requests.get, u'http://localhost:6541/adhocracy/no_more_mosquitos/_versions/B2BVZ11')
+
+
