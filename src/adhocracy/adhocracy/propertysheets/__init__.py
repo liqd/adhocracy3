783f04ba287fc6383064002cbbcce0973d78a300
Joscha Krutzki <joka@jokasis.de>
2013-10-01 14:28:01 +0000

    automatic propertysheet adapter registration

--- src/adhocracy/adhocracy/propertysheets/__init__.py
+++ src/adhocracy/adhocracy/propertysheets/__init__.py
@@ -1,75 +1,45 @@
-from zope.interface import (
-    implementer,
-    )
 from zope.dottedname.resolve import resolve
 from pyramid.interfaces import IRequest
 from substanced.interfaces import IPropertySheet
 
 from adhocracy.property import PropertySheetAdhocracyContent
-from adhocracy import propertysheets
+from adhocracy.propertysheets import interfaces
 
 # PropertySheet adapters
 # read sdi/views/property for adapter lookup examples
 
-@implementer(IPropertySheet)
-def propertysheet_iname_adapter(context, request):
-    schema_dotted = propertysheets.interfaces.IName.getTaggedValue('schema')
-    schema = resolve(schema_dotted)
-    sheet = PropertySheetAdhocracyContent(context, request)
-    sheet.schema = schema()
-    return sheet
+class PropertysheetGenericAdapter:
 
+    def __init__(self, isheetmarker):
+        self.isheetmarker = isheetmarker
 
-@implementer(IPropertySheet)
-def propertysheet_iversionable_adapter(context, request):
-    schema_dotted = propertysheets.interfaces.IVersionable.getTaggedValue('schema')
-    schema = resolve(schema_dotted)
-    sheet = PropertySheetAdhocracyContent(context, request)
-    sheet.schema = schema()
-    return sheet
-
-
-@implementer(IPropertySheet)
-def propertysheet_idocument_adapter(context, request):
-    schema_dotted = propertysheets.interfaces.IDocument.getTaggedValue('schema')
-    schema = resolve(schema_dotted)
-    sheet = PropertySheetAdhocracyContent(context, request)
-    sheet.schema = schema()
-    return sheet
-
-
-@implementer(IPropertySheet)
-def propertysheet_itext_adapter(context, request):
-    schema_dotted = propertysheets.interfaces.IText.getTaggedValue('schema')
-    schema = resolve(schema_dotted)
-    sheet = PropertySheetAdhocracyContent(context, request)
-    sheet.schema = schema()
-    return sheet
+    def __call__(self, context, request):
+        schema_dotted = self.isheetmarker.getTaggedValue('schema')
+        schema = resolve(schema_dotted)
+        sheet = PropertySheetAdhocracyContent(context, request)
+        sheet.schema = schema()
+        return sheet
 
 
 def includeme(config): # pragma: no cover
 
-    # TODO more DRY for adapter registration
-    config.registry.registerAdapter(
-        propertysheet_iname_adapter,
-        (propertysheets.interfaces.IName, IRequest),
-        IPropertySheet,
-        propertysheets.interfaces.IName.__identifier__)
-
-    config.registry.registerAdapter(
-        propertysheet_iversionable_adapter,
-        (propertysheets.interfaces.IVersionable, IRequest),
-        IPropertySheet,
-        propertysheets.interfaces.IVersionable.__identifier__)
-
-    config.registry.registerAdapter(
-        propertysheet_idocument_adapter,
-        (propertysheets.interfaces.IDocument, IRequest),
-        IPropertySheet,
-        propertysheets.interfaces.IDocument.__identifier__)
-
-    config.registry.registerAdapter(
-        propertysheet_itext_adapter,
-        (propertysheets.interfaces.IText, IRequest),
-        IPropertySheet,
-        propertysheets.interfaces.IText.__identifier__)
+    # get all IPropertySheetMarker interfaces
+    # inspect.isclass is not working with interfaces,
+    # so we have to do it manually
+    isheetmarkers = []
+    for key in dir(interfaces):
+        try:
+            value = getattr(interfaces, key)
+            if value is interfaces.IPropertySheetMarker:
+                continue
+            if issubclass(value, interfaces.IPropertySheetMarker):
+                isheetmarkers.append(value)
+        except TypeError:
+            continue
+    # register generic adapter for all IPropertySheetMarkers
+    for isheetmarker in isheetmarkers:
+        config.registry.registerAdapter(
+            PropertysheetGenericAdapter(isheetmarker),
+            (isheetmarker, IRequest),
+            IPropertySheet,
+            isheetmarker.__identifier__)
