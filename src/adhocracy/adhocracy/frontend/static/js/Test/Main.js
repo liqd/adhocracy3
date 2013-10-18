dae3f95b2f265add1e8048d561d564af93c4697d
Matthias Fischmann <mf@zerobuzz.net>
2013-10-18 16:53:46 +0200

    frontend / proposal workbench: first non-trivial test case

--- src/adhocracy/adhocracy/frontend/static/js/Test/Main.js
+++ src/adhocracy/adhocracy/frontend/static/js/Test/Main.js
@@ -1,5 +1,18 @@
-define(['jquery', 'chai', 'mocha', 'Adhocracy', 'Adhocracy/Frames/ProposalWorkbench'], function($, chai, mocha, Adhocracy, ProposalWorkbench) {
-
+define(['jquery',
+        // 'obviel',
+        // 'obvieltemplate',
+        'chai',
+        'mocha',
+        'Adhocracy',
+        'Adhocracy/Frames/ProposalWorkbench'],
+       function($,
+                // obviel,
+                // obvieltemplate,
+                chai,
+                mocha,
+                Adhocracy,
+                ProposalWorkbench)
+{
     mocha.setup('bdd');
 
     var expect = chai.expect;
@@ -18,8 +31,24 @@ define(['jquery', 'chai', 'mocha', 'Adhocracy', 'Adhocracy/Frames/ProposalWorkbe
             expect($('#proposal_workbench_discussion')).to.not.have.length(0);
         });
 
-        it('detail div must contain "..."', function() {
+        it('detail div must contain "..." after Adhocracy.init()', function() {
+            Adhocracy.init();
             expect($('#proposal_workbench_detail')[0].innerText).to.equal('...');
+        });
+
+        it('directory div must contain list of proposals as created by fixtures.js', function() {
+
+            // FIXME: fixtures.js does not exist.  the following
+            // should be slightly out of order, but the idea should be
+            // valid.
+
+            ProposalWorkbench.open_proposals('/adhocracy/');
+            var expected_names = ['proposal DAG 1', 'proposal DAG 2'];
+
+            var list_items = $('#proposal_workbench_directory')[0].getElementsByTagNames('li');
+            list_items.forEach(function(li) {
+                expect(li.innerText).to.equal(expected_names.shift());
+            });
         });
     });
 
