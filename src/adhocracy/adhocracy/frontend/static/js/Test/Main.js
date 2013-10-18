dee8777e8b60e2ac226f486e7ea2119b9bb92ca6
Matthias Fischmann <mf@zerobuzz.net>
2013-10-18 15:31:12 +0200

    frontend: more trivial test cases.

--- src/adhocracy/adhocracy/frontend/static/js/Test/Main.js
+++ src/adhocracy/adhocracy/frontend/static/js/Test/Main.js
@@ -1,15 +1,25 @@
-define(['chai', 'mocha', 'Adhocracy', 'Adhocracy/Frames/ProposalWorkbench'], function(chai, mocha, Adhocracy, ProposalWorkbench) {
+define(['jquery', 'chai', 'mocha', 'Adhocracy', 'Adhocracy/Frames/ProposalWorkbench'], function($, chai, mocha, Adhocracy, ProposalWorkbench) {
 
     mocha.setup('bdd');
 
     var expect = chai.expect;
 
-    // console.log(expect(18).to.equal(18));
-    // console.log(expect(18).to.equal(19));
-
     describe('something pure yet lame', function() {
         it('should half its input', function() {
             expect(ProposalWorkbench.something_pure(18)).to.equal(9);
+        });
+    });
+
+    describe('some trivial DOM invariants', function() {
+        it('must have three divs named appropriately', function() {
+            expect($('#___probably_not_a_valid_dom_elem_id___')).to.have.length(0);
+            expect($('#proposal_workbench_detail')).to.not.have.length(0);
+            expect($('#proposal_workbench_directory')).to.not.have.length(0);
+            expect($('#proposal_workbench_discussion')).to.not.have.length(0);
+        });
+
+        it('detail div must contain "..."', function() {
+            expect($('#proposal_workbench_detail')[0].innerText).to.equal('...');
         });
     });
 
