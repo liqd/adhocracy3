4dec5df1aac3bb58b03a699e7b105b14cbc5e641
Sönke Hahn <soenkehahn@gmail.com>
2013-10-24 13:51:15 +0200

    frontend: TypeScript imports work correctly
    with d.ts files from DefinitelyTyped

--- src/adhocracy/adhocracy/frontend/static/js/Test/Main.ts
+++ src/adhocracy/adhocracy/frontend/static/js/Test/Main.ts
@@ -1,84 +1,74 @@
-declare var define;
-declare var describe;
-declare var it;
-declare var before;
-declare var beforeEach;
+/// <reference path="../../submodules/DefinitelyTyped/requirejs/require.d.ts"/>
+/// <reference path="../../submodules/DefinitelyTyped/jquery/jquery.d.ts"/>
+/// <reference path="../../submodules/DefinitelyTyped/mocha/mocha.d.ts"/>
+/// <reference path="../../submodules/DefinitelyTyped/chai/chai.d.ts"/>
 
-// import Util = require('../Adhocracy/Util');
+// for importing things in TypeScript, see
+// http://www.codeproject.com/Articles/528295/ModularizationplusinplusTypeScript
 
-define(['jquery',
-        // 'obviel',
-        // 'obvieltemplate',
-        'chai',
-        'mocha',
-        'Adhocracy',
-        'Adhocracy/Frames/ProposalWorkbench',
-        'Adhocracy/Util',],
-       function($,
-                // obviel,
-                // obvieltemplate,
-                chai,
-                mocha,
-                Adhocracy,
-                ProposalWorkbench,
-                Util)
-{
-    mocha.setup('bdd');
+// This is missing in mocha's d.ts, isn't it?
+declare var mocha : Mocha; // Should not be called 'mocha', since that clashes
+                           // with the (required) 'mocha'-div, but I didn't get
+                           // that to work.
+mocha = require('mocha');
 
-    var expect = chai.expect;
+chai = require('chai');
+var expect = chai.expect;
 
-    describe('something pure yet lame', function() {
-        it('should half its input', function() {
-            expect(ProposalWorkbench.something_pure(18)).to.equal(9);
-        });
+import Adhocracy = require('Adhocracy');
+import ProposalWorkbench = require('Adhocracy/Frames/ProposalWorkbench');
+import Util = require('Adhocracy/Util');
+
+
+mocha.setup({ui: 'bdd', slow: 30, timeout: 2000});
+
+describe('something pure yet lame', function() {
+    it('should half its input', function() {
+        expect(ProposalWorkbench.something_pure(18)).to.equal(9);
+    });
+});
+
+describe('some trivial DOM invariants', function() {
+    it('must have three divs named appropriately', function() {
+        expect($('#___probably_not_a_valid_dom_elem_id___')).to.have.length(0);
+        expect($('#proposal_workbench_detail')).to.not.have.length(0);
+        expect($('#proposal_workbench_directory')).to.not.have.length(0);
+        expect($('#proposal_workbench_discussion')).to.not.have.length(0);
     });
 
-    describe('some trivial DOM invariants', function() {
-        it('must have three divs named appropriately', function() {
-            expect($('#___probably_not_a_valid_dom_elem_id___')).to.have.length(0);
-            expect($('#proposal_workbench_detail')).to.not.have.length(0);
-            expect($('#proposal_workbench_directory')).to.not.have.length(0);
-            expect($('#proposal_workbench_discussion')).to.not.have.length(0);
-        });
+    it('detail div must contain "..." after Adhocracy.init()', function() {
+        Adhocracy.init();
+        expect($('#proposal_workbench_detail')[0].innerText).to.equal('...');
+    });
 
-        it('detail div must contain "..." after Adhocracy.init()', function() {
-            Adhocracy.init();
-            expect($('#proposal_workbench_detail')[0].innerText).to.equal('...');
-        });
+    describe('directory div must contain list of proposals as created by fixtures.js', function() {
 
-        describe('directory div must contain list of proposals as created by fixtures.js', function() {
+        // FIXME: fixtures.js does not exist.  the following
+        // should be slightly out of order, but the idea should be
+        // valid.
 
-            // FIXME: fixtures.js does not exist.  the following
-            // should be slightly out of order, but the idea should be
-            // valid.
+        var expected_names;
+        var directory_div;
 
-            var expected_names;
-            var directory_div;
-
-            beforeEach(function(done) {
-                ProposalWorkbench.open_proposals('/adhocracy/', function() {
-                    expected_names = ['proposal DAG 1', 'proposal DAG 2'];
-                    directory_div = $.makeArray($('#proposal_workbench_directory'))[0]
-                    done();
-                });
+        beforeEach(function(done) {
+            ProposalWorkbench.open_proposals('/adhocracy/', function() {
+                expected_names = ['proposal DAG 1', 'proposal DAG 2'];
+                directory_div = $.makeArray($('#proposal_workbench_directory'))[0]
+                done();
             });
+        });
 
-            it('list_items should contain the right proposal names.', function() {
-                expected_names.forEach(function (name) {
+        it('list_items should contain the right proposal names.', function() {
+            expected_names.forEach(function (name) {
 //                     expect(directory_div.innerText).to.match(
 //                         new RegEx('/' + name + '/'),
 //                         "wef");
-                    expect(Util.isInfixOf(name, directory_div.innerText)).to.equal(true, name);
-                });
+                expect(Util.isInfixOf(name, directory_div.innerText)).to.equal(true, name);
             });
-
         });
-
     });
-
-    return {
-        run: function() {
-            mocha.run();
-        }
-    }
 });
+
+export function run_tests() {
+    mocha.run(function() {});
+};
---
+++ src/adhocracy/adhocracy/frontend/static/require-config.js
@@ -0,0 +1,35 @@
+require.config({
+    baseUrl: './js/',
+    paths: {
+        jquery: '../jquery-1.7.2',  // FIXME: ../submodules/jquery/dist/...
+        datalink: '../submodules/jquery-datalink/jquery.datalink',
+        bbq: '../submodules/submodules/jquery-bbq/jquery.ba-bbq',
+        obviel: '../submodules/obviel/src/obviel/obviel',
+        obvieltemplate: '../submodules/obviel/src/obviel/obviel-template',
+        chai: '../submodules/chai/chai',
+        mocha: '../submodules/mocha/mocha',
+    },
+    shim: {
+        jquery: {
+        },
+        datalink: {
+            deps: ['jquery'],
+        },
+        bbq: {
+            deps: ['jquery'],
+        },
+        obviel: {
+            exports: 'obviel',
+            deps: ['jquery', 'obvieltemplate'],
+        },
+        obvieltemplate: {
+            exports: 'obvieltemplate',
+            deps: ['jquery'],
+        },
+        mocha: {
+            exports: 'mocha',
+        },
+    }
+});
+
+// require(['jquery'], function($) {});
