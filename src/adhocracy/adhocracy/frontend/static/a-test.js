4368750d54a189c4b48dfb79733e7721e35ff8c8
Matthias Fischmann <mf@zerobuzz.net>
2013-12-12 17:39:42 +0100

    document detail view

--- src/adhocracy/adhocracy/frontend/static/a-test.js
+++ src/adhocracy/adhocracy/frontend/static/a-test.js
@@ -3,7 +3,6 @@ var app = angular.module('ATestMod', []);
 
 app.controller('ATest', function(adhGet, $scope) {
     this.path = '/adhocracy';
-    $scope.directory;
 
     adhGet(this.path).then(function(d) {
         var pool = d.data['adhocracy.propertysheets.interfaces.IPool'];
@@ -18,22 +17,45 @@ app.controller('ATest', function(adhGet, $scope) {
         // to change order and filters dynamically anyway.
         //
         // FIXME: write a function that follows references
-        // transparently through the data model.
+        // transparently through the data model.  (it takes an array
+        // of field names, and follows these fields names down into an
+        // object.  each time it hits a reference, the reference is
+        // replaced by the referenced object before the path is
+        // followed further.)
         pool.elements.map(function(ref) {
-            adhGet(ref.path).then(function(d) {
-                var dag = d.data['adhocracy.propertysheets.interfaces.IDAG'];
-                if (dag.versions.length > 0) {
-                    var headPath = dag.versions[0].path;
-                    adhGet(headPath).then(function(d) {
-                        var doc = d.data['adhocracy.propertysheets.interfaces.IDocument'];
-                        $scope.directory.push(doc.title);
+            adhGet(ref.path).then(function(dag) {
+                var dagPS = dag.data['adhocracy.propertysheets.interfaces.IDAG'];
+                if (dagPS.versions.length > 0) {
+                    var dagPath = dag.path;
+                    var headPath = dagPS.versions[0].path;
+                    adhGet(headPath).then(function(doc) {
+                        var docPS = doc.data['adhocracy.propertysheets.interfaces.IDocument'];
+                        $scope.directory.push([headPath, docPS.title]);
                     });
                 } else {
-                    $scope.directory.push('(empty)');
+                    $scope.directory.push(undefined);
                 }
             });
         });
     });
+
+    $scope.detail = {};
+    $scope.detail_paragraphs = [];
+
+    this.showDetail = function(path) {
+        adhGet(path).then(function(data) {
+            $scope.detail = data;
+
+            var paragraphRefs = data.data['adhocracy.propertysheets.interfaces.IDocument'].paragraphs;
+            for (ix in paragraphRefs) {
+                adhGet(paragraphRefs[ix].path).then((function(ix) {
+                    return function(paragraph) {
+                        $scope.detail_paragraphs[ix] = paragraph.data['adhocracy.propertysheets.interfaces.IParagraph'].text;
+                    };
+                })(ix));
+            }
+        });
+    }
 
     this.qty = 7;
     this.cost = 8;
