d430395e72ec1ef16529f5f9ffe611ad77381c7e
Matthias Fischmann <mf@zerobuzz.net>
2014-03-25 15:04:46 +0100

    removed deprecated code

--- src/adhocracy/adhocracy/frontend/static/js/Adhocracy/Services/Http.ts
+++ src/adhocracy/adhocracy/frontend/static/js/Adhocracy/Services/Http.ts
@@ -15,7 +15,6 @@ export interface IService {
     put : (path : string, obj : Types.Content) => ng.IPromise<Types.Content>;
     postNewVersion : (oldVersionPath : string, obj : Types.Content) => ng.IPromise<Types.Content>;
     postToPool : (poolPath : string, obj : Types.Content) => ng.IPromise<Types.Content>;
-    drill : (data : any, xpath : any, target : any, ordered : boolean) => void;
 }
 
 export function factory($http : ng.IHttpService) : IService {
@@ -24,7 +23,6 @@ export function factory($http : ng.IHttpService) : IService {
         put: put,
         postNewVersion: postNewVersion,
         postToPool: postToPool,
-        drill: drill,
     };
 
     function assertResponse(msg : string, path : string) {
@@ -56,81 +54,6 @@ export function factory($http : ng.IHttpService) : IService {
 
     function postToPool(poolPath : string, obj : Types.Content) : ng.IPromise<Types.Content> {
         return $http.post(poolPath, exportContent(obj)).then(assertResponse("adhHttp.postToPool", poolPath));
-    }
-
-    // DEPRECATED?
-    function drill(data : any, xpath : /* string[] or string[][] */ any, target : any, ordered : boolean) : void {
-        function resolveReference() {
-            if ("path" in data) {
-                adhHttp.get(data.path).then((resource) => {
-                    adhHttp.drill(resource, xpath, target, ordered);
-                });
-            } else {
-                console.log(data);
-                throw "adhHttp.drill: not a resource and not a reference.";
-            }
-        }
-
-        if ("content_type" in data) {
-            if ("data" in data) {
-                adhHttp.drill(data.data, xpath, target, ordered);
-                return;
-            } else {
-                resolveReference();
-                return;
-            }
-        } else {
-            if (xpath.length === 0) {
-                if (target.xpath.length !== 1) {
-                    throw "not implemented.";
-                }
-                target.ref[target.xpath[0]] = data;
-                return;
-            }
-            var step = xpath.shift();
-            if (typeof step === "string" || typeof step === "number") {
-                adhHttp.drill(data[step], xpath, target, ordered);
-                return;
-            }
-            if (step instanceof Array) {
-                if (step.length !== 1 || !(typeof(step[0]) === "string" || typeof(step[0]) === "number")) {
-                    // FIXME: what about "[[step]]"?
-                    // FIXME: if xpath contains more than one [step], i don't know what'll happen...
-                    console.log(step);
-                    throw "internal";
-                }
-                step = step[0];
-
-                if (!(data[step] instanceof Array)) {
-                    console.log(data);
-                    console.log(step);
-                    throw "internal";
-                }
-                var elements = data[step];
-
-                if (!(target.ref instanceof Array)) {
-                    console.log(target);
-                    throw "not implemented.";
-                }
-
-                if (!ordered) {
-                    console.log(ordered);
-                    throw "not implemented.";
-                }
-
-                // loop over step, and call drill recursively on
-                // each element, together with the corresponding
-                // element of target.
-                for (var ix in elements) {
-                    var subtarget = {
-                        ref: target.ref,
-                        xpath: [ix],
-                    };
-                    adhHttp.drill(elements[ix], Util.deepcp(xpath), subtarget, ordered);
-                }
-                return;
-            }
-        }
     }
 
     return adhHttp;
