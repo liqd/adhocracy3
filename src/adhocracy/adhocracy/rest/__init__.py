a7a5c64be6c17d16a4298510e47853d4012f111d
Joscha Krutzki <joka@jokasis.de>
2014-04-09 18:53:01 +0200

    remove colanderjson renderer, not needed anymore

--- src/adhocracy/adhocracy/rest/__init__.py
+++ src/adhocracy/adhocracy/rest/__init__.py
@@ -1,18 +1,4 @@
 """Rest API configuration."""
-from pyramid.renderers import JSON
-from pyramid.renderers import _marker
-from colander import _null
-
-
-def colander_null_json_adapter(context, request):
-    """Return None for null objects."""
-    serializable = _marker
-    if isinstance(context, _null):
-        serializable = None
-    return serializable
-
-
-jsoncolander = JSON(adapters=[(_null, colander_null_json_adapter)], indent=2)
 
 
 def includeme(config):  # pragma: no cover
@@ -20,5 +6,3 @@ def includeme(config):  # pragma: no cover
     config.include('cornice')
     config.scan('.views')
     config.scan('.exceptions')
-    ##config#.add_renderer('jsoncolander', jsoncolander)
-    #config.include('.views')
