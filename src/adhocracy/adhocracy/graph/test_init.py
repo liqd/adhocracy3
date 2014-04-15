67fbc99c0ee84e8bef59782a52c9b4e626cae1fb
Joscha Krutzki <joka@jokasis.de>
2014-04-15 18:25:39 +0200

    graph unit tests: import only from graph package

--- src/adhocracy/adhocracy/graph/test_init.py
+++ src/adhocracy/adhocracy/graph/test_init.py
@@ -1,35 +1,48 @@
-from adhocracy.folder import ResourcesAutolNamingFolder
+from adhocracy.interfaces import IResource
 from adhocracy.interfaces import AdhocracyReferenceType
-from adhocracy.interfaces import IItemVersion
-from adhocracy.resources import ResourceFactory
-from adhocracy.sheets.versions import IVersionableFollowsReference
 from pyramid import testing
-from substanced.objectmap import ObjectMap
+
 from . import collect_reftypes
 from . import is_in_subtree
 
 import unittest
 
+############
+#  helper  #
+############
+
+
+def create_dummy_resource(parent=None, iface=IResource):
+    """Create dummy resrouce and add it to the parent objectmap."""
+    from pyramid.traversal import resource_path_tuple
+    from substanced.util import find_objectmap
+    res = testing.DummyResource(__parent__=parent,
+                                __provides__=iface,)
+    om = find_objectmap(parent)
+    if om:
+        oid = om.new_objectid()
+        res.__oid__ = oid
+        res.__name__ = str(oid)
+        path = resource_path_tuple(res)
+        om.add(res, path)
+        parent[res.__name__] = res
+    return res
+
+
+##########
+#  tests #
+##########
+
 
 class GraphUnitTest(unittest.TestCase):
 
-    def new_objectid(self):
-        return self.objectmap.new_objectid()
-
-    def make_one(self, iface=None, appstructs={}):
-        """Make a resource ."""
-        return ResourceFactory(iface or IItemVersion)(self.context,
-                                                      appstructs=appstructs)
-
     def setUp(self):
-        self.config = testing.setUp()
-        context = ResourcesAutolNamingFolder()
+        from substanced.objectmap import ObjectMap
+        context = create_dummy_resource()
         context.__objectmap__ = ObjectMap(context)
         self.context = context
-        self.child = self.make_one()
-
-    def tearDown(self):
-        testing.tearDown()
+        child = create_dummy_resource(parent=context)
+        self.child = child
 
     def test_is_in_subtree_with_none_ancestor(self):
         """False if ancestors is None."""
@@ -40,7 +53,6 @@ class GraphUnitTest(unittest.TestCase):
         """False if ancestors is an empty list."""
         result = is_in_subtree(self.child, [])
         assert result is False
-
 
     def test_is_in_subtree_with_none_descendant(self):
         """False if descendant is None."""
@@ -62,9 +74,10 @@ class GraphUnitTest(unittest.TestCase):
         descendent.
 
         """
-        root = self.make_one()
-        element = self.make_one()
+        root = create_dummy_resource(parent=self.context)
+        element = create_dummy_resource(parent=self.context)
         om = self.context.__objectmap__
+        om.object_for(root.__oid__)
         om.connect(root, element, AdhocracyReferenceType)
         result = is_in_subtree(element, [root])
         assert result is True
@@ -77,8 +90,9 @@ class GraphUnitTest(unittest.TestCase):
         descendent.
 
         """
-        other_version = self.make_one()
-        old_version = self.make_one()
+        from adhocracy.sheets.versions import IVersionableFollowsReference
+        other_version = create_dummy_resource(parent=self.context)
+        old_version = create_dummy_resource(parent=self.context)
         om = self.context.__objectmap__
         om.connect(other_version, old_version, IVersionableFollowsReference)
         result = is_in_subtree(old_version, [other_version])
@@ -92,9 +106,9 @@ class GraphUnitTest(unittest.TestCase):
         descendent.
 
         """
-        grandma = self.make_one()
-        dad = self.make_one()
-        daugher = self.make_one()
+        grandma = create_dummy_resource(parent=self.context)
+        dad = create_dummy_resource(parent=self.context)
+        daugher = create_dummy_resource(parent=self.context)
         om = self.context.__objectmap__
         om.connect(grandma, dad, AdhocracyReferenceType)
         om.connect(dad, daugher, AdhocracyReferenceType)
@@ -109,9 +123,10 @@ class GraphUnitTest(unittest.TestCase):
         follows relation.
 
         """
-        dad = self.make_one()
-        daugher = self.make_one()
-        step_son = self.make_one()
+        from adhocracy.sheets.versions import IVersionableFollowsReference
+        dad = create_dummy_resource(parent=self.context)
+        daugher = create_dummy_resource(parent=self.context)
+        step_son = create_dummy_resource(parent=self.context)
         om = self.context.__objectmap__
         om.connect(dad, daugher, AdhocracyReferenceType)
         om.connect(step_son, daugher, IVersionableFollowsReference)
@@ -126,9 +141,9 @@ class GraphUnitTest(unittest.TestCase):
         one.
 
         """
-        root = self.make_one()
-        not_root = self.make_one()
-        element = self.make_one()
+        root = create_dummy_resource(parent=self.context)
+        not_root = create_dummy_resource(parent=self.context)
+        element = create_dummy_resource(parent=self.context)
         om = self.context.__objectmap__
         om.connect(root, element, AdhocracyReferenceType)
         result = is_in_subtree(element, [root, not_root])
@@ -150,26 +165,32 @@ class GraphUnitTest(unittest.TestCase):
 
     def test_collect_reftypes_two_types(self):
         """Two reftypes in objectmap, both should be found."""
-        dad = self.make_one()
-        daugher = self.make_one()
-        step_son = self.make_one()
+        dad = create_dummy_resource(parent=self.context)
+        daugher = create_dummy_resource(parent=self.context)
+        step_son = create_dummy_resource(parent=self.context)
         om = self.context.__objectmap__
         om.connect(dad, daugher, AdhocracyReferenceType)
-        om.connect(step_son, daugher, IVersionableFollowsReference)
+
+        class IOtherReferenceType(AdhocracyReferenceType):
+            pass
+        om.connect(step_son, daugher, IOtherReferenceType)
         result = collect_reftypes(om)
         assert len(result) == 2
         assert AdhocracyReferenceType in result
-        assert IVersionableFollowsReference in result
+        assert IOtherReferenceType in result
 
     def test_collect_reftypes_one_excluded(self):
         """Two reftypes in OM, one is excluded, the other should be found."""
-        dad = self.make_one()
-        daugher = self.make_one()
-        step_son = self.make_one()
+        dad = create_dummy_resource(parent=self.context)
+        daugher = create_dummy_resource(parent=self.context)
+        step_son = create_dummy_resource(parent=self.context)
         om = self.context.__objectmap__
         om.connect(dad, daugher, AdhocracyReferenceType)
-        om.connect(step_son, daugher, IVersionableFollowsReference)
+
+        class IOtherReferenceType(AdhocracyReferenceType):
+            pass
+        om.connect(step_son, daugher, IOtherReferenceType)
         result = collect_reftypes(om, [AdhocracyReferenceType])
         assert len(result) == 1
-        assert IVersionableFollowsReference in result
+        assert IOtherReferenceType in result
         assert AdhocracyReferenceType not in result
