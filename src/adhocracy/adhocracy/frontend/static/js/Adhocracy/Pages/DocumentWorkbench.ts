5a6aa0b66a000f7d37f94a5b1ff905cebd6299ab
Fabian Gerahrdt <seriousemailadress@googlemail.com>
2014-04-24 14:18:54 +0200

    directives for recursive sections; no edit yet

--- src/adhocracy/adhocracy/frontend/static/js/Adhocracy/Pages/DocumentWorkbench.ts
+++ src/adhocracy/adhocracy/frontend/static/js/Adhocracy/Pages/DocumentWorkbench.ts
@@ -52,6 +52,49 @@ export function run<Data>() {
     var app = angular.module("NGAD", []);
 
 
+    app.factory('RecursionHelper', ['$compile', function($compile){
+        return {
+            /**
+             * Manually compiles the element, fixing the recursion loop.
+             * @param element
+             * @param [link] A post-link function, or an object with function(s) registered via pre and post properties.
+             * @returns An object containing the linking functions.
+             */
+            compile: function(element, link){
+                // Normalize the link parameter
+                if(jQuery.isFunction(link)){
+                    link = { post: link };
+                }
+
+                // Break the recursion loop by removing the contents
+                var contents = element.contents().remove();
+                var compiledContents;
+                return {
+                    pre: (link && link.pre) ? link.pre : null,
+                    /**
+                     * Compiles and re-adds the contents
+                     */
+                    post: function(scope, element){
+                        // Compile the contents
+                        if(!compiledContents){
+                            compiledContents = $compile(contents);
+                        }
+                        // Re-add the compiled contents to the element
+                        compiledContents(scope, function(clone){
+                            element.append(clone);
+                        });
+
+                        // Call the post-linking function, if any
+                        if(link && link.post){
+                            link.post.apply(null, arguments);
+                        }
+                    }
+                };
+            }
+        };
+    }]);
+
+
     // services
 
     app.factory("adhHttp",   ["$http",                                    AdhHttp.factory]);
@@ -201,15 +244,25 @@ export function run<Data>() {
         };
     });
 
-    app.directive("adhDocumentSheetEdit", function() {
+    app.directive("adhDocumentSheetEdit", ["$http", "$q", function($http, $q) {
         return {
             restrict: "E",
             templateUrl: templatePath + "/Sheets/IDocument/Edit.html",
             scope: {
                 sheet: "=",
             },
+            controller: function($scope) {
+                var versionPromises = $scope.sheet.elements.map( (path) =>
+                    $http.get( decodeURIComponent(path) )
+                         .then( (resp) => resp.data )
+                );
+
+                $q.all(versionPromises).then( (versions) =>
+                    $scope.sectionVersions = versions
+                );
+            },
         };
-    });
+    }]);
 
     app.directive("adhDocumentSheetShow", function() {
         return {
@@ -242,12 +295,14 @@ export function run<Data>() {
                 $scope.proposalVersion = (new Resources.Resource("adhocracy.resources.proposal.IProposalVersion"))
                                               .addIDocument("", "", []);
 
+
                 $scope.paragraphVersions = [];
 
                 $scope.pushParagraphVersion = function () {
                     $scope.paragraphVersions.push(new Resources.Resource("adhocracy.resources.paragraph.IParagraphVersion")
                                                       .addIParagraph(""));
                 };
+
 
                 $scope.commit = function() {
                     var proposalPromise = Resources.postProposal($http, $q, $scope.proposalVersion, $scope.paragraphVersions);
@@ -260,6 +315,38 @@ export function run<Data>() {
             }
         };
     }]);
+
+
+    app.directive("adhSectionSheetEdit", ["$http", "$q", "RecursionHelper", function($http, $q, RecursionHelper) {
+        return {
+            restrict: "E",
+            templateUrl: templatePath + "/Sheets/ISection/Edit.html",
+            compile: (element) => RecursionHelper.compile(element),
+            scope: {
+                sheet: "="
+            },
+            controller: function ($scope) {
+                var versionPromises = $scope.sheet.elements.map( (path) =>
+                    $http.get(decodeURIComponent(path))
+                         .then( (resp) => resp.data )
+                );
+
+                $q.all(versionPromises).then( (versions) => {
+                    $scope.concreteElements = versions;
+                });
+            },
+        };
+    }]);
+
+    app.directive("adhParagraphSheetEdit", function() {
+        return {
+            restrict: "E",
+            templateUrl: templatePath + "/Sheets/IParagraph/Edit.html",
+            scope: {
+                sheet: "="
+            },
+        };
+    });
 
 
     // get going
