9ad4e24fe738e88318de59eeb3bf18b7e438a5e8
Matthias Fischmann <mf@zerobuzz.net>
2014-06-10 17:11:26 +0200

    add comment

--- src/adhocracy/adhocracy/frontend/static/js/Adhocracy/Services/Config.ts
+++ src/adhocracy/adhocracy/frontend/static/js/Adhocracy/Services/Config.ts
@@ -1,3 +1,9 @@
+// simple config mechanism.  exported as a service to allow for
+// injecting an alternative config object in tests.  (there are many
+// obvious design changes, but that is future work: without more
+// sophisticated requirements, not much can be gained from
+// implementing them.)
+
 export interface Type {
     templatePath: string;
     jsonPrefix: string;
