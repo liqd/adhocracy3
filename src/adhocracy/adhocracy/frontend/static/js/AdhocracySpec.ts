b1818817fde1e3aa340af3026061932204f83fd2
Tobias Bengfort <tobias.bengfort@liqd.de>
2014-06-18 17:01:59 +0200

    document usage with node

--- src/adhocracy/adhocracy/frontend/static/js/AdhocracySpec.ts
+++ src/adhocracy/adhocracy/frontend/static/js/AdhocracySpec.ts
@@ -1,3 +1,9 @@
 import UtilSpec = require("./Adhocracy/UtilSpec");
 
+/**
+ * This module includes all frontend unit tests.
+ * It can be used both in browser (compile with amd module system) and
+ * with jasmine-node (compile with commonjs module system).
+ */
+
 UtilSpec.register();
