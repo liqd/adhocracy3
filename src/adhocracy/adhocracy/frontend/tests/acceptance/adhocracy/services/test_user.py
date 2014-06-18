edbc0142154c68dd9102651be0de605dfb478063
Joscha Krutzki <joka@jokasis.de>
2014-06-18 11:58:44 +0200

    use splinter and server_sample fixtures for frontend tests WORK IN PROGESS

--- src/adhocracy/adhocracy/frontend/tests/acceptance/adhocracy/services/test_user.py
+++ src/adhocracy/adhocracy/frontend/tests/acceptance/adhocracy/services/test_user.py
@@ -7,38 +7,19 @@ import time
 
 import unittest
 
-import ipdb
-#ipdb.set_trace()
 
-
-def start_adh_session():
-    capabilities = {
-        "browserName": "firefox",
-        "version": "",
-        "platform": "ANY",
-        "javascriptEnabled": True,
-    }
-    selenium_grid_url = "http://localhost:4444/wd/hub"
-    driver = webdriver.Remote(desired_capabilities=capabilities, command_executor=selenium_grid_url)
-
-    #waiter = wait.WebDriverWait(driver, 0.1)
-    driver.set_page_load_timeout(15)
-    driver.implicitly_wait(0.2)
-
-    driver.get("http://lig:6541/frontend_static/root.html")
-    assert "adhocracy root page" in driver.title
-
-    return driver
+def test_login_success(server_sample, browser):
+    url = server_sample.application_url + 'frontend_static/root.html'
+    browser.visit(url)
+    # TODO ...
+    #browser.browser.fill_form({'credentials.name': 'mr.nice',
+    #                           'credentials.password': 'gandalf'})
+    #import ipdb; ipdb.set_trace()
 
 
 class TestDirectiveLogin(unittest.TestCase):
     def setUp(self):
-        self.driver = start_adh_session()
-        self.good_user = {"name": "mr.nice", "password": "gandalf"}
         self.bad_user = {"name": "mr.evil", "password": "getoutofmyway"}
-
-    def tearDown(self):
-        self.driver.close()
 
     def _test_login(self, user):
         time.sleep(0.8)  # FIXME: there are better ways!
@@ -62,9 +43,6 @@ class TestDirectiveLogin(unittest.TestCase):
             return False
 
         return True
-
-    def test_login_success(self):
-        self.assertTrue(self._test_login(self.good_user))
 
     def test_login_failure(self):
         self.assertFalse(self._test_login(self.bad_user))
