0bd8f021f00f49f361488508884fbabf35017b81
Joscha Krutzki <joka@jokasis.de>
2014-06-23 14:11:46 +0200

    use splinter testbrowser for frontend functional tests

--- src/adhocracy/adhocracy/frontend/tests/functional/adhocracy/test_util.py
+++ src/adhocracy/adhocracy/frontend/tests/functional/adhocracy/test_util.py
@@ -1,55 +1,28 @@
-from selenium import webdriver
-from selenium.webdriver.common import proxy
-from selenium.webdriver.support import wait
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
-import time
-
-import unittest
+import pytest
 
 
-def start_adh_session():
-    capabilities = {
-        "browserName": "firefox",
-        "version": "",
-        "platform": "ANY",
-        "javascriptEnabled": True,
-    }
-    selenium_grid_url = "http://localhost:4444/wd/hub"
-    driver = webdriver.Remote(desired_capabilities=capabilities, command_executor=selenium_grid_url)
+class TestDeepCp:
 
-    #waiter = wait.WebDriverWait(driver, 0.1)
-    driver.set_page_load_timeout(15)
-    driver.implicitly_wait(0.2)
+    @pytest.fixture()
+    def browser_root(self, browser, server_sample):
+        url = server_sample.application_url + 'frontend_static/root.html'
+        browser.visit(url)
+        return browser
 
-    driver.get("http://lig:6541/frontend_static/root.html")
-    assert "adhocracy root page" in driver.title
+    def _call_fut(self, browser, obj_in):
+        code ="""
+        var U = require('Adhocracy/Util');
+        U.deepcp({0});
+        """.format(str(obj_in))
+        return browser.evaluate_script(code)
 
-    return driver
-
-
-class TestDeepCp(unittest.TestCase):
-    def setUp(self):
-        self.driver = start_adh_session()
-
-    def tearDown(self):
-        self.driver.close()
-
-    def _call_fut(self, obj_in):
-        return self.driver.execute_script(
-            """
-            var U = require('Adhocracy/Util');
-            return U.deepcp(arguments[0]);
-            """,
-            obj_in);
-
-    def test_equal(self):
+    def test_equal(self, browser_root):
         value = None
-        assert self._call_fut(value) == value
+        assert self._call_fut(browser_root, value) == value
 
-    def test_empty_obj(self):
+    def test_empty_obj(self, browser_root):
         value = {}
-        assert self._call_fut(value) == value
+        assert self._call_fut(browser_root, value) == value
 
     # deactivated: webdriver swallows trailing null values in
     # javascript arrays, so this test breaks.  FIXME: need to fix
@@ -58,4 +31,3 @@ class TestDeepCp(unittest.TestCase):
     #def test_some_obj(self):
     #    value = {'a': 3, 'b': None, 'c': [None]}
     #    assert self._call_fut(value) == value
-
