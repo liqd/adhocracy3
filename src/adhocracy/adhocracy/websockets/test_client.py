ba800569a68edcfdffc72853cc53f5369e21bf72
Christian Siefkes <christian@siefkes.net>
2014-06-25 14:34:17 +0200

    Improve test coverage.

--- src/adhocracy/adhocracy/websockets/test_client.py
+++ src/adhocracy/adhocracy/websockets/test_client.py
@@ -1,3 +1,65 @@
+import unittest
+
+from websocket import ABNF
+
+
+class DummyWSConnection():
+
+    def __init__(self, dummy_frame: ABNF):
+        self.dummy_frame = dummy_frame
+        self.connected = True
+        self.nothing_sent = True
+        self.close_reason = None
+        self.pong_text = None
+
+    def recv_frame(self):
+        return self.dummy_frame
+
+    def send_close(self, reason: bytes):
+        self.nothing_sent = False
+        self.close_reason = reason
+
+    def close(self):
+        self.nothing_sent = False
+        self.connected = False
+
+    def pong(self, text):
+        self.nothing_sent = False
+        self.pong_text = text
+
+
+class ClientUnitTests(unittest.TestCase):
+
+    def _make_one(self, frame: ABNF):
+        from adhocracy.websockets.client import Client
+        client = Client(None)
+        self._dummy_connection = DummyWSConnection(frame)
+        client._ws_connection = self._dummy_connection
+        return client
+
+    def test_receive_text_frame(self):
+        frame = ABNF(opcode=ABNF.OPCODE_TEXT, data='hello dear')
+        client = self._make_one(frame)
+        client._connect_and_receive_and_log_messages()
+        assert self._dummy_connection.nothing_sent is True
+        assert self._dummy_connection.connected is True
+
+    def test_receive_close_frame(self):
+        frame = ABNF(opcode=ABNF.OPCODE_CLOSE, data='time to say goodbye')
+        client = self._make_one(frame)
+        client._connect_and_receive_and_log_messages()
+        assert self._dummy_connection.nothing_sent is False
+        assert self._dummy_connection.connected is False
+        assert self._dummy_connection.close_reason == b'server triggered close'
+
+    def test_receive_ping_frame(self):
+        frame = ABNF(opcode=ABNF.OPCODE_PING, data='hello')
+        client = self._make_one(frame)
+        client._connect_and_receive_and_log_messages()
+        assert self._dummy_connection.nothing_sent is False
+        assert self._dummy_connection.pong_text == 'Hi!'
+
+
 class TestFunctionalClient:
 
     # FIXME why didn't this work with a fixture instead of setUp/tearDown?
