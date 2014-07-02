d279d2eccf1ae76968f5e9cd83b1635021e5df2a
Joscha Krutzki <joka@jokasis.de>
2014-07-02 17:48:17 +0200

    generate the frontend configuration

--- src/adhocracy/adhocracy/frontend/__init__.py
+++ src/adhocracy/adhocracy/frontend/__init__.py
@@ -1,6 +1,42 @@
-"""View configurations."""
+"""Frontend view and simple pyramid app configurations."""
+from urllib.parse import urlparse
+
+from pyramid.config import Configurator
+from pyramid.request import Request
 
 
-def includeme(config):  # pragma: no cover
+def config_view(request):
+    """Return the frontend configuration."""
+    settings = request.registry.settings or {}
+    config = {}
+    internal_ws_url = settings.get('adhocracy.ws_url', '')
+    public_ws_url = _build_ws_url(request, internal_ws_url)
+    if 'adhocracy.frontend.ws_url' in settings:
+        public_ws_url = settings.get('adhocracy.frontend.ws_url')
+    config['ws_url'] = public_ws_url
+    config['template_path'] = settings.get('adhocracy.frontend.template_path',
+                                           '/frontend_static/templates')
+    config['root_path'] = '/' + settings.get('adhocracy.root', 'adhocracy')
+    return config
+
+
+def _build_ws_url(request: Request, url: str) -> str:
+        url_parsed = urlparse(url)
+        ws_domain = request.domain
+        ws_port = url_parsed.port or '80'
+        ws_scheme = 'wss' if url_parsed.scheme == 'https' else 'ws'
+        return '{}://{}:{}'.format(ws_scheme, ws_domain, ws_port)
+
+
+def includeme(config):
     """Run pyramid config."""
     config.add_static_view('frontend_static', 'adhocracy.frontend:static')
+    config.add_route('config_json', 'frontend_config.json')
+    config.add_view(config_view, route_name='config_json', renderer='json')
+
+
+def main(global_config, **settings):
+    """ Return a Pyramid WSGI application to serve the frontend application."""
+    config = Configurator(settings=settings)
+    includeme(config)
+    return config.make_wsgi_app()
