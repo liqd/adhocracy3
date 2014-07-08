147e2026f3519710dc94b9d65056304b0746ff40
Matthias Fischmann <mf@zerobuzz.net>
2014-07-08 18:35:20 +0200

    test case: example for acceptance tests on embeddee

--- tests/embed/test_embed.py
+++ tests/embed/test_embed.py
@@ -6,9 +6,15 @@ class Test:
     def test_etc_hosts(self, browser_embedder_root):
         assert True
 
-    # test acceptance test after zooming in on iframe.
+    # run acceptance test after zooming in on iframe.  (this is mostly
+    # interesting as an example on how to do acceptance tests in
+    # general; we may want to re-factor /tests/acceptance so that all
+    # the code there can be run both in the non-embed case and in any
+    # one of many embedding contexts.)
     def test_acceptance_zooming(self, browser_embedder_root):
-        assert True
+        with browser_embedder_root.get_iframe('adhocracy_iframe') as iframe:
+            element = iframe.find_by_css('.login [name="name"]').first
+            assert element != None
 
     # resize messages from iframe to embedder window.
     def test_resize_message(self, browser_embedder_root):
