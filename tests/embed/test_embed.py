b2ca5e738ef51de1fbcc8d29fb67c0211eb35cef
Matthias Fischmann <mf@zerobuzz.net>
2014-07-08 18:26:06 +0200

    Gardening

--- tests/embed/test_embed.py
+++ tests/embed/test_embed.py
@@ -3,19 +3,13 @@ class Test:
 
     # if /etc/hosts has not been prepared (see development.rst), mark
     # all tests in this class as "pending" before continuing.
-    @disable
-    def test_etc_hosts(self, browser_sample_root):
+    def test_etc_hosts(self, browser_embedder_root):
         assert True
 
     # test acceptance test after zooming in on iframe.
-    @pending
-    def test_acceptance_zooming(self, browser_sample_root):
+    def test_acceptance_zooming(self, browser_embedder_root):
         assert True
 
-    # test test resize messages from iframe to embedder window.
-    def test_resize_message(self, browser_sample_root):
+    # resize messages from iframe to embedder window.
+    def test_resize_message(self, browser_embedder_root):
         assert True
-
-
-# FIXME: disable and pending may not *really* be valid decorators.
-# which is the right one?
