2e9dc99efca248245f4073c8927f3862d17f6e1b
Joscha Krutzki <joka@jokasis.de>
2014-07-16 23:25:18 +0200

    added TokenHeaderAuthenticationPolicy

--- src/adhocracy/adhocracy/authentication/__init__.py
+++ src/adhocracy/adhocracy/authentication/__init__.py
@@ -3,10 +3,13 @@ import hashlib
 from datetime import datetime
 
 from persistent.dict import PersistentDict
+from pyramid.authentication import CallbackAuthenticationPolicy
+from pyramid.interfaces import IAuthenticationPolicy
+from pyramid.request import Request
 from zope.interface import implementer
+from zope.interface import Interface
 
 from adhocracy.interfaces import ITokenManger
-from adhocracy.interfaces import ILocation
 
 
 @implementer(ITokenManger)
@@ -27,7 +30,7 @@ class TokenMangerAnnotationStorage:
 
     annotation_key = '_tokenmanager_storage'
 
-    def __init__(self, context: ILocation, secret: str='', hashalg='sha512',
+    def __init__(self, context, secret: str='', hashalg='sha512',
                  timeout: float=None):
         self.context = context
         self.secret = secret
@@ -79,9 +82,83 @@ class TokenMangerAnnotationStorage:
             del self._token_to_user_id_date[token]
 
 
+def _get_x_user_headers(request: Request) -> tuple:
+    """Return tuple with the X-User-Path/Token values or (None, None)."""
+    return (request.headers.get('X-User-Path', None),
+            request.headers.get('X-User-Token', None))
+
+
+class CallbackToTokenMangerAdapter:
+
+    """Callback adapter for :class: `adhocracy.interfaces.ITokenManager`.
+
+    `Callback` is a function to check authentication and return the ACL groups
+    of the authenticated user, that can be passed to:
+    :class:'pyramid.authentication.CallbackAuthenticationPolicy`.
+    """
+
+    def __init__(self, tokenmanager: ITokenManger):
+        self.tokenmanager = tokenmanager
+
+    def __call__(self, user_id, request) -> list:
+        unauthenticated_user_id, token = _get_x_user_headers(request)
+        try:
+            authenticated_user_id = self.tokenmanager.get_user_id(token)
+            if authenticated_user_id == unauthenticated_user_id:
+                return authenticated_user_id
+        except KeyError:
+            pass
+
+
+@implementer(IAuthenticationPolicy)
+class TokenHeaderAuthenticationPolicy(CallbackAuthenticationPolicy):
+
+    """A :term:`authentication policy` based on the the X-User-* header.
+
+    To authenticate the client has to send http header with `X-User-Token`
+    and `X-User-Path`.
+
+    Constructor Arguments
+
+    :param tokenmanager: TokenManger to generate the callback function
+    :param secret: random string to salt the generated token.
+    :param timeout:  Maximum number of seconds which a newly create token
+                     will be considered valid.
+                     The `None` value is allowed to disable the timeout.
+    """
+
+    debug = False  # enable debug logging
+    callback = None  # callable to check
+
+    def __init__(self, tokenmanager, secret, timeout=None):
+        if tokenmanager:
+            self.callback = CallbackToTokenMangerAdapter(tokenmanager)
+            tokenmanager.secret = secret
+            tokenmanager.timeout = timeout
+        self._tokenmanager = tokenmanager
+
+    def unauthenticated_userid(self, request):
+        return _get_x_user_headers(request)[0]
+
+    def remember(self, request, user_id, **kw):
+        if self._tokenmanager:
+            token = self._tokenmanager.create_token(user_id)
+        else:
+            token = None
+            user_id = None
+        return {'X-User-Path': user_id,
+                'X-User-Token': token}
+
+    def forget(self, request):
+        if self._tokenmanager:
+            token = _get_x_user_headers(request)[0]
+            self._tokenmanager.delete_token(token)
+        return {}
+
+
 def includeme(config):
     """Register the TokenManger adapter."""
     config.registry.registerAdapter(TokenMangerAnnotationStorage,
-                                    required=(ILocation,),
+                                    required=(Interface,),
                                     provided=ITokenManger,
                                     )
