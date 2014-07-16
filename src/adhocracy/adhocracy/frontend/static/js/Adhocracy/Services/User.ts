9f90d82b9ce998ece6fc20bee1c218ecb069a124
Nicolas Dietrich <nicodietrich@gmail.com>
2014-07-16 15:56:16 +0200

    Services.User: Store user-path in localstorage
    The backend API doesn't have an endpoint, which returns the logged in
    user for a given token, so we have to store the user reference as well.

--- src/adhocracy/adhocracy/frontend/static/js/Adhocracy/Services/User.ts
+++ src/adhocracy/adhocracy/frontend/static/js/Adhocracy/Services/User.ts
@@ -21,30 +21,44 @@ export class User {
         if (_self.Modernizr.localstorage) {
             if (_self.$window.localStorage.getItem("user-token") !== null) {
                 // FIXME: check if user-token is still valid and get user data from server
-                _self.enableToken(this.$window.localStorage.getItem("user-token"));
-                _self.loggedIn = true;
+                _self.enableToken(
+                    this.$window.localStorage.getItem("user-token"),
+                    this.$window.localStorage.getItem("user-path")
+                );
             }
         }
     }
 
-    private enableToken(token : string) : void {
+    private enableToken(token : string, user_path : string) : void {
         var _self : User = this;
 
         _self.token = token;
         _self.$http.defaults.headers.common["X-User-Token"] = token;
+        _self.loggedIn = true;
+
+        return _self.adhHttp.get(user_path)
+            .then((data) => {
+                _self.data = data;
+            }, (reason) => {
+                // The user resource that was returned by the server could not be accessed.
+                // This may happen e.g. with a network disconnect
+                _self.deleteToken();
+                _self.loggedIn = false;
+                _self.$q.reject("failed to fetch user resource");
+            });
     }
 
-
-    private enableAndStoreToken(token : string) : void {
+    private storeAndEnableToken(token : string, user_path : string) : void {
         var _self : User = this;
-
-        _self.enableToken(token);
 
         if (_self.Modernizr.localstorage) {
             _self.$window.localStorage.setItem("user-token", token);
+            _self.$window.localStorage.setItem("user-path", user_path);
         } else {
             console.log("session could not be persisted");
         }
+
+        return _self.enableToken(token, user_path);
     }
 
     private deleteToken() {
@@ -76,19 +90,7 @@ export class User {
         return promise
             .then((response) => {
                 // FIXME use websockets for updates
-                _self.loggedIn = true;
-                _self.enableAndStoreToken(response.user_token);
-
-                return _self.adhHttp.get(response.user_path)
-                    .then((data) => {
-                        _self.data = data;
-                    }, (reason) => {
-                        // The user resource that was returned by the server could not be accessed.
-                        // This may happen e.g. with a network disconnect
-                        _self.deleteToken();
-                        _self.loggedIn = false;
-                        _self.$q.reject("failed to fetch user resource");
-                    });
+                return _self.storeAndEnableToken(response.user_token, response.user_path);
             }, (reason) => {
                 // FIXME server does not send details on what went wrong.
                 // This may also be an internal server error or similar.
