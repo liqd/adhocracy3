9fb630cc71373d8001ebf2af199796dbe07e8cfa
Joscha Krutzki <joka@jokasis.de>
2014-07-20 20:17:54 +0200

    Fixup

---
+++ src/adhocracy/adhocracy/catalog/__init__.py
@@ -0,0 +1,17 @@
+""" Catalog utilities."""
+from substanced import catalog
+from substanced.interfaces import IIndexingActionProcessor
+from zope.interface import Interface
+
+
+def includeme(config):
+    """Register catalog utilities."""
+    config.add_view_predicate('catalogable', catalog._CatalogablePredicate)
+    config.add_directive('add_catalog_factory', catalog.add_catalog_factory)
+    config.add_directive('add_indexview',
+                         catalog.add_indexview,
+                         action_wrap=False)
+    config.registry.registerAdapter(catalog.deferred.BasicActionProcessor,
+                                    (Interface,),
+                                    IIndexingActionProcessor)
+    config.scan('substanced.catalog')
