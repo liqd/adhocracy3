46358daf994a5d242ac392b0eb5d04ba3e589e4b
Christian Siefkes <christian@siefkes.net>
2014-07-23 18:36:38 +0200

    Define Comment & CommentVersion resource types.

---
+++ src/adhocracy_sample/adhocracy_sample/resources/comment.py
@@ -0,0 +1,39 @@
+"""Comment resource type."""
+from adhocracy.interfaces import IItemVersion
+from adhocracy.interfaces import IItem
+from adhocracy.resources import add_resource_type_to_registry
+from adhocracy.resources.itemversion import itemversion_metadata
+from adhocracy.resources.item import item_metadata
+
+from adhocracy_sample.sheets.comment import IComment
+
+
+class ICommentVersion(IItemVersion):
+
+    """A comment in a discussion."""
+
+
+commentversion_meta = itemversion_metadata._replace(
+    content_name='CommentVersion',
+    iresource=ICommentVersion,
+    extended_sheets=[IComment],
+)
+
+
+class IComment(IItem):
+
+    """Comment versions pool."""
+
+
+comment_meta = item_metadata._replace(
+    content_name='Comment',
+    iresource=IComment,
+    element_types=[IComment, ICommentVersion],
+    item_type=ICommentVersion,
+)
+
+
+def includeme(config):
+    """Add resource type to registry."""
+    add_resource_type_to_registry(comment_meta, config)
+    add_resource_type_to_registry(commentversion_meta, config)
---
+++ src/adhocracy_sample/adhocracy_sample/resources/test_comment.py
@@ -0,0 +1,49 @@
+from pyramid import testing
+
+import unittest
+
+
+class DummyFolder(testing.DummyResource):
+
+    def add(self, name, obj, **kwargs):
+        self[name] = obj
+        obj.__name__ = name
+        obj.__parent__ = self
+        obj.__oid__ = 1
+
+    def check_name(self, name):
+        if name == 'invalid':
+            raise ValueError
+        return name
+
+    def next_name(self, obj, prefix=''):
+        return prefix + '_0000000'
+
+
+class IncludemeIntegrationTest(unittest.TestCase):
+
+    def setUp(self):
+        from adhocracy.graph import Graph
+        self.config = testing.setUp()
+        self.config.include('substanced.content')
+        self.config.include('adhocracy.registry')
+        self.config.include('adhocracy_sample.resources.comment')
+        context = DummyFolder()
+        context.__graph__ = Graph(context)
+        self.context = context
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_includeme_registry_register_factories(self):
+        from adhocracy_sample.resources.comment import ICommentVersion
+        from adhocracy_sample.resources.comment import IComment
+        content_types = self.config.registry.content.factory_types
+        assert IComment.__identifier__ in content_types
+        assert ICommentVersion.__identifier__ in content_types
+
+    def test_includeme_registry_create_content(self):
+        from adhocracy_sample.resources.comment import ICommentVersion
+        res = self.config.registry.content.create(ICommentVersion.__identifier__,
+                                                  self.context)
+        assert ICommentVersion.providedBy(res)
