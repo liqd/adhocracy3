c66c0d3a524ba57e3b55f97b03e86117616ec682
Tobias Bengfort <tobias.bengfort@liqd.de>
2014-08-04 17:51:21 +0200

    implement create directive

--- src/adhocracy/adhocracy/frontend/static/js/Packages/Comment/Comment.ts
+++ src/adhocracy/adhocracy/frontend/static/js/Packages/Comment/Comment.ts
@@ -1,6 +1,8 @@
 import AdhConfig = require("../Config/Config");
 import AdhResource = require("../../Resources");
 
+import Util = require("../Util/Util");
+
 var pkgLocation = "/Comment";
 
 export interface ICommentAdapter<T extends AdhResource.Content<any>> {
@@ -21,9 +23,40 @@ export class CommentCreate {
         return {
             restrict: "E",
             templateUrl: adhConfig.pkg_path + pkgLocation + "/CommentCreate.html",
-            scope: {},
-            controller: ["$scope", "adhHttp", "adhDone", ($scope, adhHttp, adhDone) => {
+            scope: {
+                refersTo: "@"
+            },
+            controller: ["$scope", "adhHttp", ($scope, adhHttp) => {
                 $scope.errors = [];
+
+                var displayErrors = (errors) => {
+                    $scope.errors = errors;
+                    throw errors;
+                };
+
+                $scope.create = () => {
+                    var res = _self.adapter.create();
+                    _self.adapter.content(res, $scope.content);
+                    _self.adapter.refersTo(res, $scope.refersTo);
+
+                    var comment = {
+                        content_type: "adhocracy_sample.resources.comment.IComment",
+                        data: {
+                            "adhocracy.sheets.name.IName": {
+                                name: "comment"
+                            }
+                        }
+                    };
+
+                    // NOTE: If poolPath is defined like this, answers to comments are placed
+                    // inside other comment. This should be changed if we prefer to flatten
+                    // the hierarchy.
+                    var poolPath = Util.parentPath($scope.refersTo);
+                    return adhHttp.postToPool(poolPath, comment)
+                        .then(adhHttp.resolve.bind(adhHttp), displayErrors)
+                        .then((comment) => adhHttp.postNewVersion(comment.first_version_path, res))
+                        .then((x) => x, displayErrors);
+                };
             }]
         };
     }
