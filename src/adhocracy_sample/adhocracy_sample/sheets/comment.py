8ffba21d4599a858887fb9ccad2d456c3233932f
Christian Siefkes <christian@siefkes.net>
2014-08-05 19:17:10 +0200

    Add and test ICommentable for resources that can be commented upon.

--- src/adhocracy_sample/adhocracy_sample/sheets/comment.py
+++ src/adhocracy_sample/adhocracy_sample/sheets/comment.py
@@ -4,10 +4,11 @@ import colander
 from adhocracy.interfaces import ISheet
 from adhocracy.interfaces import ISheetReferenceAutoUpdateMarker
 from adhocracy.interfaces import SheetToSheet
-from adhocracy.sheets import sheet_metadata_defaults
 from adhocracy.sheets import add_sheet_to_registry
-from adhocracy.sheets.versions import IVersionable
+from adhocracy.sheets import GenericResourceSheet
+from adhocracy.schema import ListOfUniqueReferences
 from adhocracy.schema import Reference
+from adhocracy.sheets import sheet_metadata_defaults
 from adhocracy.schema import Text
 
 
@@ -16,18 +17,23 @@ class IComment(ISheet, ISheetReferenceAutoUpdateMarker):
     """Marker interface for the comment sheet."""
 
 
+class ICommentable(ISheet, ISheetReferenceAutoUpdateMarker):
+
+    """Marker interface for resources that can be commented upon."""
+
+
 class CommentRefersToReference(SheetToSheet):
 
     """Reference from comment version to the commented-on item version."""
 
     source_isheet = IComment
     source_isheet_field = 'refers_to'
-    target_isheet = IVersionable
+    target_isheet = ICommentable
 
 
 class CommentSchema(colander.MappingSchema):
 
-    """Section sheet data structure.
+    """Comment sheet data structure.
 
     `content`:  Text
     """
@@ -40,6 +46,51 @@ comment_meta = sheet_metadata_defaults._replace(isheet=IComment,
                                                 schema_class=CommentSchema)
 
 
+class CommentsReference(SheetToSheet):
+
+    """Backreference for CommentRefersToReference, not stored."""
+
+    source_isheet = ICommentable
+    source_isheet_field = 'comments'
+    target_isheet = IComment
+
+
+class CommentableSchema(colander.MappingSchema):
+
+    """Commentable sheet data structure.
+
+    `comments`: list of comments (not stored)
+    """
+
+    comments = ListOfUniqueReferences(readonly=True, reftype=CommentsReference)
+
+
+class CommentableSheet(GenericResourceSheet):
+
+    """Sheet to set/get the commentable data structure."""
+
+    isheet = ICommentable
+    schema_class = CommentableSchema
+
+    def get(self):
+        """Return appstruct."""
+        struct = super().get()
+        if self._graph:
+            struct['comments'] = self._graph.get_back_reference_sources(
+                self.context, CommentRefersToReference)
+        return struct
+
+
+commentable_meta = sheet_metadata_defaults._replace(
+    isheet=ICommentable,
+    schema_class=CommentableSchema,
+    sheet_class=CommentableSheet,
+    editable=False,
+    creatable=False,
+)
+
+
 def includeme(config):
     """Register sheets."""
     add_sheet_to_registry(comment_meta, config.registry)
+    add_sheet_to_registry(commentable_meta, config.registry)
--- src/adhocracy_sample/adhocracy_sample/sheets/test_comment.py
+++ src/adhocracy_sample/adhocracy_sample/sheets/test_comment.py
@@ -1,6 +1,7 @@
 import unittest
 
 from pyramid import testing
+from pytest import fixture
 
 from adhocracy.utils import get_sheet
 
@@ -19,3 +20,41 @@ class CommentSheetIntegrationTest(unittest.TestCase):
         context = testing.DummyResource(__provides__=IComment)
         inst = get_sheet(context, IComment)
         assert inst.meta.isheet is IComment
+
+class TestCommentableSheet:
+
+    @fixture()
+    def meta(self):
+        from adhocracy_sample.sheets.comment import commentable_meta
+        return commentable_meta
+
+    def test_create_valid(self, meta, context):
+        from zope.interface.verify import verifyObject
+        from adhocracy.interfaces import IResourceSheet
+        from adhocracy_sample.sheets.comment import ICommentable
+        from adhocracy_sample.sheets.comment import CommentableSchema
+        from adhocracy_sample.sheets.comment import CommentableSheet
+        inst = meta.sheet_class(meta, context)
+        assert isinstance(inst, CommentableSheet)
+        assert IResourceSheet.providedBy(inst)
+        assert verifyObject(IResourceSheet, inst)
+        assert inst.meta.isheet == ICommentable
+        assert inst.meta.schema_class == CommentableSchema
+
+    def test_get_empty(self, meta, context):
+        inst = meta.sheet_class(meta, context)
+        data = inst.get()
+        assert list(data['comments']) == []
+
+    def test_get_with_comments(self, meta, context, mock_graph):
+        comment = testing.DummyResource()
+        inst = meta.sheet_class(meta, context)
+        inst._graph = mock_graph
+        mock_graph.get_back_reference_sources.return_value = iter([comment])
+        data = inst.get()
+        assert list(data['comments']) == [comment]
+
+    def test_set_with_comments(self, meta, context):
+        inst = meta.sheet_class(meta, context)
+        inst.set({'comments': []})
+        assert not 'comments' in inst._data
