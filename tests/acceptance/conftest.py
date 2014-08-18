4144da518b19f1795117d86920c1d15ce78683e3
Tobias Bengfort <tobias.bengfort@liqd.de>
2014-08-18 14:40:34 +0200

    Gaerdening

--- tests/acceptance/conftest.py
+++ tests/acceptance/conftest.py
@@ -10,12 +10,14 @@ def server(server_sample):
 
 @fixture()
 def browser(browsera, server):
-    """Return  test browser instance with url=root.html."""
+    """Return test browser instance with url=root.html."""
     url = server.application_url + 'frontend_static/root.html'
     browsera.visit(url)
 
     def angular_app_loaded(browser):
-        code = 'window.hasOwnProperty("adhocracy") && window.adhocracy.hasOwnProperty("loadState") && window.adhocracy.loadState === "complete";'  # noqa
+        code = ('window.hasOwnProperty("adhocracy") && '
+                'window.adhocracy.hasOwnProperty("loadState") && '
+                'window.adhocracy.loadState === "complete";')
         return browser.evaluate_script(code)
     browsera.wait_for_condition(angular_app_loaded, 5)
 
