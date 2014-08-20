9b06cd690bdf9a8b31382d54ebcdf169fec7c92d
Matthias Fischmann <mf@zerobuzz.net>
2014-08-20 09:21:13 +0200

    Add path attributes to Resource base class; deprecate Content<any>

--- src/adhocracy/adhocracy/frontend/static/js/ResourcesBase.ts
+++ src/adhocracy/adhocracy/frontend/static/js/ResourcesBase.ts
@@ -1,6 +1,13 @@
 export class Resource {
     public data : Object;
 
+    // these path attributes may be undefined or null.
+    /* tslint:disable:variable-name */
+    public path : string;
+    public first_version_path : string;
+    public root_versions : string;
+    /* tslint:enable:variable-name */
+
     constructor(public content_type: string) {
         this.data = {};
     }
