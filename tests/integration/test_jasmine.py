4762bde38721da277ec0c85543ae2e8c7f5ea3c4
Tobias Bengfort <tobias.bengfort@liqd.de>
2014-08-25 17:29:13 +0200

    fix python formatting

--- tests/integration/test_jasmine.py
+++ tests/integration/test_jasmine.py
@@ -8,19 +8,21 @@ adhocracy_sample.
 
 from pytest import fixture
 from pytest import mark
-from adhocracy.testing import *
 
+from adhocracy.testing import Browser
+from adhocracy.testing import browser_test_helper
 from adhocracy.frontend.tests.unit.console import Parser
 from adhocracy.frontend.tests.unit.console import Formatter
 
 pytestmark = mark.jasmine
 
 
-@fixture()
+@fixture
 def browser_igtest(browser, server_sample) -> Browser:
     """Return test browser instance with url=igtest.html."""
     url = server_sample.application_url + 'frontend_static/igtest.html'
     return browser_test_helper(browser, url)
+
 
 class TestJasmine:
     def test_all(self, browser_igtest):
