0378ddd397b39b9426531d7047c6b09cd39200f4
Matthias Fischmann <mf@zerobuzz.net>
2014-08-26 17:29:04 +0200

    Add missing unit test.

--- src/adhocracy/adhocracy/frontend/static/js/Packages/Util/UtilSpec.ts
+++ src/adhocracy/adhocracy/frontend/static/js/Packages/Util/UtilSpec.ts
@@ -97,6 +97,9 @@ export var register = () => {
             it("does not support copying functions", () => {
                 expect(() => Util.deepcp(() => null)).toThrow();
             });
+            it("does supports copying undefined", () => {
+                expect(undefined).toBe(undefined);
+            });
         });
 
         describe("deepoverwrite", () => {
