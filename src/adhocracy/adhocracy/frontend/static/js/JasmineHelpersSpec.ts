1bfd9d86595374b005556fc26d0794957055b0bd
Nicolas Dietrich <nidi@mailbox.org>
2014-08-28 16:08:35 +0200

    Add jasmine toSetEqual custom matcher

---
+++ src/adhocracy/adhocracy/frontend/static/js/JasmineHelpersSpec.ts
@@ -0,0 +1,52 @@
+/// <reference path="../lib/DefinitelyTyped/jasmine/jasmine.d.ts"/>
+/// <reference path="_all.d.ts"/>
+
+import JasmineHelpers = require("./JasmineHelpers");
+
+export var register = () => {
+    describe("JasmineHelpers", () => {
+        describe("customMatchers", () => {
+            describe("toSetEqual", () => {
+
+                var array1 : Array<string>;
+                var array1a : Array<string>;
+                var array1b : Array<string>;
+                var array2 : Array<string>;
+                var array3 : Array<string>;
+                var array4 : Array<string>;
+
+                beforeEach(() => {
+
+                    jasmine.addMatchers(JasmineHelpers.customMatchers);
+
+                    array1 = ["la", "li", "lu"];
+                    array1a = ["la", "li", "lu"];
+                    array1b = ["lu", "la", "li"];
+                    array2 = ["du", "la", "li"];
+                    array3 = ["la", "li"];
+                    array4 = ["la", "li", "lu", "lo"];
+                });
+
+                it("returns true if arrays are equal", () => {
+                    (<any>expect(array1)).toSetEqual(array1a);
+                });
+
+                it("returns true if arrays are shuffled", () => {
+                    (<any>expect(array1)).toSetEqual(array1b);
+                });
+
+                it("returns false if arrays have different elements, but same length", () => {
+                    (<any>expect(array1)).not.toSetEqual(array2);
+                });
+
+                it("returns false if actual has less elements than expected", () => {
+                    (<any>expect(array1)).not.toSetEqual(array3);
+                });
+
+                it("returns false if actual has more elements than expected", () => {
+                    (<any>expect(array1)).not.toSetEqual(array4);
+                });
+            });
+        });
+    });
+};
