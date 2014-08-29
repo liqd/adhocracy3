61385a977ebdf898ab135e56f54e65ea73cc48c3
Tobias Bengfort <tobias.bengfort@liqd.de>
2014-08-29 22:29:20 +0200

    Create ResourceWidgets Package

---
+++ src/adhocracy/adhocracy/frontend/static/js/Packages/ResourceWidgets/ResourceWidgets.ts
@@ -0,0 +1,186 @@
+/**
+ * This package provides directives that represent resources.
+ *
+ * These directives provide a generic interface for creating, displaying,
+ * editing and deleting resources with minimal additional effort.
+ *
+ * Each resourceWidget takes care of its own resources.  It can communicate
+ * with other resourceWidgets py emitting events.  These events are catched
+ * by a resourceWrapper that in turn broadcasts it to all its children.
+ *
+ * The following event types exist:
+ *
+ * setMode / triggerSetMode
+ * : set the mode (display/edit) to the first parameter given with this event.
+ *
+ * cancel / triggerCancel
+ * : change from edit mode to display mode; discard all unsaved changes
+ *
+ * submit / triggerSubmit
+ * : the resourceWrapper waits for all registered resourcePromises. When
+ *   they are ready, it uses deepPost to post all updates.
+ *
+ * registerResourceDirective
+ * : is used by resourceWidgets to register a promise with the resourceWrapper
+ *   that can be resolved on submit.  Instead of unregistering, the promise
+ *   may simply be resolved with an empty list.
+ */
+
+// import _ = require("lodash");
+
+import ResourcesBase = require("../../ResourcesBase");
+
+import AdhHttp = require("../Http/Http");
+import AdhPreliminaryNames = require("../PreliminaryNames/PreliminaryNames");
+
+
+export enum Mode {display, edit};
+
+
+export var resourceWrapper = ($q : ng.IQService) => {
+    return {
+        restrict: "E",
+        link: (scope : ng.IScope) => {
+            var resourcePromises : ng.IPromise<ResourcesBase.Resource[]>[] = [];
+
+            var resetResourcePromises = (arg?) => {
+                resourcePromises = [];
+                return arg;
+            };
+
+            scope.$on("registerResourceDirective", (ev, promise) => {
+                ev.stopPropagation();
+                resourcePromises.push(promise);
+            });
+
+            scope.$on("triggerSubmit", (ev) => {
+                ev.stopPropagation();
+
+                scope.$broadcast("submit");
+                $q.all(resourcePromises)
+                    .then(resetResourcePromises)
+                    .then((resourceLists) => _.reduce(resourceLists, (a : any[], b) => a.concat(b)))
+                    .then((resources) => console.log(resources));  // FIXME do deep post
+            });
+
+            scope.$on("triggerCancel", (ev) => {
+                ev.stopPropagation();
+                scope.$broadcast("cancel");
+            });
+
+            scope.$on("triggerSetMode", (ev, mode : Mode) => {
+                ev.stopPropagation();
+                resetResourcePromises();
+                scope.$broadcast("setMode", mode);
+            });
+        }
+    };
+};
+
+
+export interface IResourceWidgetScope extends ng.IScope {
+    mode : Mode;
+    path : string;
+    edit() : void;
+    submit() : ng.IAngularEvent;
+    cancel() : ng.IAngularEvent;
+    delete() : ng.IAngularEvent;
+}
+
+export class ResourceWidget<R extends ResourcesBase.Resource> {
+    private deferred : ng.IDeferred<R[]>;
+    public templateUrl : string;
+
+    constructor(
+        public adhHttp : AdhHttp.Service<any>,
+        public adhPreliminaryNames : AdhPreliminaryNames,
+        public $q : ng.IQService
+    ) {
+        this.deferred = this.$q.defer();
+    }
+
+    public createDirective() : ng.IDirective {
+        var self : ResourceWidget<R> = this;
+
+        return {
+            restrict: "E",
+            templateUrl: self.templateUrl,
+            scope: {
+                mode: "@",
+                path: "@"
+            },
+            link: (scope : IResourceWidgetScope) => {
+                scope.mode = scope.mode || Mode.display;  // FIXME: untested
+
+                scope.$on("setMode", (ev, mode : Mode) => self.setMode(scope, mode));
+                scope.$on("triggerDelete", (ev, path : string) => self._handleDelete(scope, path));
+                scope.$on("$delete", () => self.deferred.resolve([]));
+                scope.$on("submit", () => self._provide(scope)
+                    .then((resources) => self.deferred.resolve(resources)));
+
+                scope.$on("cancel", () => {
+                    if (scope.mode === Mode.edit) {
+                        return self.update(scope).then(() => {
+                            self.setMode(scope, Mode.display);
+                        });
+                    } else {
+                        return self.$q.when();
+                    }
+                });
+
+                scope.edit = () => self.setMode(scope, Mode.edit);
+                scope.submit = () => scope.$emit("triggerSubmit");
+                scope.cancel = () => scope.$emit("triggerCancel");
+                scope.delete = () => scope.$emit("triggerDelete", scope.path);
+
+                self.update(scope);
+            }
+        };
+    }
+
+    public setMode(scope : IResourceWidgetScope, mode : Mode) : void {
+        var self : ResourceWidget<R> = this;
+
+        self.deferred.resolve([]);
+        if (mode === Mode.edit) {
+            self.deferred = self.$q.defer();
+            scope.$emit("registerResourceDirective", self.deferred.promise);
+        }
+        scope.mode = mode;
+    }
+
+    public update(scope : IResourceWidgetScope) : ng.IPromise<void> {
+        var self : ResourceWidget<R> = this;
+
+        if (self.adhPreliminaryNames.isPreliminary(scope.path)) {
+            return self.$q.when();
+        } else {
+            return self.adhHttp.get(scope.path)
+                .then((resource : R) => self._update(scope, resource));
+        }
+    }
+
+    /**
+     * Handle delete events from children.
+     */
+    public _handleDelete(scope : IResourceWidgetScope, path : string) : ng.IPromise<void> {
+        throw "not implemented";
+    }
+
+    /**
+     * Update scope from resource.
+     */
+    public _update(scope : IResourceWidgetScope, resource : R) : ng.IPromise<void> {
+        throw "not implemented";
+    }
+
+    /**
+     * Create resource(s) from scope.
+     *
+     * If the widget represents a versionable and scope.path
+     * is preliminary, this function must also provide an item.
+     */
+    public _provide(scope : IResourceWidgetScope) : ng.IPromise<R[]> {
+        throw "not implemented";
+    }
+}
---
+++ src/adhocracy/adhocracy/frontend/static/js/Packages/ResourceWidgets/ResourceWidgetsSpec.ts
@@ -0,0 +1,333 @@
+/// <reference path="../../../lib/DefinitelyTyped/jasmine/jasmine.d.ts"/>
+
+import q = require("q");
+
+import AdhResourceWidgets = require("./ResourceWidgets");
+
+
+var getArgsByFirst = (spy, needle) => {
+    var haystack = spy.calls.allArgs();
+    return haystack.filter((args) => args[0] === needle);
+};
+
+export var register = () => {
+    describe("ResourceWidgets", () => {
+        describe("resourceWrapper", () => {
+            var directive;
+
+            beforeEach(() => {
+                directive = AdhResourceWidgets.resourceWrapper(q);
+            });
+
+            describe("link", () => {
+                var scopeMock;
+
+                beforeEach(() => {
+                    scopeMock = jasmine.createSpyObj("scope", ["$on", "$broadcast"]);
+                    directive.link(scopeMock);
+                });
+
+                it("does not pollute the scope", () => {
+                    expect(scopeMock).toEqual({
+                        $on: jasmine.any(Function),
+                        $broadcast: jasmine.any(Function)
+                    });
+                });
+
+                describe("on registerResourceDirective", () => {
+                    var fn;
+                    var ev;
+
+                    beforeEach(() => {
+                        fn = getArgsByFirst(scopeMock.$on, "registerResourceDirective")[0][1];
+                        ev = jasmine.createSpyObj("event", ["stopPropagation"]);
+                    });
+
+                    it("stops event propagation", () => {
+                        fn(ev, "promise");
+                        expect(ev.stopPropagation).toHaveBeenCalled();
+                    });
+
+                    xit("stores the passed promise", () => {
+                        throw "untestable";
+                    });
+                });
+
+                describe("on triggerSubmit", () => {
+                    var ev;
+
+                    beforeEach(() => {
+                        var fn = getArgsByFirst(scopeMock.$on, "triggerSubmit")[0][1];
+                        ev = jasmine.createSpyObj("event", ["stopPropagation"]);
+                        fn(ev);
+                    });
+
+                    it("stops event propagation", () => {
+                        expect(ev.stopPropagation).toHaveBeenCalled();
+                    });
+
+                    it("broadcasts a 'submit' event", () => {
+                        expect(scopeMock.$broadcast).toHaveBeenCalledWith("submit");
+                    });
+                });
+
+                describe("on triggerCancel", () => {
+                    var ev;
+
+                    beforeEach(() => {
+                        var fn = getArgsByFirst(scopeMock.$on, "triggerCancel")[0][1];
+                        ev = jasmine.createSpyObj("event", ["stopPropagation"]);
+                        fn(ev);
+                    });
+
+                    it("stops event propagation", () => {
+                        expect(ev.stopPropagation).toHaveBeenCalled();
+                    });
+
+                    it("broadcasts a 'cancel' event", () => {
+                        expect(scopeMock.$broadcast).toHaveBeenCalledWith("cancel");
+                    });
+                });
+
+                describe("on triggerSetMode", () => {
+                    var ev;
+
+                    beforeEach(() => {
+                        var fn = getArgsByFirst(scopeMock.$on, "triggerSetMode")[0][1];
+                        ev = jasmine.createSpyObj("event", ["stopPropagation"]);
+                        fn(ev, "mode");
+                    });
+
+                    it("stops event propagation", () => {
+                        expect(ev.stopPropagation).toHaveBeenCalled();
+                    });
+
+                    it("broadcasts a 'setMode' event with mode as first parameter", () => {
+                        expect(scopeMock.$broadcast).toHaveBeenCalledWith("setMode", "mode");
+                    });
+                });
+            });
+        });
+
+        describe("ResourceWidget", () => {
+            var adhHttpMock;
+            var adhPreliminaryNamesMock;
+            var resourceWidget;
+
+            beforeEach(() => {
+                adhHttpMock = jasmine.createSpyObj("adhHttp", ["get"]);
+                adhHttpMock.get.and.returnValue(q.when());
+
+                adhPreliminaryNamesMock = jasmine.createSpyObj("adhPreliminaryNames", ["isPreliminary"]);
+                adhPreliminaryNamesMock.isPreliminary.and.returnValue(false);
+
+                resourceWidget = new AdhResourceWidgets.ResourceWidget(adhHttpMock, adhPreliminaryNamesMock, q);
+            });
+
+            describe("constructor", () => {
+                it("sets this.deferred", () => {
+                    expect(resourceWidget.deferred).toBeDefined();
+                });
+            });
+
+            describe("createDirective", () => {
+                var directive;
+
+                beforeEach(() => {
+                    resourceWidget.templateUrl = "templateUrl";
+                    directive = resourceWidget.createDirective();
+                });
+
+                it("sets the templateUrl", () => {
+                    expect(directive.templateUrl).toBe("templateUrl");
+                });
+
+                describe("link", () => {
+                    var scopeMock;
+
+                    beforeEach(() => {
+                        scopeMock = jasmine.createSpyObj("scope", ["$on", "$emit", "$broadcast"]);
+                        resourceWidget.update = jasmine.createSpy("update").and.returnValue(q.when());
+                        resourceWidget.setMode = jasmine.createSpy("setMode").and.returnValue(q.when());
+                        resourceWidget._handleDelete = jasmine.createSpy("_handleDelete").and.returnValue(q.when());
+                        resourceWidget._provide = jasmine.createSpy("_provide").and.returnValue(q.when());
+                        directive.link(scopeMock);
+                    });
+
+                    it("calls update", () => {
+                        expect(resourceWidget.update).toHaveBeenCalled();
+                    });
+
+                    describe("on $delete", () => {
+                        it("resolves the promise that is registered with resourceWrapper with an empty list", () => {
+                            var fn = getArgsByFirst(scopeMock.$on, "$delete")[0][1];
+                            resourceWidget.deferred.resolve = jasmine.createSpy("resolve");
+                            fn("event");
+                            expect(resourceWidget.deferred.resolve).toHaveBeenCalledWith([]);
+                        });
+                    });
+
+                    describe("on submit", () => {
+                        it("calls provide and resolves the promise with the result", (done) => {
+                            var fn = getArgsByFirst(scopeMock.$on, "submit")[0][1];
+                            resourceWidget._provide.and.returnValue(q.when("provided"));
+
+                            fn().then(() => {
+                                expect(resourceWidget._provide).toHaveBeenCalled();
+
+                                resourceWidget.deferred.promise.then((promised) => {
+                                    expect(promised).toBe("provided");
+                                    done();
+                                });
+                            });
+                        });
+                    });
+
+                    describe("on cancel", () => {
+                        var fn;
+
+                        beforeEach(() => {
+                            fn = getArgsByFirst(scopeMock.$on, "cancel")[0][1];
+                        });
+
+                        it("resets scope and sets mode to display if scope.mode is edit", (done) => {
+                            scopeMock.mode = AdhResourceWidgets.Mode.edit;
+                            fn().then(() => {
+                                expect(resourceWidget.update.calls.count()).toBe(2);
+                                expect(resourceWidget.setMode).toHaveBeenCalled();
+                                done();
+                            });
+                        });
+
+                        it("does not call update and does not change mode if mode is not edit", (done) => {
+                            scopeMock.mode = "foo";
+                            fn().then(() => {
+                                expect(resourceWidget.update.calls.count()).toBe(1);
+                                expect(resourceWidget.setMode).not.toHaveBeenCalled();
+                                done();
+                            });
+                        });
+                    });
+
+                    describe("on setMode", () => {
+                        it("calls setMode with first parameter", () => {
+                            var fn = getArgsByFirst(scopeMock.$on, "setMode")[0][1];
+                            fn("event", "mode");
+                            expect(resourceWidget.setMode).toHaveBeenCalledWith(scopeMock, "mode");
+                        });
+                    });
+
+                    describe("on triggerDelete", () => {
+                        it("calls _handleDelete with first parameter", () => {
+                            var fn = getArgsByFirst(scopeMock.$on, "triggerDelete")[0][1];
+                            fn("event", "path");
+                            expect(resourceWidget._handleDelete).toHaveBeenCalledWith(scopeMock, "path");
+                        });
+                    });
+
+                    describe("edit", () => {
+                        it("uses setMode to switch to edit mode", () => {
+                            scopeMock.edit();
+                            expect(resourceWidget.setMode).toHaveBeenCalledWith(scopeMock, AdhResourceWidgets.Mode.edit);
+                        });
+                    });
+
+                    describe("submit", () => {
+                        it("emits a 'triggerSubmit' event", () => {
+                            scopeMock.submit();
+                            expect(scopeMock.$emit).toHaveBeenCalledWith("triggerSubmit");
+                        });
+                    });
+
+                    describe("cancel", () => {
+                        it("emits a 'triggerCancel' event", () => {
+                            scopeMock.cancel();
+                            expect(scopeMock.$emit).toHaveBeenCalledWith("triggerCancel");
+                        });
+                    });
+
+                    describe("delete", () => {
+                        it("emits a 'triggerDelete' event with scope.path as first parameter", () => {
+                            scopeMock.path = "/some/path";
+                            scopeMock.delete();
+                            expect(scopeMock.$emit).toHaveBeenCalledWith("triggerDelete", "/some/path");
+                        });
+                    });
+                });
+            });
+
+            describe("setMode", () => {
+                var scopeMock;
+
+                beforeEach(() => {
+                    scopeMock = jasmine.createSpyObj("scope", ["$emit"]);
+                });
+
+                it("sets scope.mode", () => {
+                    resourceWidget.setMode(scopeMock, "test");
+                    expect((<any>scopeMock).mode).toBe("test");
+                });
+
+                it("resolves the promise that is registered with resourceWrapper with an empty list", () => {
+                    resourceWidget.deferred.resolve = jasmine.createSpy("resolve");
+                    resourceWidget.setMode(scopeMock, AdhResourceWidgets.Mode.display);
+                    expect(resourceWidget.deferred.resolve).toHaveBeenCalledWith([]);
+                });
+
+                it("creates a new promise and registers it with resourceWrapper using a 'registerResourceDirective' event", () => {
+                    spyOn(q, "defer").and.callThrough();
+                    resourceWidget.setMode(scopeMock, AdhResourceWidgets.Mode.edit);
+                    expect(q.defer).toHaveBeenCalled();
+                    expect(scopeMock.$emit).toHaveBeenCalledWith("registerResourceDirective", jasmine.any(Object));
+                });
+            });
+
+            describe("update", () => {
+                var scopeMock;
+
+                beforeEach(() => {
+                    scopeMock = {};
+                    resourceWidget._update = jasmine.createSpy("_update");
+                });
+
+                it("does not call adhHttp.get or _update when scope.path is preliminary", (done) => {
+                    adhPreliminaryNamesMock.isPreliminary.and.returnValue(true);
+                    resourceWidget.update(scopeMock).then(() => {
+                        expect(adhHttpMock.get).not.toHaveBeenCalled();
+                        expect(resourceWidget._update).not.toHaveBeenCalled();
+                        done();
+                    });
+                });
+
+                it("does get scope.path from server and feed it to _update", (done) => {
+                    adhPreliminaryNamesMock.isPreliminary.and.returnValue(false);
+                    scopeMock.path = "/some/path";
+                    resourceWidget.update(scopeMock).then(() => {
+                        expect(adhHttpMock.get).toHaveBeenCalledWith("/some/path");
+                        expect(resourceWidget._update).toHaveBeenCalled();
+
+                        done();
+                    });
+                });
+            });
+
+            describe("_handleDelete", () => {
+                it("raises an 'not implemented' exception", () => {
+                    expect(() => resourceWidget._handleDelete({}, "path")).toThrow();
+                });
+            });
+
+            describe("_update", () => {
+                it("raises an 'not implemented' exception", () => {
+                    expect(() => resourceWidget._update({}, "path")).toThrow();
+                });
+            });
+
+            describe("_provide", () => {
+                it("raises an 'not implemented' exception", () => {
+                    expect(() => resourceWidget._provide({}, "path")).toThrow();
+                });
+            });
+        });
+    });
+};
