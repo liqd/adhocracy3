45a2473255825f43b0e2a1b626b3a2a07a2ef574
Matthias Fischmann <mf@zerobuzz.net>
2014-08-29 09:08:23 +0200

    mkResources: generate Resources_.registry.

--- src/adhocracy/adhocracy/frontend/static/js/mkResources.ts
+++ src/adhocracy/adhocracy/frontend/static/js/mkResources.ts
@@ -186,6 +186,8 @@ compileAll = (metaApi : MetaApi.IMetaApi, outPath : string) : void => {
         var rootModule = "";
         var relativeRoot = outPath + "/Resources_/";
         var imports : string[] = [];
+        (() => {
+
         for (var modulePath in modules) {
             if (modules.hasOwnProperty(modulePath)) {
                 imports.push(mkImportStatement(modulePath, relativeRoot, metaApi));
@@ -193,6 +195,22 @@ compileAll = (metaApi : MetaApi.IMetaApi, outPath : string) : void => {
         }
         imports.sort();
         rootModule += Util.intercalate(imports, "") + "\n";
+
+        })();
+
+        (() => {
+
+        var dictEntries : string[] = [];
+        for (var modulePath in metaApi.resources) {
+            if (metaApi.resources.hasOwnProperty(modulePath)) {
+                dictEntries.push("    \"" + modulePath + "\": " + mkModuleName(modulePath, metaApi));
+            }
+        }
+        dictEntries.sort();
+        rootModule += "export var registry = {\n" + Util.intercalate(dictEntries, ",\n") + "\n};\n";
+
+        })();
+
         var absfp = outPath + "/Resources_.ts";
         fs.writeFileSync(absfp, headerFooter(relativeRoot, rootModule));
     })();
