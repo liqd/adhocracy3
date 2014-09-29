d10a5cbedba5539681a26d6ee5106608484de1ef
Christian Siefkes <christian@siefkes.net>
2014-09-29 11:53:56 +0200

    Fixed import/include paths.

--- src/adhocracy_core/adhocracy_core/messaging/test_init.py
+++ src/adhocracy_core/adhocracy_core/messaging/test_init.py
@@ -4,7 +4,7 @@ from pytest import fixture
 
 @fixture
 def integration(config):
-    config.include('adhocracy.registry')
+    config.include('adhocracy_core.registry')
     config.include('pyramid_mailer.testing')
 
 
@@ -12,7 +12,7 @@ class TestSendMail():
 
     @mark.usefixtures('integration')
     def test_send_mail_successfully(self, registry):
-        from adhocracy.messaging import send_mail, _get_mailer
+        from adhocracy_core.messaging import send_mail, _get_mailer
         mailer = _get_mailer(registry)
         assert len(mailer.outbox) == 0
         send_mail(registry=registry,
