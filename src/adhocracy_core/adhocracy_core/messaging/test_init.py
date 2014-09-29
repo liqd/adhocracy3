a2de582c075cf743ab7a775b60f4860e42988c67
Christian Siefkes <christian@siefkes.net>
2014-09-29 12:35:51 +0200

    More tests for messaging module

--- src/adhocracy_core/adhocracy_core/messaging/test_init.py
+++ src/adhocracy_core/adhocracy_core/messaging/test_init.py
@@ -1,5 +1,6 @@
-from pytest import mark
 from pytest import fixture
+from pytest import mark
+from pytest import raises
 
 
 @fixture
@@ -22,4 +23,45 @@ class TestSendMail():
               body='Blah!',
               html='<p>Bäh!</p>')
         assert len(mailer.outbox) == 1
-        assert 'Test mail' in str(mailer.outbox[0].to_message())
+        msg = str(mailer.outbox[0].to_message())
+        assert 'Test mail' in msg
+        assert 'Blah' in msg
+        assert 'Content-Type: text/html' in msg
+
+    @mark.usefixtures('integration')
+    def test_send_mail_successfully_no_html(self, registry):
+        from adhocracy_core.messaging import send_mail, _get_mailer
+        mailer = _get_mailer(registry)
+        assert len(mailer.outbox) == 0
+        send_mail(registry=registry,
+              subject='Test mail',
+              recipients=['user@example.org'],
+              sender='admin@example.com',
+              body='Blah!')
+        assert len(mailer.outbox) == 1
+        msg = str(mailer.outbox[0].to_message())
+        assert 'Content-Type: text/html' not in msg
+
+    @mark.usefixtures('integration')
+    def test_send_mail_successfully_neither_body_nor_html(self, registry):
+        from adhocracy_core.messaging import send_mail, _get_mailer
+        mailer = _get_mailer(registry)
+        assert len(mailer.outbox) == 0
+        with raises(ValueError):
+            send_mail(registry=registry,
+                  subject='Test mail',
+                  recipients=['user@example.org'],
+                  sender='admin@example.com')
+
+    @mark.usefixtures('integration')
+    def test_send_mail_no_recipient(self, registry):
+        from adhocracy_core.messaging import send_mail, _get_mailer
+        mailer = _get_mailer(registry)
+        assert len(mailer.outbox) == 0
+        with raises(ValueError):
+            send_mail(registry=registry,
+                  subject='Test mail',
+                  recipients=None,
+                  sender='admin@example.com',
+                  body='Blah!',
+                  html='<p>Bäh!</p>')
