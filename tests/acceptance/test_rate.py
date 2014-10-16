73ad882d6d86df777cb6d54709b897a612506617
Matthias Fischmann <mf@zerobuzz.net>
2014-10-16 15:50:55 +0200

    Fix

--- tests/acceptance/test_rate.py
+++ tests/acceptance/test_rate.py
@@ -6,29 +6,45 @@ from .shared import wait
 from .shared import get_column_listing
 from .shared import get_list_element
 from .shared import get_listing_create_form
-from .shared import login_annotator
-from .test_proposal import proposal
-from .test_comment import comment
-from .test_comment import browser
+from .shared import login_god
+from .test_comment import create_comment
 
+class TestRate:
 
-def test_upvote(browser, comment):
-    rateable = get_column_listing(browser, 'content2').find_by_css('.comment')
-    pro_button = rateable.find_by_css('.rate-pro')
-    pro_button.click()
-    total = rateable.find_by_css('.rate-difference')
-    assert total[0].text is '+1'
+    def test_create(self, browser):
+        login_god(browser)
+        comment = create_comment(browser, 'comment1')
+        assert comment is not None
 
-def test_downvote(browser, comment):
-    rateable = get_column_listing(browser, 'content2').find_by_css('.comment')
-    pro_button = rateable.find_by_css('.rate-contra')
-    pro_button.click()
-    total = rateable.find_by_css('.rate-difference')
-    assert total[0].text is '-1'
+    def test_upvote(self, browser):
+        rateable = get_column_listing(browser, 'content2').find_by_css('.comment')
+        pro_button = rateable.find_by_css('.rate-pro')
+        pro_button.click()
+        def check_result():
+            total = rateable.find_by_css('.rate-difference')
+            return total[0].text == '+1'
+        assert wait(check_result)
 
-def test_neutralvote(browser, comment):
-    rateable = get_column_listing(browser, 'content2').find_by_css('.comment')
-    pro_button = rateable.find_by_css('.rate-neutral')
-    pro_button.click()
-    total = rateable.find_by_css('.rate-difference')
-    assert total[0].text is '0'
+    def test_downvote(self, browser):
+        rateable = get_column_listing(browser, 'content2').find_by_css('.comment')
+        pro_button = rateable.find_by_css('.rate-contra')
+        pro_button.click()
+        def check_result():
+            total = rateable.find_by_css('.rate-difference')
+            return total[0].text == '-1'
+        assert wait(check_result)
+
+    def test_neutralvote(self, browser):
+        rateable = get_column_listing(browser, 'content2').find_by_css('.comment')
+        pro_button = rateable.find_by_css('.rate-neutral')
+        pro_button.click()
+        def check_result():
+            total = rateable.find_by_css('.rate-difference')
+            return total[0].text == '0'
+        assert wait(check_result)
+
+    # FIXME: test detail list.
+
+    # FIXME: test replacing god user with one that is allowed to rate, but not much more.
+
+    # FIXME: test manu users and more interesting totals.
