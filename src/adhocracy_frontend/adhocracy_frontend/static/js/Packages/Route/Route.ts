4961dac93eef791ac599c18c96327654786df801
Tobias Bengfort <tobias.bengfort@liqd.de>
2014-10-20 16:36:21 +0200

    Use some actual content types

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Route/Route.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Route/Route.ts
@@ -2,6 +2,11 @@ import AdhHttp = require("../Http/Http");
 import AdhConfig = require("../Config/Config");
 import AdhTopLevelState = require("../TopLevelState/TopLevelState");
 
+import RIBasicPool = require("Resources_/adhocracy_core/resources/pool/IBasicPool");
+import RIMercatorProposal = require("Resources_/adhocracy_core/resources/mercator/IMercatorProposal");
+import RIUser = require("Resources_/adhocracy_core/resources/principal/IUser");
+import RIUsersService = require("Resources_/adhocracy_core/resources/principal/IUsersService");
+
 var notFoundTemplate = "<h1>404 - not Found</h1>";
 
 
@@ -21,8 +26,17 @@ export var resourceRouter = (
 
     adhHttp.get(resourceUrl).then((resource) => {
         switch (resource.content_type) {
-            case "adhocracy_core.resources.sample_proposal.IProposal":
+            case RIBasicPool.content_type:
                 $scope.template = "<adh-document-workbench></adh-document-workbench>";
+                break;
+            case RIMercatorProposal.content_type:
+                $scope.template = "<adh-document-workbench></adh-document-workbench>";
+                break;
+            case RIUser.content_type:
+                $scope.template = "<h1>User</h1>";
+                break;
+            case RIUsersService.content_type:
+                $scope.template = "<h1>User List</h1>";
                 break;
             default:
                 $scope.template = notFoundTemplate;
