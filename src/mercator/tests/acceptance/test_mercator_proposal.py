b01205e2bff63f3f01e52b1620c77a43cd634148
Nicolas Dietrich <nidi@mailbox.org>
2014-11-01 01:24:03 +0100

    Fix mercator acceptance tests

--- src/mercator/tests/acceptance/test_mercator_proposal.py
+++ src/mercator/tests/acceptance/test_mercator_proposal.py
@@ -36,25 +36,25 @@ class TestMercatorForm:
         assert len(elements) == 1
 
     def test_field_extra_exprerience_is_optional(self, browser):
-        browser.find_by_name('extra-experience').first.fill('')
+        browser.find_by_name('experience').first.fill('')
         assert can_submit(browser)
 
     def test_field_status_text_is_not_shown_if_non_custom_status(self, browser):
-        browser.find_by_name('basic-organisation-status').first.check()
-        status_text = 'basic-organisation-statustext'
+        browser.find_by_name('organization_info-status_enum').first.check()
+        status_text = 'organization_info-status_other'
         assert browser.is_element_not_present_by_name(status_text)
         assert can_submit(browser)
 
     def test_field_status_text_is_required_if_custom_status(self, browser):
-        browser.find_by_name('basic-organisation-status').last.check()
-        status_text = 'basic-organisation-statustext'
+        browser.find_by_name('organization_info-status_enum').last.check()
+        status_text = 'organization_info-status_other'
         assert browser.is_element_present_by_name(status_text)
         assert not can_submit(browser)
         browser.find_by_name(status_text).first.fill('statustext')
         assert can_submit(browser)
 
     def test_field_name_is_required(self, browser):
-        browser.find_by_name('basic-user-name').first.fill('')
+        browser.find_by_name('user_info-first_name').first.fill('')
         assert not can_submit(browser)
 
 
@@ -65,41 +65,40 @@ def can_submit(browser):
 
 
 def fill_all(browser):
-    browser.find_by_name('basic-user-name').first.fill('name')
-    browser.find_by_name('basic-user-lastname').first.fill('lastname')
-    browser.find_by_name('basic-user-email').first.fill(
-        'name.lastname@domain.com')
+    browser.find_by_name('user_info-first_name').first.fill('name')
+    browser.find_by_name('user_info-last_name').first.fill('lastname')
 
-    browser.find_by_name('basic-organisation-name').first.fill(
+    # NOTE: Due to angular magic used in ng-options, the value which is
+    # stored in the respective ng-model (e.g. 'DE') isn't reflected in the
+    # DOM, and an index is used instead.
+    browser.select('user_info-country', '1')
+
+    browser.find_by_name('organization_info-name').first.fill(
         'organisation name')
-    browser.find_by_name('basic-organisation-email').first.fill(
-        'info@domain.com')
-    browser.find_by_name('basic-organisation-address').first.fill('address')
-    browser.find_by_name('basic-organisation-postcode').first.fill('12345')
-    browser.find_by_name('basic-organisation-city').first.fill('city')
-    browser.select('basic-organisation-country', 'DE')
-    browser.find_by_name('basic-organisation-status').first.check()
-    browser.find_by_name('basic-organisation-description').first.fill('about')
-    browser.find_by_name('basic-organisation-size').first.check()
-    browser.find_by_name('basic-organisation-cooperation').last.check()
+    browser.find_by_name('organization_info-website').first.fill(
+        'http://example.com')
+    browser.find_by_name('organization_info-date_of_foreseen_registration')\
+        .first.fill('02/2015')
+    browser.select('organization_info-country', '2')
+    browser.find_by_name('organization_info-status_enum').first.check()
 
     browser.find_by_name('introduction-title').first.fill('title')
     browser.find_by_name('introduction-teaser').first.fill('teaser')
 
-    browser.find_by_name('detail-description').first.fill('description')
-    browser.find_by_name('detail-location-city').first.check()
-    browser.find_by_name('detail-location-ruhr').first.check()
-    browser.find_by_name('detail-story').first.fill('story')
+    browser.find_by_name('details-description').first.fill('description')
+    browser.find_by_name('details-location_is_specific').first.check()
+    browser.find_by_name('details-location_specific_1').first.fill('Bonn')
+    browser.find_by_name('details-location_is_linked_to_ruhr').first.check()
+    browser.find_by_name('story').first.fill('story')
 
-    browser.find_by_name('motivation-outcome').first.fill('success')
-    browser.find_by_name('motivation-steps').first.fill('plan')
-    browser.find_by_name('motivation-value').first.fill('relevance')
-    browser.find_by_name('motivation-partners').first.fill('partners')
+    browser.find_by_name('outcome').first.fill('success')
+    browser.find_by_name('steps').first.fill('plan')
+    browser.find_by_name('value').first.fill('relevance')
+    browser.find_by_name('partners').first.fill('partners')
 
     browser.find_by_name('finance-budget').first.fill(1000)
-    browser.find_by_name('finance-funding').first.fill(1000)
+    browser.find_by_name('finance-requested_funding').first.fill(1000)
     browser.find_by_name('finance-granted').first.check()
 
-    browser.find_by_name('extra-experience').first.fill('experience')
-    browser.find_by_name('extra-experience').first.fill('experience')
-    browser.find_by_name('extra-hear-colleague').first.check()
+    browser.find_by_name('experience').first.fill('experience')
+    browser.find_by_name('heard_from-colleague').first.check()
