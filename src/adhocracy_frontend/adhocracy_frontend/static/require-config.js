75567a2906395e4fa7bc7258223a17503fb8373e
Caroline Clifford <me@carolineclifford.com>
2014-11-18 12:27:29 +0100

    Bower now uses the sticky plugin fork from my repo

--- src/adhocracy_frontend/adhocracy_frontend/static/require-config.js
+++ src/adhocracy_frontend/adhocracy_frontend/static/require-config.js
@@ -22,7 +22,7 @@ require.config({
         q: "../lib/q/q",
         modernizr: "../lib2/modernizr/modernizr-2.8.3.min",
         moment: "../lib/moment/min/moment-with-locales",
-        sticky: "../lib/sticky-kit/jquery.sticky-kit_updated"
+        sticky: "../lib/sticky-kit/jquery.sticky-kit"
     },
     shim: {
         jquery: {
