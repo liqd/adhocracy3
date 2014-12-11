6930498b472c2b47b2e01e9a6bfd636879cc33af
mkind <github@mknd.net>
2014-12-11 21:02:29 +0100

    adding test to verify non-editing

--- src/mercator/tests/acceptance/test_mercator_proposal_edit.py
+++ src/mercator/tests/acceptance/test_mercator_proposal_edit.py
@@ -4,14 +4,27 @@ from webtest import TestApp
 from pytest import mark
 
 from mercator.tests.fixtures.fixturesMercatorProposals1 import create_proposals
+from adhocracy_frontend.tests.acceptance.shared import get_random_string
 from adhocracy_frontend.tests.acceptance.shared import api_login_god
 from adhocracy_frontend.tests.acceptance.shared import login_god
+from adhocracy_frontend.tests.fixtures.users import create_user
+from adhocracy_frontend.tests.acceptance.shared import login
 from adhocracy_frontend.tests.acceptance.shared import wait
 
 
 @fixture(scope='module')
 def proposals():
     return create_proposals(user_token=api_login_god(), n=1)
+
+
+@fixture(scope='module')
+def user():
+    name = get_random_string(n=5)
+    password = 'password'
+    create_user(name, password)
+    import time
+    time.sleep(1)
+    return name, password
 
 
 class TestMercatorForm:
@@ -30,7 +43,6 @@ class TestMercatorForm:
     @mark.xfail
     def test_resubmitting_proposal(self, browser, proposal):
         login_god(browser)
-
         wait(lambda: browser.find_link_by_text("edit"))
 
         browser.find_link_by_text("edit").first.click()
@@ -39,6 +51,10 @@ class TestMercatorForm:
         browser.find_by_name('accept-disclaimer').first.check()
         browser.find_by_css('input[type="submit"]').first.click()
         assert not internal_error(browser)
+
+    def test_editing_foreign_proposals(self, browser, user, proposal):
+        login(browser, user[0], user[1])
+        assert not browser.is_text_present("edit", wait_time=1)
 
 
 def internal_error(browser):
