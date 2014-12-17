796c1af8511b88062fe42afc20904242c704f0d8
mkind <github@mknd.net>
2014-12-17 14:00:59 +0100

    Proposal selection is no fixture anymore. After logging in, the first proposal in the proposal list is selected and its details are loaded.

--- src/mercator/tests/acceptance/test_mercator_proposal_edit.py
+++ src/mercator/tests/acceptance/test_mercator_proposal_edit.py
@@ -34,15 +34,11 @@ class TestMercatorForm:
         assert browser.is_text_present("supporters", wait_time=1)
         return browser
 
-    @fixture(scope='class')
-    def proposal(self, browser):
-       proposal_list = browser.find_by_css(".moving-column-body").\
-                                first.find_by_tag("ol").first
-       proposal_list.find_by_css("h3 a").first.click()
-
     @mark.xfail
-    def test_resubmitting_proposal(self, browser, proposal):
+    def test_resubmitting_proposal(self, browser):
         login_god(browser)
+        select_proposal(browser)
+
         assert wait(lambda: browser.find_link_by_text("edit"))
 
         browser.find_link_by_text("edit").first.click()
@@ -52,10 +48,28 @@ class TestMercatorForm:
         browser.find_by_css('input[type="submit"]').first.click()
         assert not internal_error(browser)
 
-    def test_editing_foreign_proposals(self, browser, user, proposal):
+    def test_editing_foreign_proposals(self, browser, user):
         login(browser, user[0], user[1])
+        select_proposal(browser)
+
         assert not browser.is_text_present("edit", wait_time=1)
 
 
 def internal_error(browser):
     return browser.is_text_present("Internal Error", wait_time=10)
+
+
+def select_proposal(browser):
+    """First click on proposal title in proposal list column and wait until
+    proposal is properly loaded in content column. In order to verify proper
+    proposal loading, check for the detail's commentary link to contain the
+    correct proposal title."""
+    proposal_list = browser.find_by_css(".moving-column-body").\
+                            first.find_by_tag("ol").first
+    proposal_title = proposal_list.find_by_css("h3 a").first
+    assert wait(lambda: proposal_title.html)
+    proposal_title.click()
+
+    link = browser.find_by_css(".moving-column-content").first.\
+                   find_by_css(".mercator-proposal-detail-cover h1 a")
+    assert wait(lambda: proposal_title.html in link["href"])
