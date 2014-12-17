743cea175386ecb0d0a161c68aea46621c84afa6
mkind <github@mknd.net>
2014-12-17 14:40:36 +0100

    gardening

--- src/adhocracy_frontend/adhocracy_frontend/tests/fixtures/users.py
+++ src/adhocracy_frontend/adhocracy_frontend/tests/fixtures/users.py
@@ -18,7 +18,8 @@ null = None
 true = True
 false = False
 
-def register_user(user_name, password="password"):
+
+def register_user(user_name, password='password'):
     uri = root_uri + '/principals/users/'
     body = json.dumps({
             'data': {
@@ -28,9 +29,9 @@ def register_user(user_name, password="password"):
                 'adhocracy_core.sheets.principal.IUserBasic': {
                 'email': user_name + '@example.org',
                 'name': user_name
-            }
-        },
-        'content_type': 'adhocracy_core.resources.principal.IUser'
+                }
+            },
+            'content_type': 'adhocracy_core.resources.principal.IUser'
     })
     headers = {
         'Content-Type': 'application/json;charset=UTF-8',
