211db471d2f6d7d46e3c591ee715724833663127
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-01-12 12:56:12 +0100

    Revert "change upstream"
    This reverts commit f915050eaa51b5b2d0b4f72e918a36ffdf6840ee.

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/SocialShare/SocialShare.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/SocialShare/SocialShare.ts
@@ -2,16 +2,17 @@ import socialSharePrivacy = require("socialSharePrivacy");  if (socialSharePriva
 
 import AdhConfig = require("../Config/Config");
 
+export var PATH = "/static/lib/jquery.socialshareprivacy/socialshareprivacy/";
+
 
 export var socialShare = (adhConfig : AdhConfig.IService) => {
     return {
         restrict: "E",
         link: (scope, element, attrs) => {
             element.socialSharePrivacy({
-                path_prefix: "/static/lib/SocialSharePrivacy/build/",
-                css_path: "stylesheets/jquery.socialshareprivacy.min.css",
-                lang_path: "javascrips/",
-                language: adhConfig.locale  // FIXME: does not watch adhConfig.locale
+                "css_path": PATH + "socialshareprivacy.css",
+                "lang_path": PATH + "lang/",
+                "language": adhConfig.locale  // FIXME: does not watch adhConfig.locale
             });
 
             element.find(".help_info").each(function() {
