acff4a19f6f120b0456f371f5e5e8d0e4bc8ccf9
Caroline Clifford <me@carolineclifford.com>
2015-01-13 14:21:42 +0100

    Added new user message directive with include

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/User/User.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/User/User.ts
@@ -384,6 +384,13 @@ export var userListDirective = (adhConfig : AdhConfig.IService) => {
     };
 };
 
+export var userMessageDirective = (adhConfig : AdhConfig.IService) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/UserMessage.html"
+    };
+};
+
 export var userListItemDirective = (adhConfig : AdhConfig.IService) => {
     return {
         restrict: "E",
@@ -467,5 +474,6 @@ export var register = (angular) => {
         .directive("adhLogin", ["adhConfig", loginDirective])
         .directive("adhRegister", ["adhConfig", registerDirective])
         .directive("adhUserIndicator", ["adhConfig", indicatorDirective])
-        .directive("adhUserMeta", ["adhConfig", metaDirective]);
+        .directive("adhUserMeta", ["adhConfig", metaDirective])
+        .directive("adhUserMessage", ["adhConfig", userMessageDirective]);
 };
