81fc4e1e367c788e8c498bc20e0cd2d8e9a479b8
mkind <github@mknd.net>
2015-01-15 17:36:49 +0100

    Commit 8935629 fixes the issue, so test should not fail again.
    Revert "Other users are not able to reply on god's comments. Marking the test as xfailed. I am not sure whether this is a issue."
    
    This reverts commit 7ec2cd0c367136f66ad0562a54b799d61ed6a6f8.

--- src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/test_comment.py
+++ src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/test_comment.py
@@ -98,7 +98,6 @@ class TestComment:
         comment = browser.find_by_css('.comment').first
         assert not _get_button(browser, comment, EDIT)
 
-    @mark.xfail
     def test_reply_other_user(self, browser):
         comment = browser.find_by_css('.comment').first
         reply = create_reply_comment(browser, comment, 'other user reply')
