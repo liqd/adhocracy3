f4acbbd6f2a09daa93388941dfb41aff191141f1
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-01-20 23:22:30 +0100

    Fix DateTime unit tests after #489

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/DateTime/DateTimeSpec.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/DateTime/DateTimeSpec.ts
@@ -21,21 +21,22 @@ export var register = () => {
 
                 $intervalMock = jasmine.createSpy("$interval");
 
-                scopeMock = {
-                    datetime: "1970-01-01T00:00:00.000Z"
-                };
+                scopeMock = jasmine.createSpyObj("scope", ["$watch"]);
+                scopeMock.datetime = "1970-01-01T00:00:00.000Z";
 
                 directive = AdhDateTime.createDirective(configMock, momentMock, $intervalMock);
             });
 
             it("uses moment.js to parse the input", () => {
                 directive.link(scopeMock);
+                scopeMock.$watch.calls.mostRecent().args[1](scopeMock.datetime);
                 expect(momentMock).toHaveBeenCalledWith("1970-01-01T00:00:00.000Z");
             });
 
             it("saves the formatted datetime in scope.datetimeString", () => {
                 dtMock.format.and.returnValue("formatted");
                 directive.link(scopeMock);
+                scopeMock.$watch.calls.mostRecent().args[1](scopeMock.datetime);
                 expect(dtMock.format).toHaveBeenCalled();
                 expect(scopeMock.datetimeString).toBe("formatted");
             });
@@ -43,12 +44,14 @@ export var register = () => {
             it("saves user-friendly relative datetime in scope.text", () => {
                 dtMock.fromNow.and.returnValue("fromNow");
                 directive.link(scopeMock);
+                scopeMock.$watch.calls.mostRecent().args[1](scopeMock.datetime);
                 expect(dtMock.fromNow).toHaveBeenCalled();
                 expect(scopeMock.text).toBe("fromNow");
             });
 
             it("updates scope.text every 5 seconds", () => {
                 directive.link(scopeMock);
+                scopeMock.$watch.calls.mostRecent().args[1](scopeMock.datetime);
                 expect($intervalMock).toHaveBeenCalled();
                 var fn = $intervalMock.calls.mostRecent().args[0];
                 var delay = $intervalMock.calls.mostRecent().args[1];
