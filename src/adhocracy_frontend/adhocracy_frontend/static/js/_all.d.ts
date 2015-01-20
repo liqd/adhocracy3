296755afa1ee3e4f34882319514e60bec4a2fa4a
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-01-21 00:21:55 +0100

    Fix Flow Types

--- src/adhocracy_frontend/adhocracy_frontend/static/js/_all.d.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/_all.d.ts
@@ -69,6 +69,7 @@ declare class Flow {
     resume : () => any;
     pause : () => any;
     cancel : () => any;
+    on : (event : string, callback : Function) => void;
 }
 
 declare class FlowFile {
@@ -95,7 +96,6 @@ declare class FlowOpts {
     generateUniqueIdentifier : any;
     headers : any;
     maxChunkRetries : number;
-    maximumByteSize : number;
     method : string;
     onDropStopPropagation : boolean;
     permanentErrors : number[];
@@ -109,6 +109,11 @@ declare class FlowOpts {
     target : string;
     testChunks : boolean;
     withCredentials : boolean;
+
+    // these are not native to flow but used by custom functions
+    minimumWidth : number;
+    maximumByteSize : number;
+    acceptedFileTypes : string[];
 }
 
 declare module "socialSharePrivacy" {}
