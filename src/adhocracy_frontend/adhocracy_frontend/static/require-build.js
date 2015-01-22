ff3cb03dd2329ae7447d79173ba40c90004adbe5
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-01-22 14:11:20 +0100

    add r.js build config

---
+++ src/adhocracy_frontend/adhocracy_frontend/static/require-build.js
@@ -0,0 +1,28 @@
+({
+    baseUrl: "js",
+    paths: {
+        text: "empty:",
+        jquery: "empty:",
+        angular: "empty:",
+        angularAnimate: "empty:",
+        angularAria: "empty:",
+        angularTranslate: "empty:",
+        angularTranslateLoader: "empty:",
+        angularElastic: "empty:",
+        angularScroll: "empty:",
+        angularFlow: "empty:",
+        angularMessages: "empty:",
+        angularPlaceholderShim: "empty:",
+        flow: "empty:",
+        lodash: "empty:",
+        q: "empty:",
+        modernizr: "empty:",
+        moment: "empty:",
+        sticky: "empty:",
+        socialSharePrivacy: "empty:",
+        jqueryPlaceholderShim: "empty:",
+        adhTemplates: "./templates"
+    },
+    name: "Adhocracy",
+    out: "js/Adhocracy.min.js"
+})
