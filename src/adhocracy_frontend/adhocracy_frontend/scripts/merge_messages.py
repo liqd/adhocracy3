a252ecc783bdde2baf7da322a66103fb8d042113
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-02-03 14:17:26 +0100

    add merge messages script

---
+++ src/adhocracy_frontend/adhocracy_frontend/scripts/merge_messages.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""Merge keys from stdin into translation files.
+
+Does remove existing entries for keys that are not in stdin.
+"""
+
+import fileinput
+import json
+
+
+FILES = [
+    'src/adhocracy_frontend/adhocracy_frontend/static/i18n/de.json',
+    'src/adhocracy_frontend/adhocracy_frontend/static/i18n/en.json',
+]
+
+
+if __name__ == '__main__':
+    keys = [l.rstrip() for l in fileinput.input()]
+
+    for filename in FILES:
+        with open(filename) as fh:
+            data = json.load(fh)
+
+        # add new keys
+        for key in keys:
+            if key not in data:
+                data[key] = ''
+
+        # remove old keys
+        for key in list(data.keys()):
+            if key not in keys:
+                del data[key]
+
+        with open(filename, 'w') as fh:
+            json.dump(
+                data,
+                fh,
+                indent=2,
+                sort_keys=True,
+                separators=(',', ': '),
+                ensure_ascii=False)
