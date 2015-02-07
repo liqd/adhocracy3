dc2ed90941bd61f3c195d0b57f706a99739f9040
Nicolas Dietrich <nidi@mailbox.org>
2015-02-07 20:50:08 +0100

    Mark non-deterministic comment acceptance tests as xfail

--- src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/test_comment.py
+++ src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/test_comment.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlencode
 
 from pytest import fixture
+from pytest import mark
 
 from adhocracy_core.testing import god_login
 from adhocracy_frontend.tests.acceptance.shared import wait
@@ -60,6 +61,7 @@ class TestComment:
         assert wait(lambda: browser.find_by_css('.comment-content')
                                    .first.text == 'edited')
 
+    @mark.xfail(reason='Random timeouts')
     def test_edit_twice(self, browser):
         comment = browser.find_by_css('.comment').first
         edit_comment(browser, comment, 'edited 1')
@@ -92,6 +94,7 @@ class TestComment:
         comment = browser.find_by_css('.comment').last
         assert not _get_button(browser, comment, REPLY)
 
+    @mark.xfail(reason='Random timeouts')
     def test_edit_other_user(self, browser, rest_url, user):
         login(browser, user[0], user[1])
         _visit_url(browser, rest_url)
@@ -99,6 +102,7 @@ class TestComment:
         comment = browser.find_by_css('.comment').first
         assert not _get_button(browser, comment, EDIT)
 
+    @mark.xfail(reason='Random timeouts')
     def test_reply_other_user(self, browser):
         comment = browser.find_by_css('.comment').first
         reply = create_reply_comment(browser, comment, 'other user reply')
