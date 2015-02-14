f8f72c7366ba7ae6c050424827c5144524dc8464
Nicolas Dietrich <nidi@mailbox.org>
2015-02-14 21:39:55 +0100

    Automatically start test environment for protractor tests

--- etc/protractorConf.js
+++ etc/protractorConf.js
@@ -1,9 +1,27 @@
-// conf.js
+var exec = require("sync-exec");
+
 exports.config = {
-    specs: ["../src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/*Spec.js"],
+    suites: {
+        core: "../src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/*Spec.js",
+        mercator: "../src/mercator/tests/acceptance/*Spec.js"
+    },
     baseUrl: "http://localhost:6552",
     directConnect: true,
     capabilities: {
         "browserName": "chrome"
+    },
+    beforeLaunch: function() {
+        exec("bin/supervisord");
+        exec("bin/supervisorctl restart adhocracy_test:test_zeo test_backend_with_ws adhocracy_test:test_autobahn adhocracy_test:test_frontend");
+    },
+    afterLaunch: function() {
+        exec("bin/supervisorctl stop adhocracy_test:test_zeo test_backend_with_ws adhocracy_test:test_autobahn adhocracy_test:test_frontend");
+        exec("rm -rf var/test_zeodata/Data.fs* var/test_zeodata/blobs");
+    },
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        isVerbose: true,
+        includeStackTrace: true
     }
 }
