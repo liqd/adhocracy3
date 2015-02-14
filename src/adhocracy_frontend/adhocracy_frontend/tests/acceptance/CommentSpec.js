d9169a957b422118bab1ea5d36a0d1334bae2ab6
Nicolas Dietrich <nidi@mailbox.org>
2015-02-14 21:34:36 +0100

    Refactor & more comment acceptance tests

--- src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/CommentSpec.js
+++ src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/CommentSpec.js
@@ -1,85 +1,101 @@
-/// <reference path="src/mercator/build/lib/DefinitelyTyped/jasmine/jasmine.d.ts"/>
-/// <reference path="src/mercator/build/lib/DefinitelyTyped/angular-protractor/angular-protractor.d.ts"/>
-
-
-// porting a3 comment tests from splinter to protractor
-
-
 "use strict";
 
 var shared = require("./shared.js");
+var EmbeddedCommentsPage = require("./EmbeddedCommentsPage.js");
 
-
-var EmbeddedCommentsPage = function(referer) {
-    this.referer = referer;
-    this.poolPath = shared.restUrl + 'adhocracy/';
-    this.url = "/embed/create-or-show-comment-listing"
-        + "?key=" + referer + "&pool-path=" + this.poolPath;
-
-    this.listing = element(by.css(".listing"));
-    this.listingCreateForm = this.listing.element(by.css(".listing-create-form"));
-    this.commentInput = this.listingCreateForm.element(by.model("data.content"));
-    this.submitButton = this.listingCreateForm.element(by.css("input[type=\"submit\"]"));
-
-    this.get = function() {
-        browser.get(this.url);
-    };
-
-    this.createComment = function(comment) {
-        this.commentInput.sendKeys(comment);
-        this.submitButton.click();
-        return this.listing.element(by.css('.comment'));
-    };
-
-    this.createReply = function(parent, content) {
-        parent.element(by.css(".icon-reply")).click();
-        parent.element(by.model("data.content")).sendKeys(content);
-        parent.element(by.css("input[type=\"submit\"]")).click();
-        return parent.element(by.css('.comment'));
-    };
-};
-
-describe("adhocracy root page", function() {
-    it("should have a title", function() {
-        browser.get('/');
-        expect(browser.getTitle()).toEqual("adhocracy root page");
-    });
-});
 
 describe("comments", function() {
-    it("can be created", function() {
+    beforeEach(function() {
         shared.loginAnnotator();
-        var page = new EmbeddedCommentsPage("c1");
-        page.get();
+    });
+
+    it("can be created", function() {
+        var page = new EmbeddedCommentsPage("c1").get();
         var comment = page.createComment("comment 1");
         expect(comment.isPresent()).toBe(true);
-        expect(comment.element(by.css(".comment-content")).getText()).toEqual("comment 1");
+        expect(page.getCommentText(comment)).toEqual("comment 1");
+        expect(page.getCommentAuthor(comment)).toEqual(shared.annotatorName);
     });
 
     it("cannot be created empty", function() {
-        shared.loginAnnotator();
-        var page = new EmbeddedCommentsPage("c2");
-        page.get();
+        var page = new EmbeddedCommentsPage("c2").get();
         var comment = page.createComment("");
         expect(comment.isPresent()).toBe(false);
     });
 
     it("can be created nested", function() {
-        shared.loginAnnotator();
-        var page = new EmbeddedCommentsPage("c3");
-        page.get();
-        var comment = page.createComment("comment 3");
-
         var createNestedReplies = function(parent, parentName, remaining) {
             var name = parentName + ".1";
             var reply = page.createReply(parent, name);
             expect(reply.isPresent()).toBe(true);
-            expect(reply.element(by.css(".comment-content")).getText()).toEqual(name);
+            expect(page.getCommentText(reply)).toEqual(name);
             if (remaining > 0) {
                 createNestedReplies(reply, name, remaining - 1);
             }
         };
 
+        var page = new EmbeddedCommentsPage("c3").get();
+        var comment = page.createComment("comment 3");
         createNestedReplies(comment, "reply 3", 3);
+    });
+
+    it("can be edited", function() {
+        var page = new EmbeddedCommentsPage("c4").get();
+        var comment = page.createComment("comment 1");
+        page.editComment(comment, [protractor.Key.BACK_SPACE, "0a"]);
+        expect(page.getCommentText(comment)).toEqual("comment 0a");
+    });
+
+    it("can not be edited by anonymous", function() {
+        var page = new EmbeddedCommentsPage("c5").get();
+        var comment = page.createComment("comment 1");
+        shared.logout();
+        expect(page.getEditLink(comment).isPresent()).toBe(false);
+    });
+
+    it("can not be edited by other user", function() {
+        var page = new EmbeddedCommentsPage("c6").get();
+        var comment = page.createComment("comment 1");
+        shared.logout();
+        shared.loginContributor();
+        page.get();
+        expect(page.getEditLink(comment).isPresent()).toBe(false);
+    });
+
+    it("can not be replied to by anonymous", function() {
+        var page = new EmbeddedCommentsPage("c7").get();
+        var comment = page.createComment("comment 1");
+        shared.logout();
+        expect(page.getReplyLink(comment).isPresent()).toBe(false);
+    });
+
+    it("can be replied to by other user", function() {
+        var page = new EmbeddedCommentsPage("c8").get();
+        var comment = page.createComment("comment 1");
+        shared.logout();
+        shared.loginContributor();
+        page.get();
+        expect(page.getReplyLink(comment).isPresent()).toBe(true);
+        var reply = page.createReply(comment, "reply 1");
+        expect(page.getCommentText(reply)).toEqual("reply 1");
+    });
+
+    it("can be edited twice", function() {
+        var page = new EmbeddedCommentsPage("c9").get();
+        var comment = page.createComment("comment 1");
+        page.editComment(comment, [protractor.Key.BACK_SPACE, "0a"]);
+        expect(page.getCommentText(comment)).toEqual("comment 0a");
+        page.editComment(comment, [protractor.Key.BACK_SPACE, "b1"]);
+        expect(page.getCommentText(comment)).toEqual("comment 0b1");
+    });
+
+    it("can be edited after child edit", function() {
+        var page = new EmbeddedCommentsPage("c10").get();
+        var parent = page.createComment("comment 1");
+        var reply = page.createReply(parent, "comment 1.1");
+        var changedReply = page.editComment(reply, ["b"]);
+        var changedParent = page.editComment(parent, ["b"]);
+        expect(page.getCommentText(parent)).toEqual("comment 1b");
+        expect(page.getCommentText(reply)).toEqual("comment 1.1b");
     });
 });
---
+++ src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/EmbeddedCommentsPage.js
@@ -0,0 +1,75 @@
+"use strict";
+
+var shared = require("./shared.js");
+
+
+var EmbeddedCommentsPage = function(referer) {
+    this.referer = referer;
+    this.poolPath = shared.restUrl + 'adhocracy/';
+    this.url = "/embed/create-or-show-comment-listing"
+        + "?key=" + referer + "&pool-path=" + this.poolPath;
+
+    this.listing = element(by.css(".listing"));
+    this.listingCreateForm = this.listing.element(by.css(".listing-create-form"));
+    this.commentInput = this.listingCreateForm.element(by.model("data.content"));
+    this.submitButton = this.listingCreateForm.element(by.css("input[type=\"submit\"]"));
+
+    this.get = function() {
+        browser.get(this.url);
+        return this;
+    };
+
+    this.createComment = function(content) {
+        this.commentInput.sendKeys(content);
+        this.submitButton.click();
+        // FIXME: Return created comment
+        return this.listing.element(by.tagName("adh-comment-resource"));
+
+        /*
+        return all.reduce(function(acc, elem) {
+            return protractor.promise.all(
+                acc.getAttribute("data-path"),
+                elem.getAttribute("data-path")
+            ).then(function(paths) {
+                return (path[0] > path[1] ? acc : elem);
+            })
+        });*/
+    };
+
+    this.getReplyLink = function(comment) {
+        return comment.element(by.css(".icon-reply"));
+    };
+
+    this.getEditLink = function(comment) {
+        return comment.element(by.css("[data-ng-click='edit()']"));
+    };
+
+    this.createReply = function(parent, content) {
+        this.getReplyLink(parent).click();
+        parent.element(by.model("data.content")).sendKeys(content);
+        parent.element(by.css("input[type=\"submit\"]")).click();
+        return parent.element(by.css('.comment-children .comment'));
+    };
+
+    this.editComment = function(comment, keys) {
+        this.getEditLink(comment).click();
+        var textarea = comment.element(by.model("data.content"));
+        textarea.sendKeys.apply(textarea, keys);
+        comment.element(by.css("[data-ng-click='submit()']")).click();
+        return comment;
+    };
+
+    this.getCommentText = function(comment) {
+        return comment.element(by.css(".comment-content")).getText();
+    };
+
+    this.getCommentAuthor = function(comment) {
+        return comment.element(by.css("adh-user-meta a")).getText();
+    };
+
+    this.getRateWidget = function(comment) {
+        return comment.element(by.tagName("adh-rate"));
+    };
+};
+
+module.exports = EmbeddedCommentsPage;
