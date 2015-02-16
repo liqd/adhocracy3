522a3fcd32ece13b18b17d452f1894b0821fcadb
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-02-16 15:02:48 +0100

    Gardening

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Http/Http.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Http/Http.ts
@@ -454,8 +454,8 @@ export class Busy {
 export var busyDirective = (adhConfig : AdhConfig.IService, adhBusy : Busy) => {
     return {
         restrict: "E",
-        template: "<div class='busy-bar' data-ng-show='busy.count > 0'>" +
-            (adhConfig.debug ? "<span class='busy-count/'>{{busy.count}}</span>" : "") + "</div>",
+        template: "<div class=\"busy-bar\" data-ng-show=\"busy.count > 0\">" +
+            (adhConfig.debug ? "<span class=\"busy-count\"/>{{busy.count}}</span>" : "") + "</div>",
         link: (scope) => {
             scope.busy = adhBusy;
         }
