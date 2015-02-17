96ee4c621a36dee2e0b47ade2ca2f0b80e3c9883
Nicolas Dietrich <nidi@mailbox.org>
2015-02-17 16:47:55 +0100

    Only warmup on initial listing load
    Practically that means: Don't load full resources on websocket triggered
    updates.

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Listing/Listing.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Listing/Listing.ts
@@ -63,7 +63,7 @@ export interface ListingScope<Container> extends ng.IScope {
     poolOptions : AdhHttp.IOptions;
     createPath? : string;
     elements : string[];
-    update : () => ng.IPromise<void>;
+    update : (boolean?) => ng.IPromise<void>;
     wshandle : number;
     clear : () => void;
     onCreate : () => void;
@@ -123,7 +123,7 @@ export class Listing<Container extends ResourcesBase.Resource> {
             ) : void => {
                 $scope.createPath = adhPreliminaryNames.nextPreliminary();
 
-                $scope.update = () : ng.IPromise<void> => {
+                $scope.update = (warmup? : boolean) : ng.IPromise<void> => {
                     var params = <any>_.extend({}, $scope.params);
                     if (typeof $scope.contentType !== "undefined") {
                         params.content_type = $scope.contentType;
@@ -144,7 +144,7 @@ export class Listing<Container extends ResourcesBase.Resource> {
                     if ($scope.sort) {
                         params["sort"] = $scope.sort.replace(/^-/, "");
                     }
-                    return adhHttp.get($scope.path, params, true).then((container) => {
+                    return adhHttp.get($scope.path, params, warmup).then((container) => {
                         $scope.container = container;
                         $scope.poolPath = _self.containerAdapter.poolPath($scope.container);
                         $scope.elements = _self.containerAdapter.elemRefs($scope.container);
@@ -177,7 +177,7 @@ export class Listing<Container extends ResourcesBase.Resource> {
                         // order to not miss any messages in between. But in
                         // order to subscribe we already need the resource. So
                         // that is not possible.
-                        $scope.update().then(() => {
+                        $scope.update(true).then(() => {
                             try {
                                 $scope.wshandle = adhWebSocket.register($scope.poolPath, $scope.update);
                             } catch (e) {
