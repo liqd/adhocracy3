918d16287a0f3a831e56ce549cdf5dd34fc72c81
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-02-23 18:24:43 +0100

    update extract_messages
    do not search for `| translate` anymore, just match for the `TR__`

--- src/adhocracy_frontend/adhocracy_frontend/scripts/extract_messages.sh
+++ src/adhocracy_frontend/adhocracy_frontend/scripts/extract_messages.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-
-(
-    git grep "{{[^{]*['\"]\s*|\s*translate" \
-        | grep -o '{{[^{]*|\s*translate' \
-        | sed "s/{{\s*[\"']//;s/[\"']\s*|\s*translate//";
-    grep -o -h "TR__[^\"']*" $(git ls-files | grep '\.ts$');
-) | sort | uniq
+grep -o -h "TR__[^\"']*" $(git ls-files | grep '\.\(html\|ts\)$') | sort | uniq
