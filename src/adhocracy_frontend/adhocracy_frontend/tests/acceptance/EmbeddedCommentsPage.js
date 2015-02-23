812deb21d1fd359e92664b23e3b6c064c8e769db
Pierre Allix <pierre.allix@liqd.de>
2015-02-23 15:45:01 +0100

    Fix warnings in the acceptance tests

--- src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/EmbeddedCommentsPage.js
+++ src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/EmbeddedCommentsPage.js
@@ -22,18 +22,7 @@ var EmbeddedCommentsPage = function(referer) {
     this.createComment = function(content) {
         this.commentInput.sendKeys(content);
         this.submitButton.click();
-        // FIXME: Return created comment
-        return this.listing.element(by.tagName("adh-comment-resource"));
-
-        /*
-        return all.reduce(function(acc, elem) {
-            return protractor.promise.all(
-                acc.getAttribute("data-path"),
-                elem.getAttribute("data-path")
-            ).then(function(paths) {
-                return (path[0] > path[1] ? acc : elem);
-            })
-        });*/
+        return this.listing.element(by.xpath("(//adh-comment-resource)[1]"));
     };
 
     this.getReplyLink = function(comment) {
@@ -41,14 +30,14 @@ var EmbeddedCommentsPage = function(referer) {
     };
 
     this.getEditLink = function(comment) {
-        return comment.element(by.css("[data-ng-click='edit()']"));
+        return comment.element(by.xpath('(.//*[@data-ng-click="edit()"])[1]'));
     };
 
     this.createReply = function(parent, content) {
         this.getReplyLink(parent).click();
         parent.element(by.model("data.content")).sendKeys(content);
         parent.element(by.css("input[type=\"submit\"]")).click();
-        return parent.element(by.css('.comment-children .comment'));
+        return parent.all(by.css('.comment-children .comment')).first();
     };
 
     this.editComment = function(comment, keys) {
@@ -60,7 +49,7 @@ var EmbeddedCommentsPage = function(referer) {
     };
 
     this.getCommentText = function(comment) {
-        return comment.element(by.css(".comment-content")).getText();
+        return comment.all(by.css(".comment-content")).first().getText();
     };
 
     this.getCommentAuthor = function(comment) {
