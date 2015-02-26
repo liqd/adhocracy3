19ca922d6b6bc9a8d3fe0ca36ffb85564efdc69c
Nicolas Dietrich <nidi@mailbox.org>
2015-02-26 03:59:34 +0100

    Better check for comment edited state
    This is possible due to the fix of #770.

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Comment/Adapter.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Comment/Adapter.ts
@@ -1,7 +1,3 @@
-/// <reference path="../../../lib/DefinitelyTyped/lodash/lodash.d.ts"/>
-
-import _ = require("lodash");
-
 import AdhComment = require("./Comment");
 import AdhListing = require("../Listing/Listing");
 import AdhUtil = require("../Util/Util");
@@ -94,6 +90,7 @@ export class CommentAdapter extends ListingCommentableAdapter implements AdhComm
     }
 
     edited(resource : RICommentVersion) : boolean {
-        return !(<any>_).endsWith(resource.path, "/VERSION_0000001/");
+        var meta = resource.data[SIMetadata.nick];
+        return (meta.modification_date > meta.item_creation_date);
     }
 }
