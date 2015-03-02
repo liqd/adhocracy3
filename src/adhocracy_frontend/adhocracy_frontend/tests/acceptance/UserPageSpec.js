541cef15f4c3083a8b6206edf14b96b890055bef
Pierre Allix <pierre.allix@liqd.de>
2015-03-02 17:49:59 +0100

    Attempt at fixing user page acceptance tests

--- src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/UserPageSpec.js
+++ src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/UserPageSpec.js
@@ -5,14 +5,17 @@ var UserPages = require("./UserPages.js");
 
 describe("user page", function() {
     it("displays the correct name for each user", function() {
-        var usersListing = new UserPages.UsersListing().get();
+        var usersListing = new UserPages.UsersListing();
 
+        usersListing.get();
         var annotatorPage = usersListing.getUserPage("annotator");
         expect(annotatorPage.getUserName()).toBe("annotator");
 
+        usersListing.get();
         var contributorPage = usersListing.getUserPage("contributor");
         expect(contributorPage.getUserName()).toBe("contributor");
 
+        usersListing.get();
         var reviewerPage = usersListing.getUserPage("reviewer");
         expect(reviewerPage.getUserName()).toBe("reviewer");
     });
