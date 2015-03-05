9522fabed94e0651f0b4c0cd32073f3948728ce6
Pierre Allix <pierre.allix@liqd.de>
2015-03-05 17:14:42 +0100

    Update protractorConfSauce.js

--- etc/protractorConfSauce.js
+++ etc/protractorConfSauce.js
@@ -1,4 +1,6 @@
 var exec = require("sync-exec");
+var fs = require("fs");
+var ini = require("ini");
 var pr = process.env.TRAVIS_PULL_REQUEST;
 var name = ((pr === "false") ? "" : "#" + pr + " ") + process.env.TRAVIS_COMMIT;
 
@@ -24,6 +26,17 @@ exports.config = {
     afterLaunch: function() {
         exec("bin/supervisorctl stop adhocracy_test:test_zeo test_backend_with_ws adhocracy_test:test_autobahn adhocracy_test:test_frontend");
         exec("rm -rf var/test_zeodata/Data.fs* var/test_zeodata/blobs");
+    },
+    onPrepare: function() {
+        var getMailQueuePath = function() {
+            var testConf = ini.parse(fs.readFileSync("etc/test_with_ws.ini", "utf-8"));
+            return testConf["app:main"]["mail.queue_path"]
+                   .replace("%(here)s", process.cwd() + "/etc");
+        };
+
+        browser.params.mail = {
+            queue_path: getMailQueuePath()
+        }
     },
     jasmineNodeOpts: {
         showColors: true,
