93fbb4a87440ec93027f8968c10f1b2dd4f2d9de
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-03-19 15:30:00 +0100

    add leaflet integration

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Mapping/Mapping.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Mapping/Mapping.ts
@@ -1,11 +1,50 @@
-export var mapinput = () => {
-    return {
-        scope: {
 
+
+
+export var mapinput = ($timeout, L) => {
+    return {
+         scope: {
+            lat :  "=",
+            lng : "="
         },
         restrict: "E",
         replace: true,
-        template: "<div></div>"
+        template: (elem, attr) => {
+            return "<div style=\"height:" + attr.height + "px; cursor:crosshair;\"  id=\"" + attr.id + "\"></div>";
+            },
+        link: (scope, element, attrs) => {
+
+            var map = L.map(attrs.id, {center: [attrs.lat, attrs.lng], zoom: 14});
+            map.clicked = 0;
+            L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {maxZoom: 18}).addTo(map);
+
+            var marker = L.marker([scope.lat, scope.lng], {draggable: true});
+            map.on("click", (event) => {
+                map.clicked = map.clicked + 1;
+                setTimeout( () => {
+                    if ( map.clicked === 1 ) {
+                        marker.setLatLng(event.latlng);
+                        marker.addTo(map);
+                        $timeout( () => {
+                            scope.lat = event.latlng.lat;
+                            scope.lng = event.latlng.lng;
+                        });
+                        map.clicked = 0;
+                    }
+                }, 200);
+            });
+            map.on("dblclick", (event) => {
+                map.clicked = 0;
+                map.zoomIn();
+            });
+            marker.on("dragend", (event) => {
+                var result = event.target.getLatLng();
+                $timeout(function() {
+                    scope.lat = result.lat;
+                    scope.lng = result.lng;
+                });
+            });
+        }
     };
 };
 
@@ -14,5 +53,8 @@ export var moduleName = "adhMapping";
 export var register = (angular) => {
     angular
         .module(moduleName, [])
-        .directive("mapinput", [mapinput]);
+        .directive("adhMapInput", ["$timeout", "leaflet", mapinput]);
 };
+
+
+
