5195eae7d46ed1e6497ea5e2394f19840261f8fb
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-03-25 16:50:12 +0100

    cast to any to fix

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Mapping/Mapping.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Mapping/Mapping.ts
@@ -22,7 +22,9 @@ export var mapinput = (adhClickContext, $timeout : angular.ITimeoutService, leaf
 
             var map = leaflet.map(mapElement[0]);
             leaflet.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {maxZoom: 18}).addTo(map);
-            scope.polygon = leaflet.polygon(leaflet.GeoJSON.coordsToLatLngs(scope.polygon));
+
+            // FIXME: Definetely Typed
+            scope.polygon = leaflet.polygon((<any>leaflet.GeoJSON).coordsToLatLngs(scope.polygon));
             scope.polygon.addTo(map);
 
             // limit map to polygon
