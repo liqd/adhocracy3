618a98ad465bc8d8a9d022fd1783017720e9cbcd
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-03-31 12:17:56 +0200

    increase pointInPolygon readability

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Mapping/MappingUtils.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Mapping/MappingUtils.ts
@@ -4,14 +4,25 @@
 export function pointInPolygon(point : number[], polygon: number[][]) : Boolean {
     "use strict";
 
+    var n = polygon.length;
     var x = point[1], y = point[0];
 
     var inside = false;
-    for (var i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+    for (var i = 0; i < n; i++) {
+        var j = (i + n - 1) % n;
         var xi = polygon[i][0], yi = polygon[i][1];
         var xj = polygon[j][0], yj = polygon[j][1];
-        var intersect = ((yi > y) !== (yj > y))
-            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+
+        //      *
+        //     /
+        // *--/----------->>
+        //   *
+        // Check that
+        //
+        // 1.  yi and yj are on opposite sites of a ray to the right
+        // 2.  the intersection of the ray and the segment is right of x
+        var intersect = ((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+
         if (intersect) {
             inside = !inside;
         }
