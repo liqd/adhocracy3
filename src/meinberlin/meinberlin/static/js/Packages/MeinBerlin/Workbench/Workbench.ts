b7b27fad962035d95cd4e5863b7028dd92df7397
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-04-08 12:52:56 +0200

    add specific routing

--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Workbench/Workbench.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Workbench/Workbench.ts
@@ -1,6 +1,7 @@
 /// <reference path="../../../../lib/DefinitelyTyped/angularjs/angular.d.ts"/>
 
 import AdhConfig = require("../../Config/Config");
+import AdhHttp = require("../../Http/Http");
 import AdhMovingColumns = require("../../MovingColumns/MovingColumns");
 import AdhProcess = require("../../Process/Process");
 import AdhResourceArea = require("../../ResourceArea/ResourceArea");
@@ -8,6 +9,7 @@ import AdhResourceArea = require("../../ResourceArea/ResourceArea");
 import RICommentVersion = require("../../../Resources_/adhocracy_core/resources/comment/ICommentVersion");
 import RIKiezkassenProcess = require("../../../Resources_/adhocracy_core/resources/pool/IBasicPool");  // FIXME
 import RIProposalVersion = require("../../../Resources_/adhocracy_meinberlin/resources/kiezkassen/IProposalVersion");
+import SIComment = require("../../../Resources_/adhocracy_core/sheets/comment/IComment");
 
 var pkgLocation = "/MeinBerlin/Workbench";
 
@@ -81,6 +83,7 @@ export var moduleName = "adhMeinBerlinWorkbench";
 export var register = (angular) => {
     angular
         .module(moduleName, [
+            AdhHttp.moduleName,
             AdhMovingColumns.moduleName,
             AdhProcess.moduleName,
             AdhResourceArea.moduleName
@@ -92,22 +95,68 @@ export var register = (angular) => {
                     space: "content",
                     movingColumns: "is-show-hide-hide"
                 })
+                .specific(RIKiezkassenProcess.content_type, "", "", [() => (resource : RIKiezkassenProcess) => {
+                    return {
+                        processUrl: resource.path
+                    };
+                }])
                 .default(RIKiezkassenProcess.content_type, "create_proposal", "", {
                     space: "content",
                     movingColumns: "is-show-show-hide"
                 })
+                .specific(RIKiezkassenProcess.content_type, "create_proposal", "", [() => (resource : RIKiezkassenProcess) => {
+                    return {
+                        processUrl: resource.path
+                    };
+                }])
                 .default(RIProposalVersion.content_type, "", "", {
                     space: "content",
                     movingColumns: "is-show-show-hide"
                 })
+                .specific(RIProposalVersion.content_type, "", "", [() => (resource : RIProposalVersion) => {
+                    return {
+                        proposalUrl: resource.path,
+                        processUrl: "/adhocracy"  // FIXME
+                    };
+                }])
                 .default(RIProposalVersion.content_type, "comments", "", {
                     space: "content",
                     movingColumns: "is-collapse-show-show"
                 })
+                .specific(RIProposalVersion.content_type, "comments", "", [() => (resource : RIProposalVersion) => {
+                    return {
+                        commentableUrl: resource.path,
+                        proposalUrl: resource.path,
+                        processUrl: "/adhocracy"  // FIXME
+                    };
+                }])
                 .default(RICommentVersion.content_type, "", "", {
                     space: "content",
                     movingColumns: "is-collapse-show-show"
-                });
+                })
+                .specific(RIProposalVersion.content_type, "", "", ["adhHttp", "$q", (
+                    adhHttp : AdhHttp.Service<any>,
+                    $q : angular.IQService
+                ) => {
+                    var getCommentableUrl = (resource) : angular.IPromise<any> => {
+                        if (resource.content_type !== RICommentVersion.content_type) {
+                            return $q.when(resource);
+                        } else {
+                            var url = resource.data[SIComment.nick].refers_to;
+                            return adhHttp.get(url).then(getCommentableUrl);
+                        }
+                    };
+
+                    return (resource : RICommentVersion) => {
+                        return getCommentableUrl(resource).then((commentable) => {
+                            return {
+                                commentableUrl: commentable.path,
+                                proposalUrl: commentable.path,
+                                processUrl: "/adhocracy"  // FIXME
+                            };
+                        });
+                    };
+                }]);
         }])
         .config(["adhProcessProvider", (adhProcessProvider : AdhProcess.Provider) => {
             adhProcessProvider.templateFactories[""] = ["$q", ($q : angular.IQService) => {
