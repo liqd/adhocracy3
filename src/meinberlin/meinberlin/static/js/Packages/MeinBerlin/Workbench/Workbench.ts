77ff8c301530ffe3ea9f4e35b431e935e13d6abe
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-04-08 12:24:49 +0200

    Some simple routing

--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Workbench/Workbench.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Workbench/Workbench.ts
@@ -1,6 +1,13 @@
+/// <reference path="../../../../lib/DefinitelyTyped/angularjs/angular.d.ts"/>
+
 import AdhConfig = require("../../Config/Config");
 import AdhMovingColumns = require("../../MovingColumns/MovingColumns");
+import AdhProcess = require("../../Process/Process");
 import AdhResourceArea = require("../../ResourceArea/ResourceArea");
+
+import RICommentVersion = require("../../../Resources_/adhocracy_core/resources/comment/ICommentVersion");
+import RIKiezkassenProcess = require("../../../Resources_/adhocracy_core/resources/pool/IBasicPool");  // FIXME
+import RIProposalVersion = require("../../../Resources_/adhocracy_meinberlin/resources/kiezkassen/IProposalVersion");
 
 var pkgLocation = "/MeinBerlin/Workbench";
 
@@ -75,10 +82,37 @@ export var register = (angular) => {
     angular
         .module(moduleName, [
             AdhMovingColumns.moduleName,
+            AdhProcess.moduleName,
             AdhResourceArea.moduleName
         ])
+        // FIXME: the following should be specific to kiezkassen process
         .config(["adhResourceAreaProvider", (adhResourceAreaProvider : AdhResourceArea.Provider) => {
-            // FIXME
+            adhResourceAreaProvider
+                .default(RIKiezkassenProcess.content_type, "", "", {
+                    space: "content",
+                    movingColumns: "is-show-hide-hide"
+                })
+                .default(RIKiezkassenProcess.content_type, "create_proposal", "", {
+                    space: "content",
+                    movingColumns: "is-show-show-hide"
+                })
+                .default(RIProposalVersion.content_type, "", "", {
+                    space: "content",
+                    movingColumns: "is-show-show-hide"
+                })
+                .default(RIProposalVersion.content_type, "comments", "", {
+                    space: "content",
+                    movingColumns: "is-collapse-show-show"
+                })
+                .default(RICommentVersion.content_type, "", "", {
+                    space: "content",
+                    movingColumns: "is-collapse-show-show"
+                });
+        }])
+        .config(["adhProcessProvider", (adhProcessProvider : AdhProcess.Provider) => {
+            adhProcessProvider.templateFactories[""] = ["$q", ($q : angular.IQService) => {
+                return $q.when("<adh-mein-berlin-workbench></adh-mein-berlin-workbench>");
+            }];
         }])
         .directive("adhMeinBerlinWorkbench", ["adhConfig", meinBerlinWorkbenchDirective])
         .directive("adhCommentColumn", ["adhBindVariablesAndClear", "adhConfig", commentColumnDirective])
