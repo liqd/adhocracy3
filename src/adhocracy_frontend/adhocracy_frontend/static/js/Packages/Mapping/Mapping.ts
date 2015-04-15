6571150fd1ecc6b5aa3be0957dbb475fe29dab35
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-04-15 14:19:28 +0200

    experiment with listing and maps
    Conflicts:
    	src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Mapping/Mapping.ts

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Mapping/Mapping.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Mapping/Mapping.ts
@@ -3,9 +3,12 @@
 
 import _ = require("lodash");
 
+import ResourcesBase = require("../../ResourcesBase");
+
 import AdhAngularHelpers = require("../AngularHelpers/AngularHelpers");
 import AdhConfig = require("../Config/Config");
 import AdhEmbed = require("../Embed/Embed");
+import AdhListing = require("../Listing/Listing");
 import AdhMappingUtils = require("./MappingUtils");
 
 var pkgLocation = "/Mapping";
@@ -378,6 +381,12 @@ export var mapList = (adhConfig : AdhConfig.IService, leaflet : typeof L, $timeo
     };
 };
 
+
+export class Listing<Container extends ResourcesBase.Resource> extends AdhListing.Listing<Container> {
+    public static templateUrl : string = pkgLocation + "/Listing.html";
+}
+
+
 export var moduleName = "adhMapping";
 
 export var register = (angular) => {
@@ -385,6 +394,7 @@ export var register = (angular) => {
         .module(moduleName, [
             AdhAngularHelpers.moduleName,
             AdhEmbed.moduleName,
+            AdhListing.moduleName,
             "duScroll"
         ])
         .config(["adhEmbedProvider", (adhEmbedProvider : AdhEmbed.Provider) => {
@@ -392,5 +402,7 @@ export var register = (angular) => {
         }])
         .directive("adhMapInput", ["adhConfig", "adhSingleClickWrapper", "$timeout", "leaflet", mapInput])
         .directive("adhMapDetail", ["leaflet", "$timeout", mapDetail])
-        .directive("adhMapList", ["adhConfig", "leaflet", "$timeout" , mapList]);
+        .directive("adhMapList", ["adhConfig", "leaflet", "$timeout" , mapList])
+        .directive("adhMapListing", ["adhConfig", "adhWebSocket", (adhConfig, adhWebSocket) =>
+                new Listing(new AdhListing.ListingPoolAdapter()).createDirective(adhConfig, adhWebSocket)]);
 };
