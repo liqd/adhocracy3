412e607306705b0e5da9311594e23007e56afbf1
Pierre Allix <pierre.allix@liqd.de>
2015-04-23 15:08:27 +0200

    Typo

--- src/adhocracy_core/adhocracy_core/interfaces.py
+++ src/adhocracy_core/adhocracy_core/interfaces.py
@@ -186,7 +186,7 @@ class ResourceMetadata(namedtuple('ResourceMetadata',
     permission_view:
         Permission to view resource data and view in listings
     is_implicit_addable:
-        Make this type adddable if supertype is addable.
+        Make this type addable if supertype is addable.
     basic_sheets:
         Basic property interfaces to define data
     extended_sheets:
