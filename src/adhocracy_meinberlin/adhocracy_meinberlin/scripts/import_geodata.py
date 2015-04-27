df92ef2eea70556becfed7739c1281e3c9ab3ac5
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-04-27 12:31:27 +0200

    gardening

--- src/adhocracy_meinberlin/adhocracy_meinberlin/scripts/import_geodata.py
+++ src/adhocracy_meinberlin/adhocracy_meinberlin/scripts/import_geodata.py
@@ -1,4 +1,4 @@
-"""Import GEO-Data to meinBerlin.
+"""Import GEO-Data to adhocracy meinBerlin.
 
 The functions are registered in setup.py
 in setup.py.
@@ -10,8 +10,12 @@ import os
 import json
 import re
 import unicodedata
+import optparse
+import textwrap
+import inspect
 
 from pyramid.paster import bootstrap
+from pyramid.registry import Registry
 
 from substanced.util import find_service
 
@@ -20,128 +24,42 @@ import transaction
 from adhocracy_core.sheets.name import IName
 from adhocracy_core.sheets.title import ITitle
 from adhocracy_core.sheets.pool import IPool
+from adhocracy_core.interfaces import IResource
+from adhocracy_core.resources.geo import ILocationsService
 from adhocracy_core.resources.geo import multipolygon_meta
 from adhocracy_core.sheets.geo import IMultiPolygon
 from adhocracy_core.utils import get_sheet
 from adhocracy_core.utils import get_sheet_field
 
 
-def slugify(value):
-    """slugify string."""
-    value = unicodedata.normalize(
-        'NFKD',
-        value).encode(
-        'ascii',
-        'ignore').decode('ascii')
-    value = re.sub('[^\w\s-]', '', value).strip().lower()
-    return re.sub('[-\s]+', '-', value)
+def import_bezirke():
+    """Import Berlin districts to adhocracy meinBerlin.
 
+    usage::
 
-def import_bezirksregions():
-    """Get geodata for berlin's bezirksregions from FIS-Broker."""
-    try:
-        import_bezirke()
-    except:
-        pass
+        bin/import_bezirke etc/development.ini
+    """
+    usage = 'usage: %prog config_file'
+    parser = optparse.OptionParser(
+        usage=usage,
+        description=textwrap.dedent(inspect.getdoc(import_bezirke))
+    )
+    options, args = parser.parse_args(sys.argv[1:])
+    if not len(args) >= 1:
+        print('You must provide at least one argument')
+        return 2
 
-    call = 'ogr2ogr -s_srs EPSG:25833 -t_srs WGS84 -f geoJSON ' \
-           '/tmp/bezirksregions.json WFS:"http://fbinter.stadt-berlin.de' \
-           '/fb/wfs/geometry/senstadt/re_bezirksregion?TYPENAMES=GML2" ' \
-           ' fis:re_bezirksregion'
-
-    try:
-        os.remove('/tmp/bezirksregions.json')
-        print('Old file removed')
-    except:
-        print('No file to remove')
-
-    try:
-        os.system(call)
-    except Exception:
-        t, e = sys.exc_info()[:2]
-        print(e)
-        sys.exit()
-
-    data = json.load(open('/tmp/bezirksregions.json', 'r'))
-
-    env = bootstrap('./etc/development.ini')
+    env = bootstrap(args[0])
     root = env['root']
     registry = env['registry']
     locations = find_service(root, 'locations')
-    pool = get_sheet(root, IPool)
-    params = {'depth': 3,
-              'content_type': IMultiPolygon,
-              'elements': 'content',
-              }
-    results = pool.get(params)
-    bezirke = results['elements']
-    lookup = {}
-    for bezirk in bezirke:
-        if (get_sheet_field(bezirk,
-                            IMultiPolygon,
-                            'administrative_division') == 'stadtbezirk'):
-            name = (get_sheet_field(bezirk, IName, 'name'))
-            lookup[name] = bezirk
 
-    for feature in data['features']:
+    url = 'http://fbinter.stadt-berlin.de/fb/'\
+          'wfs/geometry/senstadt/re_bezirke/?TYPENAMES=GML2'
 
-        geometry = feature['geometry']['coordinates']
-        bezirk = feature['properties']['BEZIRK']
-        bezirks_region_name = feature['properties']['BZR_NAME']
-        type = feature['geometry']['type']
-        if type == 'Polygon':
-            geometry = [geometry]
-
-        geosheet = {'coordinates': geometry,
-                    'administrative_division': 'bezirksregion',
-                    'part_of': lookup[slugify(bezirk)],
-                    'type': 'MultiPolygon'}
-
-        appstructs = {
-            IName.__identifier__: {
-                'name': slugify(bezirks_region_name)},
-            IMultiPolygon.__identifier__: geosheet,
-            ITitle.__identifier__: {
-                'title': bezirks_region_name}}
-
-        try:
-            registry.content.create(
-                multipolygon_meta.iresource.__identifier__,
-                appstructs=appstructs,
-                parent=locations)
-            root._p_changed = True
-            transaction.commit()
-        except Exception:
-            t, e = sys.exc_info()[:2]
-            print(e)
-
-
-def import_bezirke():
-    """Get geodata for berlin's bezirke from FIS-Broker."""
-    call = 'ogr2ogr -s_srs EPSG:25833 -t_srs WGS84 -f geoJSON ' \
-           ' /tmp/bezirke.json WFS:"http://fbinter.stadt-berlin.de' \
-           '/fb/wfs/geometry/senstadt/re_bezirke?TYPENAMES=GML2"' \
-           'fis:re_bezirke'
-
-    try:
-        os.remove('/tmp/bezirke.json')
-        print('Old file removed')
-    except:
-        print('No file to remove')
-
-    try:
-        os.system(call)
-    except Exception:
-        t, e = sys.exc_info()[:2]
-        print(e)
-        sys.exit()
+    _download_geodata('/tmp/bezirke.json', url, 'fis:re_bezirke')
 
     data = json.load(open('/tmp/bezirke.json', 'r'))
-
-    env = bootstrap('./etc/development.ini')
-    root = env['root']
-    registry = env['registry']
-    locations = find_service(root, 'locations')
 
     for feature in data['features']:
 
@@ -156,17 +74,133 @@ def import_bezirke():
                     'part_of': None,
                     'type': 'MultiPolygon'}
 
-        appstructs = {IName.__identifier__: {'name': slugify(bezirk)},
+        appstructs = {IName.__identifier__: {'name': _slugify(bezirk)},
                       IMultiPolygon.__identifier__: geosheet,
                       ITitle.__identifier__: {'title': bezirk}
                       }
 
-        try:
-            registry.content.create(
-                multipolygon_meta.iresource.__identifier__,
-                appstructs=appstructs,
-                parent=locations)
-        except Exception:
-            t, e = sys.exc_info()[:2]
-            print(e)
+        _create_multipolygon(registry, appstructs, locations)
+
     transaction.commit()
+
+
+def import_bezirksregions():
+    """Import Berlin district-regions to adhocracy meinBerlin.
+
+    usage::
+
+        bin/import_bezirksregions etc/development.ini
+    """
+    usage = 'usage: %prog config_file'
+    parser = optparse.OptionParser(
+        usage=usage,
+        description=textwrap.dedent(inspect.getdoc(import_bezirksregions))
+    )
+    options, args = parser.parse_args(sys.argv[1:])
+    if not len(args) >= 1:
+        print('You must provide at least one argument')
+        return 2
+
+    env = bootstrap(args[0])
+    root = env['root']
+    registry = env['registry']
+    locations = find_service(root, 'locations')
+
+    url = 'http://fbinter.stadt-berlin.de/fb/'\
+          'wfs/geometry/senstadt/re_bezirksregion?TYPENAMES=GML2'
+    _download_geodata('/tmp/bezirksregions.json', url, 'fis:re_bezirksregion')
+
+    data = json.load(open('/tmp/bezirksregions.json', 'r'))
+
+    lookup = _fetch_all_districs(root)
+
+    for feature in data['features']:
+
+        geometry = feature['geometry']['coordinates']
+        bezirk = feature['properties']['BEZIRK']
+        bezirks_region_name = feature['properties']['BZR_NAME']
+        type = feature['geometry']['type']
+        if type == 'Polygon':
+            geometry = [geometry]
+
+        try:
+            bezirk_resource = lookup[_slugify(bezirk)]
+        except:
+            bezirk_resource = None
+
+        geosheet = {'coordinates': geometry,
+                    'administrative_division': 'bezirksregion',
+                    'part_of': bezirk_resource,
+                    'type': 'MultiPolygon'}
+
+        appstructs = {
+            IName.__identifier__: {
+                'name': _slugify(bezirks_region_name)},
+            IMultiPolygon.__identifier__: geosheet,
+            ITitle.__identifier__: {
+                'title': bezirks_region_name}}
+
+        _create_multipolygon(registry, appstructs, locations)
+
+    transaction.commit()
+
+
+def _slugify(value: str) -> str:
+    value = unicodedata.normalize(
+        'NFKD',
+        value).encode(
+        'ascii',
+        'ignore').decode('ascii')
+    value = re.sub('[^\w\s-]', '', value).strip().lower()
+    return re.sub('[-\s]+', '-', value)
+
+
+def _download_geodata(filename: str, url: str, layer: str):
+    call = 'ogr2ogr -s_srs EPSG:25833'\
+           ' -t_srs WGS84 -f'\
+           ' geoJSON %s WFS:"%s" %s' % (filename, url, layer)
+    try:
+        os.remove(filename)
+    except:
+        pass
+
+    try:
+        print('trying to download file from %s' % (url))
+        os.system(call)
+    except Exception:
+        sys.exit()
+
+
+def _fetch_all_districs(root: IResource) -> dict:
+    pool = get_sheet(root, IPool)
+    params = {'depth': 3,
+              'content_type': IMultiPolygon,
+              'elements': 'content',
+              }
+    results = pool.get(params)
+    bezirke = results['elements']
+    lookup = {}
+    for bezirk in bezirke:
+        if (get_sheet_field(bezirk,
+                            IMultiPolygon,
+                            'administrative_division') == 'stadtbezirk'):
+            name = (get_sheet_field(bezirk, IName, 'name'))
+            lookup[name] = bezirk
+    return lookup
+
+
+def _create_multipolygon(registry: Registry,
+                         appstructs: dict,
+                         locations: ILocationsService):
+    try:
+        print(
+            'creating MultiPolygon Resource for %s' %
+            (appstructs[
+                ITitle.__identifier__]['title']))
+        registry.content.create(
+            multipolygon_meta.iresource.__identifier__,
+            appstructs=appstructs,
+            parent=locations)
+    except Exception:
+        t, e = sys.exc_info()[:2]
+        print(e)
