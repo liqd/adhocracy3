b8473e3cb03310ac4637f0b323d4eba28fd9dd90
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-04-28 11:17:21 +0200

    configure kiezkassen context

--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Kiezkassen/Kiezkassen.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Kiezkassen/Kiezkassen.ts
@@ -1,3 +1,4 @@
+import AdhMeinBerlinKiezkassenContext = require("./Context/Context");
 import AdhMeinBerlinKiezkassenProcess = require("./Process/Process");
 import AdhMeinBerlinKiezkassenProposal = require("./Proposal/Proposal");
 import AdhMeinBerlinKiezkassenWorkbench = require("./Workbench/Workbench");
@@ -6,12 +7,14 @@ import AdhMeinBerlinKiezkassenWorkbench = require("./Workbench/Workbench");
 export var moduleName = "adhMeinBerlinKiezkassen";
 
 export var register = (angular) => {
+    AdhMeinBerlinKiezkassenContext.register(angular);
     AdhMeinBerlinKiezkassenProcess.register(angular);
     AdhMeinBerlinKiezkassenProposal.register(angular);
     AdhMeinBerlinKiezkassenWorkbench.register(angular);
 
     angular
         .module(moduleName, [
+            AdhMeinBerlinKiezkassenContext.moduleName,
             AdhMeinBerlinKiezkassenProcess.moduleName,
             AdhMeinBerlinKiezkassenProposal.moduleName,
             AdhMeinBerlinKiezkassenWorkbench.moduleName
