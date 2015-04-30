adb3bcec1c592cc85990a2f12bff75fdacdef156
Pierre Allix <pierre.allix@liqd.de>
2015-04-30 16:20:49 +0200

    Remove proposals votes and users with script

--- src/adhocracy_mercator/adhocracy_mercator/scripts/delete_users.py
+++ src/adhocracy_mercator/adhocracy_mercator/scripts/delete_users.py
@@ -9,6 +9,7 @@ setup.py.
 
 import argparse
 import inspect
+import transaction
 from pyramid.paster import bootstrap
 
 from adhocracy_core.interfaces import IResource
@@ -17,8 +18,7 @@ from adhocracy_core.resources.principal import IUser
 from adhocracy_core.resources.rate import IRateVersion
 from adhocracy_core.sheets.pool import IPool
 from adhocracy_core.sheets.rate import IRate
-# from adhocracy_core.sheets.rate import IRateable
-# from adhocracy_core.utils import get_sheet
+from adhocracy_core.utils import get_sheet
 from adhocracy_core.utils import get_sheet_field
 from adhocracy_mercator.resources.mercator import IMercatorProposalVersion
 from pyramid.request import Request
@@ -48,19 +48,43 @@ def delete_users():
 
 
 def _delete_users_and_votes(filename: str, root: IResource, registry: Registry):
+    print('Reading emails from file...')
     users_emails = _get_emails(filename)
+    print('Getting users from the database...')
     users = _get_users(root, registry, users_emails)
-    print('users=', users)
-    return users
+    pool = get_sheet(root, IPool)
+    for user in users:
+        user_name = user.__name__
+        _delete_proposals_rates(pool, user)
+        _delete_user(user)
+        print('User {} and its votes deleted.'.format(user_name))
+    transaction.commit()
+    print('Users deleted successfully.')
+
+
+def _delete_proposals_rates(pool: IPool, user: IUser):
+    rates = _get_rates_from_user(pool, user)
+    proposals_rates = _select_proposal_rates(rates)
+    for proposal_rate in proposals_rates:
+        proposal_rate.__parent__.remove(proposal_rate.__name__)
+
+
+def _delete_user(user: IUser):
+    user.__parent__.remove(user.__name__)
 
 
 def _get_users(root: IResource, registry: Registry, emails: [str]) -> [IUser]:
     locator = _get_user_locator(root, registry)
-    return [locator.get_user_by_email(email) for email in emails]
+    for email in emails:
+        user = locator.get_user_by_email(email)
+        if user is None:
+            print('No user found for email: ', email)
+        else:
+            yield user
 
 
 def _get_emails(filename: str) -> [str]:
-    return ['user1@example.com']
+    return [line.rstrip() for line in open(filename)]
 
 
 def _get_user_locator(context: IResource, registry: Registry) -> IUserLocator:
@@ -79,26 +103,6 @@ def _get_rates_from_user(pool: IPool, user: IUser) -> [IRateVersion]:
     return rates
 
 
-def _select_proposal_rates_(rates: [IRateVersion]) -> [IRateVersion]:
+def _select_proposal_rates(rates: [IRateVersion]) -> [IRateVersion]:
     return [rate for rate in rates
             if IMercatorProposalVersion.providedBy(get_sheet_field(rate, IRate, 'object'))]
-
-#
-# get_sheet(rvs[0], adhocracy_core.sheets.rate.IRate).get()
-# deletion
-# rv0.__parent__.remove(rv0.__name__)
-
-# from adhocracy_mercator.scripts.delete_users import _delete_users_and_votes
-# from adhocracy_mercator.scripts.delete_users import _get_rates_from_user
-# from adhocracy_core.sheets.pool import IPool
-# from adhocracy_core.sheets.rate import IRate
-# from adhocracy_core.utils import get_sheet
-# from adhocracy_core.utils import get_isheets
-# import transaction
-#
-# users = _delete_users_and_votes('', root, registry)
-# u1 = users[0]
-# pool = get_sheet(root, IPool)
-# rvs =  _get_rates_from_user(pool, u1)
-# rv0 = rvs[0]
-# rate0 = get_sheet(rv0, IRate)
