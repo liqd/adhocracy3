1a4c30e96f673b0c4bc0581e3aca35c1bd9dc8f7
Pierre Allix <pierre.allix@liqd.de>
2015-05-18 15:29:34 +0200

    fix meinberlin unit test

--- src/adhocracy_meinberlin/adhocracy_meinberlin/resources/test_subscriber.py
+++ src/adhocracy_meinberlin/adhocracy_meinberlin/resources/test_subscriber.py
@@ -11,9 +11,9 @@ def test_application_created_subscriber(monkeypatch):
     # avoid substanced querying the real registry when the commit occurs
     monkeypatch.setattr(transaction, 'commit', lambda: None)
     event.app.registry.content.permissions = ['edit_kiezkassen_proposal',
+                                              'create_kiezkassen_proposal'
                                               'create_kiezkassen_process']
     root = testing.DummyResource(__acl__=[(Allow, 'role:creator', 'view')])
     event.app.root_factory.return_value = root
     _application_created_subscriber(event)
-    assert (Allow, 'role:god', 'edit_kiezkassen_proposal') in root.__acl__
-    assert (Allow, 'role:god', 'create_kiezkassen_process') in root.__acl__
+    assert (Allow, 'role:participant', 'create_kiezkassen_proposal') in root.__acl__
