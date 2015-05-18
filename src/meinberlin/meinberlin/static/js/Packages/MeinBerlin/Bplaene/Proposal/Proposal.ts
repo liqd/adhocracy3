2e3e5449feb0c33fe5887e386c8738f873e7a0a1
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-05-18 14:38:25 +0200

    added bplaene to project

---
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Bplaene/Proposal/Proposal.ts
@@ -0,0 +1,66 @@
+/// <reference path="../../../../../lib/DefinitelyTyped/angularjs/angular.d.ts"/>
+
+import AdhAngularHelpers = require("../../../AngularHelpers/AngularHelpers");
+import AdhEmbed = require("../../../Embed/Embed");
+import AdhHttp = require("../../../Http/Http");
+import AdhPermissions = require("../../../Permissions/Permissions");
+import AdhResourceArea = require("../../../ResourceArea/ResourceArea");
+import AdhTopLevelState = require("../../../TopLevelState/TopLevelState");
+import AdhConfig = require("../../../Config/Config");
+import AdhPreliminaryNames = require("../../../PreliminaryNames/PreliminaryNames");
+
+var pkgLocation = "/MeinBerlin/Bplaene/Proposal";
+
+
+export var createDirective = (
+    adhConfig : AdhConfig.IService,
+    adhHttp : AdhHttp.Service<any>,
+    adhPreliminaryNames : AdhPreliminaryNames.Service,
+    adhTopLevelState : AdhTopLevelState.Service,
+    adhShowError,
+    adhSubmitIfValid,
+    adhResourceUrlFilter
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/Create.html",
+        link: (scope, element) => {
+            scope.errors = [];
+            scope.data = {};
+            scope.create = true;
+            scope.showError = adhShowError;
+
+            scope.data.lat = undefined;
+            scope.data.lng = undefined;
+        }
+    };
+};
+
+
+
+export var moduleName = "adhMeinBplaeneProposal";
+
+export var register = (angular) => {
+    angular
+        .module(moduleName, [
+            AdhAngularHelpers.moduleName,
+            AdhEmbed.moduleName,
+            AdhHttp.moduleName,
+            AdhPermissions.moduleName,
+            AdhResourceArea.moduleName,
+            AdhTopLevelState.moduleName
+        ])
+        .config(["adhEmbedProvider", (adhEmbedProvider: AdhEmbed.Provider) => {
+            adhEmbedProvider.embeddableDirectives.push("mein-berlin-bplaene-proposal-create");
+        }])
+        .directive("adhMeinBerlinBplaeneProposalCreate", [
+            "adhConfig",
+            "adhHttp",
+            "adhPreliminaryNames",
+            "adhTopLevelState",
+            "adhShowError",
+            "adhSubmitIfValid",
+            "adhResourceUrlFilter",
+            createDirective
+        ]);
+};
--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/MeinBerlin.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/MeinBerlin.ts
@@ -1,13 +1,16 @@
 import AdhMeinBerlinKiezkassen = require("./Kiezkassen/Kiezkassen");
+import AdhMeinBerlinBplaene = require("./Bplaene/Bplaene");
 
 
 export var moduleName = "adhMeinBerlin";
 
 export var register = (angular) => {
     AdhMeinBerlinKiezkassen.register(angular);
+    AdhMeinBerlinBplaene.register(angular);
 
     angular
         .module(moduleName, [
-            AdhMeinBerlinKiezkassen.moduleName
+            AdhMeinBerlinKiezkassen.moduleName,
+            AdhMeinBerlinBplaene.moduleName
         ]);
 };
