6e0fa635b92c8cd32f9420ef1abb045f8b18c604
Pierre Allix <pierre.allix@liqd.de>
2015-05-18 15:48:04 +0200

    Remove outdated mercator evolution scripts
    Permissions are now set in the subscriber.

--- src/adhocracy_mercator/adhocracy_mercator/evolution/__init__.py
+++ src/adhocracy_mercator/adhocracy_mercator/evolution/__init__.py
@@ -40,27 +40,6 @@ def evolve1_add_ititle_sheet_to_proposals(root):  # pragma: no cover
     logger.info('Finished substanced evolve step 1: add new ITitle sheet')
 
 
-def evolve2_disable_add_proposal_permission(root):  # pragma: no cover
-    """Disable add_proposal permissions."""
-    from adhocracy_core.authorization import set_acl
-    from substanced.util import get_acl
-    from pyramid.threadlocal import get_current_registry
-    from pyramid.security import Deny
-
-    logger.info('Running substanced evolve step 2:'
-                'remove add_proposal permission')
-
-    registry = get_current_registry()
-    acl = get_acl(root)
-    deny_acl = [(Deny, 'role:contributor', 'add_proposal'),
-                (Deny, 'role:creator', 'edit_mercator_proposal')]
-    updated_acl = deny_acl + acl
-    set_acl(root, updated_acl, registry=registry)
-
-    logger.info('Finished substanced evolve step 2:'
-                'remove add_proposal permission')
-
-
 def evolve3_use_adhocracy_core_title_sheet(root):  # pragma: no cover
     """Migrate mercator title sheet to adhocracy_core title sheet."""
     from adhocracy_core.sheets.title import ITitle
@@ -71,34 +50,7 @@ def evolve3_use_adhocracy_core_title_sheet(root):  # pragma: no cover
                       fields_mapping=[('title', 'title')])
 
 
-def evolve4_disable_voting_and_commenting(root):
-    """Disable rate and comment permissions."""
-    from adhocracy_core.authorization import set_acl
-    from substanced.util import get_acl
-    from pyramid.threadlocal import get_current_registry
-    from pyramid.security import Deny
-
-    logger.info('Running substanced evolve step 3:'
-                'remove add_rate, edit_rate, add_comment and'
-                'edit_comment permissions')
-
-    registry = get_current_registry()
-    acl = get_acl(root)
-    deny_acl = [(Deny, 'role:annotator', 'add_comment'),
-                (Deny, 'role:annotator', 'add_rate'),
-                (Deny, 'role:creator', 'edit_comment'),
-                (Deny, 'role:creator', 'edit_rate')]
-    updated_acl = deny_acl + acl
-    set_acl(root, updated_acl, registry=registry)
-
-    logger.info('Finished substanced evolve step 3:'
-                'remove add_rate, edit_rate, add_comment and'
-                'edit_comment permissions')
-
-
 def includeme(config):  # pragma: no cover
     """Register evolution utilities and add evolution steps."""
     config.add_evolution_step(evolve1_add_ititle_sheet_to_proposals)
-    config.add_evolution_step(evolve2_disable_add_proposal_permission)
     config.add_evolution_step(evolve3_use_adhocracy_core_title_sheet)
-    config.add_evolution_step(evolve4_disable_voting_and_commenting)
