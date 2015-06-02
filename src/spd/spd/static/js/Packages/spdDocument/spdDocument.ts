d56db02f1bc25c9fe44b9d4eff5e83d335bb7f36
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-06-02 10:06:19 +0200

    added detail directive

--- src/spd/spd/static/js/Packages/spdDocument/spdDocument.ts
+++ src/spd/spd/static/js/Packages/spdDocument/spdDocument.ts
@@ -1,19 +1,55 @@
 /* tslint:disable:variable-name */
 /// <reference path="../../../lib/DefinitelyTyped/moment/moment.d.ts"/>
 
-// import _ = require("lodash");
 import AdhAngularHelpers = require("../AngularHelpers/AngularHelpers");
+import AdhConfig = require("../Config/Config");
 import AdhHttp = require("../Http/Http");
 import AdhInject = require("../Inject/Inject");
 import AdhPreliminaryNames = require("../PreliminaryNames/PreliminaryNames");
+import AdhPermissions = require("../Permissions/Permissions");
 import AdhResourceArea = require("../ResourceArea/ResourceArea");
 import AdhResourceWidgets = require("../ResourceWidgets/ResourceWidgets");
 import AdhTopLevelState = require("../TopLevelState/TopLevelState");
+import AdhRate = require("../Rate/Rate");
 import AdhSticky = require("../Sticky/Sticky");
+import AdhEmbed = require("../Embed/Embed");
 
 
 
-// var pkgLocation = "/spdDocument";
+var pkgLocation = "/spdDocument";
+
+export interface IScope extends angular.IScope {
+    path? : string;
+    options : AdhHttp.IOptions;
+    errors? : AdhHttp.IBackendErrorItem[];
+    data : {
+
+        // FIXME: not final
+        title : string;
+        detail : string;
+    };
+    selectedState? : string;
+    resource: any;
+}
+
+export var detailDirective = (
+    adhConfig : AdhConfig.IService,
+    adhHttp : AdhHttp.Service<any>,
+    adhPermissions : AdhPermissions.Service,
+    adhRate : AdhRate.Service,
+    adhTopLevelState : AdhTopLevelState.Service
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/Detail.html",
+        scope: {
+            path: "@"
+        },
+        link: (scope : IScope) => {
+            console.log("here comes the stuff");
+        }
+    };
+};
 
 export var moduleName = "adhSPDDocument";
 
@@ -30,5 +66,10 @@ export var register = (angular) => {
             AdhResourceWidgets.moduleName,
             AdhSticky.moduleName,
             AdhTopLevelState.moduleName
-        ]);
+        ])
+        .config(["adhEmbedProvider", (adhEmbedProvider: AdhEmbed.Provider) => {
+            adhEmbedProvider.embeddableDirectives.push("spd-document-detail");
+        }])
+        .directive("adhSPDDocumentDetail", [
+            "adhConfig", "adhHttp", "adhPermissions", "adhRate", "adhTopLevelState", detailDirective]);
 };
