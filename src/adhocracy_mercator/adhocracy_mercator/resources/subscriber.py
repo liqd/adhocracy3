94d8a7f258772828660a1fd6d226d836263389a9
Pierre Allix <pierre.allix@liqd.de>
2015-06-04 16:04:32 +0200

    Initialize mercator's workflow to 'participate'

--- src/adhocracy_mercator/adhocracy_mercator/resources/subscriber.py
+++ src/adhocracy_mercator/adhocracy_mercator/resources/subscriber.py
@@ -7,17 +7,37 @@ from adhocracy_core.authorization import set_god_all_permissions
 from adhocracy_core.resources.root import root_acm
 from adhocracy_mercator.resources.root import mercator_acm
 import transaction
+from pyramid.request import Request
 
 
 def _application_created_subscriber(event):
     """Called when the Pyramid application is started."""
     app = event.app
+    _set_permissions(app)
+    _initialize_workflow(app)
+    transaction.commit()
+
+
+def _set_permissions(app):
     root = get_root(app)
     clean_acl(root, app.registry)
     add_acm(root, root_acm, app.registry)
     add_acm(root, mercator_acm, app.registry)
     set_god_all_permissions(root, app.registry)
-    transaction.commit()
+
+
+def _initialize_workflow(app):
+    root = get_root(app)
+    registry = app.registry
+    request = Request.blank('/dummy')
+    request.root = root
+    request.registry = registry
+    request.__cached_principals__ = ['role:god']
+    mercator_process = root['mercator']
+    workflow = registry.content.workflows['mercator']
+    workflow.initialize(mercator_process)
+    workflow.transition_to_state(mercator_process, request, 'announce')
+    workflow.transition_to_state(mercator_process, request, 'participate')
 
 
 def includeme(config):
--- src/adhocracy_mercator/adhocracy_mercator/resources/test_subscriber.py
+++ src/adhocracy_mercator/adhocracy_mercator/resources/test_subscriber.py
@@ -3,17 +3,58 @@ from pyramid.security import Allow
 from pyramid.security import Deny
 from pyramid.security import ALL_PERMISSIONS
 from pyramid import testing
-
+from pytest import fixture
+from pytest import mark
+from adhocracy_core.interfaces import IResource
 
 def test_application_created_subscriber(monkeypatch):
     import transaction
-    from adhocracy_mercator.resources.subscriber import _application_created_subscriber
+    import adhocracy_mercator.resources.subscriber
+    commit_mock = Mock()
+    mock_set_permissions = Mock()
+    mock_initialize_workflow = Mock()
+    monkeypatch.setattr(adhocracy_mercator.resources.subscriber,
+                        '_set_permissions', mock_set_permissions)
+    monkeypatch.setattr(adhocracy_mercator.resources.subscriber,
+                        '_initialize_workflow', mock_initialize_workflow)
+    monkeypatch.setattr(transaction, 'commit', commit_mock)
     event = Mock()
-    # avoid substanced querying the real registry when the commit occurs
-    monkeypatch.setattr(transaction, 'commit', lambda: None)
-    event.app.registry.content.permissions = ['edit_proposal']
-    root = testing.DummyResource(__acl__=[])
-    event.app.root_factory.return_value = root
+    from adhocracy_mercator.resources.subscriber import _application_created_subscriber
     _application_created_subscriber(event)
+    assert mock_set_permissions.called
+    assert commit_mock.called
+
+
+def test_set_permissions():
+    from adhocracy_mercator.resources.subscriber import _set_permissions
+    app = Mock()
+    app.registry.content.permissions = ['edit_proposal']
+    root = testing.DummyResource(__acl__=[])
+    app.root_factory.return_value = root
+    _set_permissions(app)
     assert (Allow, 'role:admin', 'edit_proposal') in root.__acl__
     assert (Allow, 'role:god', ALL_PERMISSIONS) == root.__acl__[0]
+
+
+@fixture
+def integration(config):
+    config.include('adhocracy_core.events')
+    config.include('adhocracy_core.content')
+    config.include('adhocracy_core.rest')
+    config.include('adhocracy_core.workflows.sample')
+    config.include('adhocracy_mercator.workflows')
+
+
+@mark.usefixtures('integration')
+def test_initialize_workflow(registry):
+    from adhocracy_mercator.resources.subscriber import _initialize_workflow
+    app = Mock()
+    root = testing.DummyResource()
+    mercator = testing.DummyResource()
+    root['mercator'] = mercator
+    root.__acl__ = [(Allow, 'role:god', ALL_PERMISSIONS)]
+    app.root_factory.return_value = root
+    app.registry = registry
+    workflow = registry.content.workflows['mercator']
+    _initialize_workflow(app)
+    assert workflow.state_of(mercator) == 'participate'
