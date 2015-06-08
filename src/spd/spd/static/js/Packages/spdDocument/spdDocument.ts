f4de86e9c5ca4dcfb9942cc021a5e1e689ca140d
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-06-08 16:20:18 +0200

    implement postCreate

--- src/spd/spd/static/js/Packages/spdDocument/spdDocument.ts
+++ src/spd/spd/static/js/Packages/spdDocument/spdDocument.ts
@@ -14,6 +14,16 @@ import AdhResourceArea = require("../ResourceArea/ResourceArea");
 import AdhResourceWidgets = require("../ResourceWidgets/ResourceWidgets");
 import AdhSticky = require("../Sticky/Sticky");
 import AdhTopLevelState = require("../TopLevelState/TopLevelState");
+import AdhUtil = require("../Util/Util");
+
+import RIDocument = require("../../Resources_/adhocracy_core/resources/document/IDocument");
+import RIDocumentVersion = require("../../Resources_/adhocracy_core/resources/document/IDocumentVersion");
+import RIParagraph = require("../../Resources_/adhocracy_core/resources/paragraph/IParagraph");
+import RIParagraphVersion = require("../../Resources_/adhocracy_core/resources/paragraph/IParagraphVersion");
+import SIDocument = require("../../Resources_/adhocracy_core/sheets/document/IDocument");
+import SIName = require("../../Resources_/adhocracy_core/sheets/name/IName");
+import SIParagraph = require("../../Resources_/adhocracy_core/sheets/document/IParagraph");
+import SIVersionable = require("../../Resources_/adhocracy_core/sheets/versions/IVersionable");
 
 var pkgLocation = "/spdDocument";
 
@@ -70,7 +80,57 @@ export interface IFormScope extends IScope {
     create : boolean;
     showError;
     addParagraph() : void;
+    submit() : angular.IPromise<any>;
 }
+
+
+var postCreate = (
+    adhHttp : AdhHttp.Service<any>,
+    adhPreliminaryNames : AdhPreliminaryNames.Service
+) => (
+    scope : IFormScope,
+    poolPath : string
+) => {
+    var doc = new RIDocument({preliminaryNames: adhPreliminaryNames});
+    doc.parent = poolPath;
+    doc.data[SIName.nick] = new SIName.Sheet({
+        name: AdhUtil.normalizeName(scope.data.title)
+    });
+
+    var paragraphItems = [];
+    var paragraphVersions = [];
+
+    _.forEach(scope.data.paragraphs, (paragraph) => {
+        var item = new RIParagraph({preliminaryNames: adhPreliminaryNames});
+        item.parent = doc.path;
+
+        var version = new RIParagraphVersion({preliminaryNames: adhPreliminaryNames});
+        version.parent = item.path;
+        version.data[SIVersionable.nick] = new SIVersionable.Sheet({
+            follows: [item.first_version_path]
+        });
+        version.data[SIParagraph.nick] = new SIParagraph.Sheet({
+            text: paragraph.body
+        });
+
+        paragraphItems.push(item);
+        paragraphVersions.push(version);
+    });
+
+    var documentVersion = new RIDocumentVersion({preliminaryNames: adhPreliminaryNames});
+    documentVersion.parent = doc.path;
+    documentVersion.data[SIVersionable.nick] = new SIVersionable.Sheet({
+        follows: [doc.first_version_path]
+    });
+    documentVersion.data[SIDocument.nick] = new SIDocument.Sheet({
+        title: scope.data.title,
+        description: "",
+        elements: <string[]>_.map(paragraphVersions, "path")
+    });
+
+    return adhHttp.deepPost(<any[]>_.flatten([doc, documentVersion, paragraphItems, paragraphVersions]));
+};
+
 
 export var detailDirective = (
     adhConfig : AdhConfig.IService,
@@ -120,14 +180,16 @@ export var createDirective = (
     adhConfig : AdhConfig.IService,
     adhHttp : AdhHttp.Service<any>,
     adhPermissions : AdhPermissions.Service,
-    adhRate : AdhRate.Service,
-    adhTopLevelState : AdhTopLevelState.Service,
+    adhPreliminaryNames : AdhPreliminaryNames.Service,
     adhShowError,
     adhSubmitIfValid
 ) => {
     return {
         restrict: "E",
         templateUrl: adhConfig.pkg_path + pkgLocation + "/Create.html",
+        scope: {
+            path: "@"
+        },
         link: (scope : IFormScope) => {
             scope.errors = [];
             scope.data = {
@@ -141,6 +203,10 @@ export var createDirective = (
                 scope.data.paragraphs.push({
                     body: ""
                 });
+            };
+
+            scope.submit = () => {
+                return postCreate(adhHttp, adhPreliminaryNames)(scope, scope.path).then((r) => console.log(r));
             };
         }
     };
@@ -191,7 +257,7 @@ export var register = (angular) => {
         .directive("adhSpdDocumentDetail", [
             "adhConfig", "adhHttp", "adhPermissions", "adhRate", "adhTopLevelState", detailDirective])
         .directive("adhSpdDocumentCreate", [
-            "adhConfig", "adhHttp", "adhPermissions", "adhRate", "adhTopLevelState", "adhShowError", "adhSubmitIfValid", createDirective])
+            "adhConfig", "adhHttp", "adhPermissions", "adhPreliminaryNames", "adhShowError", "adhSubmitIfValid", createDirective])
         .directive("adhSpdDocumentEdit", [
             "adhConfig", "adhHttp", "adhPermissions", "adhRate", "adhTopLevelState", editDirective])
         .directive("adhSpdDocumentListItem", [
