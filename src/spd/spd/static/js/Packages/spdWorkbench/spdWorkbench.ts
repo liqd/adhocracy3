fa75ba9b11ed0020d895eca9c44db52a4f53dbfa
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-06-09 14:25:24 +0200

    spd workbench

--- src/spd/spd/static/js/Packages/spdWorkbench/spdWorkbench.ts
+++ src/spd/spd/static/js/Packages/spdWorkbench/spdWorkbench.ts
@@ -1,31 +1,226 @@
 /// <reference path="../../../lib/DefinitelyTyped/angularjs/angular.d.ts"/>
-import AdhAbuse = require("../Abuse/Abuse");
+
 import AdhComment = require("../Comment/Comment");
+import AdhConfig = require("../Config/Config");
 import AdhHttp = require("../Http/Http");
-import AdhListing = require("../Listing/Listing");
-import AdhSPDDocument = require("../spdDocument/spdDocument");
 import AdhMovingColumns = require("../MovingColumns/MovingColumns");
 import AdhPermissions = require("../Permissions/Permissions");
+import AdhProcess = require("../Process/Process");
 import AdhResourceArea = require("../ResourceArea/ResourceArea");
+import AdhSPDDocument = require("../spdDocument/spdDocument");
 import AdhTopLevelState = require("../TopLevelState/TopLevelState");
-import AdhUser = require("../User/User");
+import AdhUtil = require("../Util/Util");
 
-// var pkgLocation = "/spdWorkbench";
+import RIComment = require("../../Resources_/adhocracy_core/resources/comment/IComment");
+import RICommentVersion = require("../../Resources_/adhocracy_core/resources/comment/ICommentVersion");
+import RIDocument = require("../../Resources_/adhocracy_core/resources/document/IDocument");
+import RIDocumentVersion = require("../../Resources_/adhocracy_core/resources/document/IDocumentVersion");
+import RIProcess = require("../../Resources_/adhocracy_core/resources/process/IProcess");
+import SIComment = require("../../Resources_/adhocracy_core/sheets/comment/IComment");
+
+var pkgLocation = "/spdWorkbench";
+
+
+export var spdWorkbenchDirective = (
+    adhConfig : AdhConfig.IService,
+    adhTopLevelState : AdhTopLevelState.Service
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/Workbench.html",
+        link: (scope) => {
+            scope.$on("$destroy", adhTopLevelState.bind("view", scope));
+        }
+    };
+};
+
+
+export var commentColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/CommentColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["documentUrl", "commentableUrl"]);
+        }
+    };
+};
+
+export var spdDocumentDetailColumnDirective = (
+    adhConfig : AdhConfig.IService,
+    adhPermissions : AdhPermissions.Service
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/SpdDocumentDetailColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl", "documentUrl"]);
+            adhPermissions.bindScope(scope, () => scope.documentUrl && AdhUtil.parentPath(scope.documentUrl), "proposalItemOptions");
+        }
+    };
+};
+
+export var spdDocumentCreateColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/SpdDocumentCreateColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl"]);
+        }
+    };
+};
+
+export var spdDocumentEditColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/SpdDocumentEditColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl", "documentUrl"]);
+        }
+    };
+};
+
+export var processDetailColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/ProcessDetailColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl"]);
+        }
+    };
+};
+
+export var processDetailAnnounceColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    var directive = processDetailColumnDirective(adhConfig);
+    directive.templateUrl = adhConfig.pkg_path + pkgLocation + "/ProcessDetailAnnounceColumn.html";
+    return directive;
+};
+
 
 export var moduleName = "adhSPDWorkbench";
 
 export var register = (angular) => {
+    var processType = "";  // FIXME: change to RIProcess.content_type
+
     angular
         .module(moduleName, [
-            AdhAbuse.moduleName,
             AdhComment.moduleName,
             AdhHttp.moduleName,
-            AdhListing.moduleName,
-            AdhSPDDocument.moduleName,
             AdhMovingColumns.moduleName,
             AdhPermissions.moduleName,
+            AdhProcess.moduleName,
             AdhResourceArea.moduleName,
-            AdhTopLevelState.moduleName,
-            AdhUser.moduleName
-        ]);
+            AdhSPDDocument.moduleName,
+            AdhTopLevelState.moduleName
+        ])
+        .config(["adhProcessProvider", (adhProcessProvider : AdhProcess.Provider) => {
+            adhProcessProvider.templateFactories[processType] = ["$q", ($q : angular.IQService) => {
+                return $q.when("<adh-spd-workbench></adh-spd-workbench>");
+            }];
+        }])
+        .config(["adhResourceAreaProvider", (adhResourceAreaProvider : AdhResourceArea.Provider) => {
+            adhResourceAreaProvider
+                .default(RIProcess, "", processType, "", {
+                    space: "content",
+                    movingColumns: "is-show-hide-hide"
+                })
+                .default(RIProcess, "create_document", processType, "", {
+                    space: "content",
+                    movingColumns: "is-show-hide-hide"
+                })
+                .specific(RIProcess, "create_document", processType, "", [
+                    "adhHttp", (adhHttp : AdhHttp.Service<any>) => (resource : RIProcess) => {
+                        return adhHttp.options(resource.path).then((options : AdhHttp.IOptions) => {
+                            if (!options.POST) {
+                                throw 401;
+                            } else {
+                                return {};
+                            }
+                        });
+                    }])
+                .defaultVersionable(RIDocument, RIDocumentVersion, "", processType, "", {
+                    space: "content",
+                    movingColumns: "is-show-show-hide"
+                })
+                .specificVersionable(RIDocument, RIDocumentVersion, "", processType, "", [
+                    () => (item : RIDocument, version : RIDocumentVersion) => {
+                        return {
+                            documentUrl: version.path
+                        };
+                    }])
+                .defaultVersionable(RIDocument, RIDocumentVersion, "edit", processType, "", {
+                    space: "content",
+                    movingColumns: "is-show-show-hide"
+                })
+                .specificVersionable(RIDocument, RIDocumentVersion, "edit", processType, "", [
+                    "adhHttp", (adhHttp : AdhHttp.Service<any>) => (item : RIDocument, version : RIDocumentVersion) => {
+                        return adhHttp.options(item.path).then((options : AdhHttp.IOptions) => {
+                            if (!options.POST) {
+                                throw 401;
+                            } else {
+                                return {
+                                    documentUrl: version.path
+                                };
+                            }
+                        });
+                    }])
+                .defaultVersionable(RIDocument, RIDocumentVersion, "comments", processType, "", {
+                    space: "content",
+                    movingColumns: "is-collapse-show-show"
+                })
+                .specificVersionable(RIDocument, RIDocumentVersion, "comments", processType, "", [
+                    () => (item : RIDocument, version : RIDocumentVersion) => {
+                        return {
+                            commentableUrl: version.path,
+                            documentUrl: version.path
+                        };
+                    }])
+                .defaultVersionable(RIComment, RICommentVersion, "", processType, "", {
+                    space: "content",
+                    movingColumns: "is-collapse-show-show"
+                })
+                .specificVersionable(RIComment, RICommentVersion, "", processType, "", ["adhHttp", "$q", (
+                    adhHttp : AdhHttp.Service<any>,
+                    $q : angular.IQService
+                ) => {
+                    var getCommentableUrl = (resource) : angular.IPromise<any> => {
+                        if (resource.content_type !== RICommentVersion.content_type) {
+                            return $q.when(resource);
+                        } else {
+                            var url = resource.data[SIComment.nick].refers_to;
+                            return adhHttp.get(url).then(getCommentableUrl);
+                        }
+                    };
+
+                    return (item : RIComment, version : RICommentVersion) => {
+                        return getCommentableUrl(version).then((commentable) => {
+                            return {
+                                commentableUrl: commentable.path,
+                                documentUrl: commentable.path
+                            };
+                        });
+                    };
+                }]);
+        }])
+        .directive("adhSpdWorkbench", ["adhConfig", "adhTopLevelState", spdWorkbenchDirective])
+        .directive("adhCommentColumn", ["adhConfig", commentColumnDirective])
+        .directive("adhSpdDocumentDetailColumn", ["adhConfig", "adhPermissions", spdDocumentDetailColumnDirective])
+        .directive("adhSpdDocumentCreateColumn", ["adhConfig", spdDocumentCreateColumnDirective])
+        .directive("adhSpdDocumentEditColumn", ["adhConfig", spdDocumentEditColumnDirective])
+        .directive("adhSpdProcessDetailColumn", ["adhConfig", processDetailColumnDirective])
+        .directive("adhSpdProcessDetailAnnounceColumn", ["adhConfig", processDetailAnnounceColumnDirective]);
 };
