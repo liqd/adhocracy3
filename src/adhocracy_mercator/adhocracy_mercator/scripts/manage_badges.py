28706b1479068586ac115b6060cd1e8269db19f0
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-06-09 11:01:05 +0200

    remove first script

--- src/adhocracy_mercator/adhocracy_mercator/scripts/manage_badges.py
+++ src/adhocracy_mercator/adhocracy_mercator/scripts/manage_badges.py
@@ -16,64 +16,13 @@ from adhocracy_core.resources.badge import IBadgeAssignment as BadgeResource
 from adhocracy_core.sheets.badge import IBadgeAssignment as BadgeSheet
 
 
-def add_badge_assignment():
-    """add badgeassignment.
-
-    usage::
-      bin/create_badge <config> <user> <badge> <proposal> <parent>
-    """
-    docstring = inspect.getdoc(add_badge_assignment)
-    parser = argparse.ArgumentParser(description=docstring)
-    parser.add_argument('ini_file',
-                        help='path to the adhocracy backend ini file')
-    parser.add_argument('user',
-                        type=str,
-                        help='path to user')
-    parser.add_argument('badge',
-                        type=str,
-                        help='path to badge')
-    parser.add_argument('proposal',
-                        type=str,
-                        help='path to proposal')
-    parser.add_argument('parent',
-                        type=str,
-                        help='path to parent')
-
-    args = parser.parse_args()
-    env = bootstrap(args.ini_file)
-
-    root = env['root']
-    registry = env['registry']
-
-    user = args.user
-    badge = args.badge
-    proposal_version = args.proposal
-    parent = args.parent
-
-    user = find_resource(root, user)
-    badge = find_resource(root, badge)
-    proposal_version = find_resource(root, proposal_version)
-    parent = find_resource(root, parent)
-
-    appstructs = {BadgeSheet.__identifier__:
-                  {'subject': user,
-                   'badge': badge,
-                   'object': proposal_version}}
-
-    registry.content.create(BadgeResource.__identifier__,
-                            parent=parent,
-                            appstructs=appstructs)
-
-    transaction.commit()
-
-
 def add_badge_assignment_from_json():
     """add badgeassignment.
 
     usage::
       bin/create_badge <config> <jsonfile>
     """
-    docstring = inspect.getdoc(add_badge_assignment)
+    docstring = inspect.getdoc(add_badge_assignment_from_json)
     parser = argparse.ArgumentParser(description=docstring)
     parser.add_argument('ini_file',
                         help='path to the adhocracy backend ini file')
