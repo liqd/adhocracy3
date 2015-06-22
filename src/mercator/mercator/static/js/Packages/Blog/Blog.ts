4eb8c6516067a14fcedce72084085b925764a036
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-06-22 17:05:13 +0200

    add create form

--- src/mercator/mercator/static/js/Packages/Blog/Blog.ts
+++ src/mercator/mercator/static/js/Packages/Blog/Blog.ts
@@ -2,6 +2,9 @@ import AdhConfig = require("../Config/Config");
 import AdhDocument = require("../Document/Document");
 import AdhEmbed = require("../Embed/Embed");
 import AdhHttp = require("../Http/Http");
+import AdhPreliminaryNames = require("../PreliminaryNames/PreliminaryNames");
+
+import RIDocumentVersion = require("../../Resources_/adhocracy_core/resources/document/IDocumentVersion");
 
 var pkgLocation = "/Blog";
 
@@ -23,6 +26,42 @@ export var detailDirective = (
     };
 };
 
+export var createDirective = (
+    adhConfig : AdhConfig.IService,
+    adhHttp : AdhHttp.Service<any>,
+    adhPreliminaryNames : AdhPreliminaryNames.Service,
+    adhShowError,
+    adhSubmitIfValid
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/Create.html",
+        scope: {
+            path: "@"
+        },
+        link: (scope : AdhDocument.IFormScope, element) => {
+            scope.errors = [];
+            scope.data = {
+                title: "",
+                paragraphs: [{
+                    body: ""
+                }]
+            };
+            scope.showError = adhShowError;
+
+            scope.submit = () => {
+                return adhSubmitIfValid(scope, element, scope.documentForm, () => {
+                    return AdhDocument.postCreate(adhHttp, adhPreliminaryNames)(scope, scope.path);
+                }).then((documentVersion : RIDocumentVersion) => {
+                    // FIXME: update listing
+                }, (errors) => {
+                    scope.errors = errors;
+                });
+            };
+        }
+    };
+};
+
 
 export var moduleName = "adhBlog";
 
@@ -34,6 +73,9 @@ export var register = (angular) => {
         ])
         .config(["adhEmbedProvider", (adhEmbedProvider: AdhEmbed.Provider) => {
             adhEmbedProvider.embeddableDirectives.push("blog-post");
+            adhEmbedProvider.embeddableDirectives.push("blog-post-create");
         }])
-        .directive("adhBlogPost", ["$q", "adhConfig", "adhHttp", detailDirective]);
+        .directive("adhBlogPost", ["$q", "adhConfig", "adhHttp", detailDirective])
+        .directive("adhBlogPostCreate", [
+            "adhConfig", "adhHttp", "adhPreliminaryNames", "adhShowError", "adhSubmitIfValid", createDirective]);
 };
