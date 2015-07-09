21de52fbf796e0767b4b1d21cd0c77ca57c283c2
Pierre Allix <pierre.allix@liqd.de>
2015-07-09 16:26:05 +0200

    Add proposal resources types for alexanderplatz

--- src/adhocracy_meinberlin/adhocracy_meinberlin/resources/alexanderplatz.py
+++ src/adhocracy_meinberlin/adhocracy_meinberlin/resources/alexanderplatz.py
@@ -3,18 +3,42 @@
 from adhocracy_core.resources.document_process import document_process_meta
 from adhocracy_core.resources.document_process import IDocumentProcess
 from adhocracy_core.resources import add_resource_type_to_registry
+from adhocracy_core.sheets.geo import IPoint
+from adhocracy_core.resources import proposal
+
+
+class IProposalVersion(proposal.IProposalVersion):
+
+    """Alexanderplatz proposal version."""
+
+
+proposal_version_meta = proposal.proposal_version_meta._replace(
+    iresource=IProposalVersion,
+)._add(extended_sheets=[IPoint])
+
+
+class IProposal(proposal.IProposal):
+
+    """Alexanderplatz proposal versions pool."""
+
+proposal_meta = proposal.proposal_meta._replace(
+    iresource=IProposal,
+    element_types=[IProposalVersion],
+    item_type=IProposalVersion,
+)
 
 
 class IProcess(IDocumentProcess):
 
     """Alexanderplatz participation process."""
 
-
 process_meta = document_process_meta._replace(
     iresource=IProcess
-)
+)._add(element_types=[IProposal])
 
 
 def includeme(config):
     """Add resource type to content."""
+    add_resource_type_to_registry(proposal_meta, config)
+    add_resource_type_to_registry(proposal_version_meta, config)
     add_resource_type_to_registry(process_meta, config)
