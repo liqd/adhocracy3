9f23dbabb9e601d11091ebddebbf215ee473ba7e
Pierre Allix <pierre.allix@liqd.de>
2015-07-13 14:45:36 +0200

    Add geolocalisable documents to alexanderplatz

--- src/adhocracy_meinberlin/adhocracy_meinberlin/resources/alexanderplatz.py
+++ src/adhocracy_meinberlin/adhocracy_meinberlin/resources/alexanderplatz.py
@@ -1,10 +1,13 @@
 """Alexanderplatz process resources."""
 
-from adhocracy_core.resources.document_process import document_process_meta
-from adhocracy_core.resources.document_process import IDocumentProcess
+from adhocracy_core.interfaces import ITag
 from adhocracy_core.resources import add_resource_type_to_registry
-from adhocracy_core.sheets.geo import IPoint
+from adhocracy_core.resources import document
 from adhocracy_core.resources import proposal
+from adhocracy_core.resources.document_process import IDocumentProcess
+from adhocracy_core.resources.document_process import document_process_meta
+from adhocracy_core.resources.paragraph import IParagraph
+from adhocracy_core.sheets.geo import IPoint
 
 
 class IProposalVersion(proposal.IProposalVersion):
@@ -28,13 +31,40 @@ proposal_meta = proposal.proposal_meta._replace(
 )
 
 
+class IDocumentVersion(document.IDocumentVersion):
+
+    """Versionable document with geo-location."""
+
+
+document_version_meta = document.document_version_meta._replace(
+    iresource=IDocumentVersion,
+)._add(
+    extended_sheets=[IPoint]
+)
+
+
+class IDocument(document.IDocument):
+
+    """Geolocalisable document."""
+
+
+document_meta = document.document_meta._replace(
+    iresource=IDocument,
+    element_types=[ITag,
+                   IParagraph,
+                   IDocumentVersion]
+)
+
+
 class IProcess(IDocumentProcess):
 
     """Alexanderplatz participation process."""
 
 process_meta = document_process_meta._replace(
-    iresource=IProcess
-)._add(element_types=[IProposal])
+    iresource=IProcess,
+    element_types=[IProposal,
+                   IDocument]
+)
 
 
 def includeme(config):
@@ -42,3 +72,5 @@ def includeme(config):
     add_resource_type_to_registry(proposal_meta, config)
     add_resource_type_to_registry(proposal_version_meta, config)
     add_resource_type_to_registry(process_meta, config)
+    add_resource_type_to_registry(document_version_meta, config)
+    add_resource_type_to_registry(document_meta, config)
--- src/adhocracy_meinberlin/adhocracy_meinberlin/resources/test_alexanderplatz.py
+++ src/adhocracy_meinberlin/adhocracy_meinberlin/resources/test_alexanderplatz.py
@@ -44,6 +44,52 @@ class TestProposalVersion:
         res = registry.content.create(meta.iresource.__identifier__)
         assert meta.iresource.providedBy(res)
 
+class TestDocument:
+
+    @fixture
+    def meta(self):
+        from .alexanderplatz import document_meta
+        return document_meta
+
+    def test_meta(self, meta):
+        from adhocracy_core import resources
+        from adhocracy_core.interfaces import ITag
+        from .alexanderplatz import IDocument
+        from .alexanderplatz import IDocumentVersion
+        assert meta.iresource == IDocument
+        assert meta.element_types == [ITag,
+                                      resources.paragraph.IParagraph,
+                                      IDocumentVersion
+                                      ]
+
+    @mark.usefixtures('integration')
+    def test_create(self, registry, meta):
+        assert registry.content.create(meta.iresource.__identifier__)
+
+class TestDocumentVersion:
+
+    @fixture
+    def meta(self):
+        from .alexanderplatz import document_version_meta
+        return document_version_meta
+
+    def test_meta(self, meta):
+        from adhocracy_core import resources
+        from adhocracy_core import sheets
+        from .alexanderplatz import IDocumentVersion
+        assert meta.iresource == IDocumentVersion
+        assert meta.extended_sheets == [sheets.document.IDocument,
+                                        sheets.comment.ICommentable,
+                                        sheets.badge.IBadgeable,
+                                        sheets.rate.IRateable,
+                                        sheets.image.IImageReference,
+                                        sheets.title.ITitle,
+                                        sheets.geo.IPoint
+                                        ]
+
+    @mark.usefixtures('integration')
+    def test_create(self, registry, meta):
+        assert registry.content.create(meta.iresource.__identifier__)
 
 class TestProcess:
 
