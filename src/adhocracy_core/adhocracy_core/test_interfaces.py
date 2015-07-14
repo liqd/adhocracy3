7ee5f5703cc95c6309e1e094963fb833074f88c4
Pierre Allix <pierre.allix@liqd.de>
2015-07-14 15:43:37 +0200

    Remove old test

--- src/adhocracy_core/adhocracy_core/test_interfaces.py
+++ src/adhocracy_core/adhocracy_core/test_interfaces.py
@@ -44,45 +44,6 @@ class SheetToSheetUnitTests(unittest.TestCase):
         assert Reference.getTaggedValue('target_isheet') is IA
 
 
-class TestResourceMetadata:
-
-    class ISample:
-        pass
-
-    class ISampleSheetA:
-        pass
-
-    class ISampleSheetB:
-        pass
-
-    def make_one(self):
-        from .interfaces import ResourceMetadata
-        meta = ResourceMetadata(content_name='',
-                                iresource=self.ISample,
-                                content_class=None,
-                                permission_create='',
-                                is_implicit_addable=False,
-                                basic_sheets=(),
-                                extended_sheets=(self.ISampleSheetA,),
-                                after_creation=(),
-                                use_autonaming=False,
-                                autonaming_prefix='',
-                                use_autonaming_random=False,
-                                element_types=(),
-                                item_type=False,
-        )
-        return meta
-
-    def after_creation_sample(self):
-        pass
-
-    def test_add(self):
-        meta = self.make_one()
-        new_meta = meta._add(extended_sheets=(self.ISampleSheetB,),
-                             after_creation=(self.after_creation_sample,))
-        assert new_meta.extended_sheets == (self.ISampleSheetA, self.ISampleSheetB)
-        assert new_meta.after_creation == (self.after_creation_sample,)
-
 class TestNamedTuple:
 
     class ISample:
