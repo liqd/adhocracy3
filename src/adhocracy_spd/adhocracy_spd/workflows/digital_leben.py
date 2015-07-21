c1dc571faa9bf2ae0e0a580365943806826ced4a
Pierre Allix <pierre.allix@liqd.de>
2015-07-21 12:31:32 +0200

    Make flake8 happy for spd

--- src/adhocracy_spd/adhocracy_spd/workflows/digital_leben.py
+++ src/adhocracy_spd/adhocracy_spd/workflows/digital_leben.py
@@ -2,17 +2,17 @@
 from adhocracy_core.workflows import add_workflow
 from adhocracy_core.workflows.standard import standard_meta
 
-digital_leben_meta =  standard_meta \
-                      .transform(('states', 'participate', 'acm'),
-                                 {'principals': [                   'participant', 'moderator', 'creator', 'initiator'],  # noqa
-                                'permissions':
-                                  [['create_proposal',             None,          None,        None,     'Allow'],  # noqa
-                                   ['edit_proposal',               None,          None,       'Allow',   'Allow'],  # noqa
-                                   ['create_comment',             'Allow',       'Allow',      None,     'Allow'],  # noqa
-                                   ['edit_comment',                None,          None,       'Allow',    None  ],  # noqa
-                                   ['create_rate',                'Allow',        None,        None,      None  ],  # noqa
-                                   ['edit_rate',                   None,          None,       'Allow',    None  ],  # noqa
-                                   ]})
+digital_leben_meta = standard_meta \
+                     .transform(('states', 'participate', 'acm'),
+                                {'principals':                  ['participant', 'moderator', 'creator', 'initiator'],  # noqa
+                                 'permissions':
+                                 [['create_proposal',             None,          None,        None,     'Allow'],  # noqa
+                                  ['edit_proposal',               None,          None,       'Allow',   'Allow'],  # noqa
+                                  ['create_comment',             'Allow',       'Allow',      None,     'Allow'],  # noqa
+                                  ['edit_comment',                None,          None,       'Allow',    None],  # noqa
+                                  ['create_rate',                'Allow',        None,        None,      None],  # noqa
+                                  ['edit_rate',                   None,          None,       'Allow',    None],  # noqa
+                                 ]})
 
 
 def includeme(config):
