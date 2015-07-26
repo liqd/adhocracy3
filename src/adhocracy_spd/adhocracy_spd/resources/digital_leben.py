4734a140f245bf3f13d6fb8f3108ef320fa57c39
Joscha Krutzki <joka@jokasis.de>
2015-07-26 16:00:16 +0200

    rm sheet adhcoracy_spd.sheets.digital_leben.IWorkflowAssignment API CHANGE

--- src/adhocracy_spd/adhocracy_spd/resources/digital_leben.py
+++ src/adhocracy_spd/adhocracy_spd/resources/digital_leben.py
@@ -1,7 +1,6 @@
 """Resource types for digital leben process."""
 from adhocracy_core.resources import add_resource_type_to_registry
 from adhocracy_core.resources import document_process
-import adhocracy_spd.sheets.digital_leben
 
 
 class IProcess(document_process.IDocumentProcess):
@@ -11,9 +10,7 @@ class IProcess(document_process.IDocumentProcess):
 
 process_meta = document_process.document_process_meta._replace(
     iresource=IProcess,
-    extended_sheets=(
-        adhocracy_spd.sheets.digital_leben.IWorkflowAssignment,
-    ),
+    workflow_name='digital_leben'
 )
 
 
--- src/adhocracy_spd/adhocracy_spd/sheets/digital_leben.py
+++ src/adhocracy_spd/adhocracy_spd/sheets/digital_leben.py
@@ -1,5 +1,6 @@
 """Sheets for digital leven process."""
-from adhocracy_core.sheets import add_sheet_to_registry
+from zope.deprecation import deprecated
+
 from adhocracy_core.sheets import workflow
 
 
@@ -8,25 +9,5 @@ class IWorkflowAssignment(workflow.IWorkflowAssignment):
     """Marker interface for the digital leben workflow assignment sheet."""
 
 
-class WorkflowAssignmentSchema(workflow.WorkflowAssignmentSchema):
-
-    """Data structure the digital_leben workflow assignment sheet."""
-
-    workflow_name = 'digital_leben'
-
-    draft = workflow.StateAssignment()
-    participate = workflow.StateAssignment()
-    evaluate = workflow.StateAssignment()
-    result = workflow.StateAssignment()
-    closed = workflow.StateAssignment()
-
-
-workflow_meta = workflow.workflow_meta._replace(
-    isheet=IWorkflowAssignment,
-    schema_class=WorkflowAssignmentSchema,
-)
-
-
-def includeme(config):
-    """Register sheets."""
-    add_sheet_to_registry(workflow_meta, config.registry)
+deprecated('IWorkflowAssignment',
+           'Backward compatible code use process IWorkflowAssignment instead')
