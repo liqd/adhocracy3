6cb4703b83e39543b7acbce3c89224cc3d98be6e
Pierre Allix <pierre.allix@liqd.de>
2015-07-27 15:34:21 +0200

    Move geolocalisable proposal resource to core

--- src/adhocracy_meinberlin/adhocracy_meinberlin/resources/alexanderplatz.py
+++ src/adhocracy_meinberlin/adhocracy_meinberlin/resources/alexanderplatz.py
@@ -3,34 +3,13 @@
 from adhocracy_core.interfaces import ITag
 from adhocracy_core.resources import add_resource_type_to_registry
 from adhocracy_core.resources import document
-from adhocracy_core.resources import proposal
+from adhocracy_core.resources.proposal import IGeoProposal
 from adhocracy_core.resources.document_process import IDocumentProcess
 from adhocracy_core.resources.document_process import document_process_meta
 from adhocracy_core.resources.paragraph import IParagraph
 from adhocracy_core.sheets.geo import ILocationReference
 from adhocracy_core.sheets.geo import IPoint
 from adhocracy_core.sheets import workflow
-
-
-class IProposalVersion(proposal.IProposalVersion):
-
-    """Alexanderplatz proposal version."""
-
-
-proposal_version_meta = proposal.proposal_version_meta._replace(
-    iresource=IProposalVersion,
-)._add(extended_sheets=(IPoint,))
-
-
-class IProposal(proposal.IProposal):
-
-    """Alexanderplatz proposal versions pool."""
-
-proposal_meta = proposal.proposal_meta._replace(
-    iresource=IProposal,
-    element_types=(IProposalVersion,),
-    item_type=IProposalVersion,
-)
 
 
 class IDocumentVersion(document.IDocumentVersion):
@@ -64,7 +43,7 @@ class IProcess(IDocumentProcess):
 
 process_meta = document_process_meta._replace(
     iresource=IProcess,
-    element_types=(IProposal,
+    element_types=(IGeoProposal,
                    IDocument),
     extended_sheets=(workflow.IStandard,
                      ILocationReference,)
@@ -73,8 +52,6 @@ process_meta = document_process_meta._replace(
 
 def includeme(config):
     """Add resource type to content."""
-    add_resource_type_to_registry(proposal_meta, config)
-    add_resource_type_to_registry(proposal_version_meta, config)
     add_resource_type_to_registry(process_meta, config)
     add_resource_type_to_registry(document_version_meta, config)
     add_resource_type_to_registry(document_meta, config)
