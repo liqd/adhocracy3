cfa1ce037a1621a9222e1c58b0605b2c4fdf36b1
Pierre Allix <pierre.allix@liqd.de>
2015-07-29 15:36:41 +0200

    Gardening

--- src/adhocracy_core/adhocracy_core/scripts/test_assign_badges.py
+++ src/adhocracy_core/adhocracy_core/scripts/test_assign_badges.py
@@ -10,18 +10,28 @@ class TestAssignBadges:
 
     @fixture
     def context(self, pool, service):
+        from substanced.interfaces import IFolder
         pool['principals'] = service
         pool['principals']['users'] = deepcopy(service)
         pool['organisation'] = deepcopy(pool)
         pool['organisation']['badges'] = service
+        user = testing.DummyResource()
+        pool['principals']['users']['0000000'] = user
+        badge = testing.DummyResource()
+        pool['organisation']['badges']['winning'] = badge
+        # without the __provides__, the find_service blocks and does not return!
+        item = testing.DummyResource(__provides__=IFolder)
+        pool['organisation']['Winning_123'] = item
+        badgeable = testing.DummyResource()
+        pool['organisation']['Winning_123']['VERSION_0000001'] = badgeable
+        badge_assignments_service = deepcopy(service)
+        pool['organisation']['Winning_123']['badge_assignments'] = badge_assignments_service
         return pool
 
-    def test_create_badge_assignment(self, service, context):
+    def write_json(self):
         import json
         from tempfile import mkstemp
-        from .assign_badges import _import_assignments
-        from substanced.interfaces import IFolder
-        import adhocracy_core.resources.badge
+
         self._tempfd, filename = mkstemp()
         with open(filename, 'w') as f:
             f.write(json.dumps([
@@ -31,20 +41,21 @@ class TestAssignBadges:
                  "description" : "## Lorem ipsum"
                 }
             ]))
-        user = testing.DummyResource()
-        context['principals']['users']['0000000'] = user
-        badge = testing.DummyResource()
-        context['organisation']['badges']['winning'] = badge
-        # without the __provides__, the find_service blocks and does not return!
-        item = testing.DummyResource(__provides__=IFolder)
-        context['organisation']['Winning_123'] = item
-        badgeable = testing.DummyResource()
-        context['organisation']['Winning_123']['VERSION_0000001'] = badgeable
-        badge_assignments_service = deepcopy(service)
-        context['organisation']['Winning_123']['badge_assignments'] = badge_assignments_service
+        return filename
+
+    def test_create_badge_assignment(self, context):
+        from .assign_badges import _import_assignments
+        import adhocracy_core.resources.badge
+
+        filename = self.write_json()
         registry = Mock()
 
         _import_assignments(context, registry, filename)
+
+        user = context['principals']['users']['0000000']
+        badge = context['organisation']['badges']['winning']
+        badgeable = context['organisation']['Winning_123']['VERSION_0000001']
+        badge_assignments_service = context['organisation']['Winning_123']['badge_assignments']
 
         registry.content.create.assert_called_with(
             adhocracy_core.resources.badge.IBadgeAssignment.__identifier__,
