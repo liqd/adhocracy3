9e5013fecc7372b914b216788672edefafc977f2
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-08-07 13:36:18 +0200

    add alexanderplatz context

--- src/meinberlin/meinberlin/static/js/Adhocracy.ts
+++ src/meinberlin/meinberlin/static/js/Adhocracy.ts
@@ -114,7 +114,7 @@ export var init = (config : AdhConfig.IService, metaApi) => {
         adhTopLevelStateProvider
             .when("", ["$location", ($location) : AdhTopLevelState.IAreaInput => {
                 $location.replace();
-                $location.path("/r/organisation/kiezkasse/");
+                $location.path("/r/organisation/alexanderplatz/");
                 return {
                     skip: true
                 };
@@ -129,7 +129,7 @@ export var init = (config : AdhConfig.IService, metaApi) => {
                 resourceUrl: "/principals/users/"
             })
             .space("content", {
-                resourceUrl: "/organisation/kiezkasse/"
+                resourceUrl: "/organisation/alexanderplatz/"
             });
     }]);
     app.config(["$compileProvider", ($compileProvider) => {
