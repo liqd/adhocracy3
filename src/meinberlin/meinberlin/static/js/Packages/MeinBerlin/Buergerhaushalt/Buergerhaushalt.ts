51d682dd8971d1010b517295275ec6772bf255c5
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-08-18 14:03:57 +0200

    add Buergerhaushalt module

---
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Buergerhaushalt/Buergerhaushalt.ts
@@ -0,0 +1,14 @@
+import AdhMeinBerlinBuergerhaushaltWorkbench = require("./Workbench/Workbench");
+
+export var moduleName = "adhMeinBerlinBuergerhaushalt";
+
+export var register = (angular) => {
+
+
+    angular
+        .module(moduleName, [
+
+        ]);
+};
+
+
--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/MeinBerlin.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/MeinBerlin.ts
@@ -1,6 +1,7 @@
 import AdhMeinBerlinKiezkassen = require("./Kiezkassen/Kiezkassen");
 import AdhMeinBerlinBplaene = require("./Bplaene/Bplaene");
 import AdhMeinBerlinAlexanderplatz = require("./Alexanderplatz/Alexanderplatz");
+import AdhMeinBerlinBuergerhaushalt = require("./Buergerhaushalt/Buergerhaushalt");
 
 
 export var moduleName = "adhMeinBerlin";
@@ -9,11 +10,13 @@ export var register = (angular) => {
     AdhMeinBerlinKiezkassen.register(angular);
     AdhMeinBerlinBplaene.register(angular);
     AdhMeinBerlinAlexanderplatz.register(angular);
+    AdhMeinBerlinBuergerhaushalt.register(angular);
 
     angular
         .module(moduleName, [
             AdhMeinBerlinAlexanderplatz.moduleName,
             AdhMeinBerlinBplaene.moduleName,
-            AdhMeinBerlinKiezkassen.moduleName
+            AdhMeinBerlinKiezkassen.moduleName,
+            AdhMeinBerlinBuergerhaushalt.moduleName
         ]);
 };
