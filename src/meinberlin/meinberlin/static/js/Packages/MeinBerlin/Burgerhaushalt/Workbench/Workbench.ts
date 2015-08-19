80a331694e836ff536c16b05b7a270bff7be0c34
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-08-19 19:33:15 +0200

    add burgerhaushalt workbench
    mostly copied from kiezkassen

--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Burgerhaushalt/Workbench/Workbench.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Burgerhaushalt/Workbench/Workbench.ts
@@ -1,6 +1,251 @@
-export var moduleName = "adhMeinBerlinBurgerhaushaltWorkbench";
+/// <reference path="../../../../../lib/DefinitelyTyped/angularjs/angular.d.ts"/>
+
+import AdhAbuse = require("../../../Abuse/Abuse");
+import AdhComment = require("../../../Comment/Comment");
+import AdhConfig = require("../../../Config/Config");
+import AdhHttp = require("../../../Http/Http");
+import AdhMovingColumns = require("../../../MovingColumns/MovingColumns");
+import AdhProcess = require("../../../Process/Process");
+import AdhResourceArea = require("../../../ResourceArea/ResourceArea");
+import AdhTopLevelState = require("../../../TopLevelState/TopLevelState");
+import AdhUtil = require("../../../Util/Util");
+import AdhPermissions = require("../../../Permissions/Permissions");
+
+import AdhMeinBerlinBurgerhaushaltProcess = require("../Process/Process");
+import AdhMeinBerlinProposal = require("../../../Proposal/Proposal");
+
+import RIComment = require("../../../../Resources_/adhocracy_core/resources/comment/IComment");
+import RICommentVersion = require("../../../../Resources_/adhocracy_core/resources/comment/ICommentVersion");
+import RIBurgerhaushaltProcess = require("../../../../Resources_/adhocracy_meinberlin/resources/burgerhaushalt/IProcess");
+import RIProposal = require("../../../../Resources_/adhocracy_meinberlin/resources/burgerhaushalt/IProposal");
+import RIProposalVersion = require("../../../../Resources_/adhocracy_meinberlin/resources/burgerhaushalt/IProposalVersion");
+import SIComment = require("../../../../Resources_/adhocracy_core/sheets/comment/IComment");
+import SIWorkflow = require("../../../../Resources_/adhocracy_core/sheets/workflow/IWorkflowAssignment");
+
+var pkgLocation = "/MeinBerlin/Burgerhaushalt/Workbench";
+
+
+export var meinBerlinWorkbenchDirective = (
+    adhTopLevelState : AdhTopLevelState.Service,
+    adhConfig : AdhConfig.IService,
+    adhHttp : AdhHttp.Service<any>
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/Workbench.html",
+        link: (scope) => {
+            scope.$on("$destroy", adhTopLevelState.bind("view", scope));
+            scope.$on("$destroy", adhTopLevelState.bind("processUrl", scope));
+            scope.$on("$destroy", adhTopLevelState.bind("contentType", scope));
+
+            scope.views = {
+                process: "default",
+                proposal: "default",
+                comment: "default"
+            };
+
+            scope.$watchGroup(["contentType", "view"], (values) => {
+                var contentType = values[0];
+                var view = values[1];
+
+                if (contentType === RIBurgerhaushaltProcess.content_type) {
+                    scope.views.process = view;
+                } else {
+                    scope.views.process = "default";
+                }
+
+                if (contentType === RIProposal.content_type || contentType === RIProposalVersion.content_type) {
+                    scope.views.proposal = view;
+                } else {
+                    scope.views.proposal = "default";
+                }
+            });
+
+            scope.$watch("processUrl", (processUrl) => {
+                if (processUrl) {
+                    adhHttp.get(processUrl).then((resource) => {
+                        scope.currentPhase = resource.data[SIWorkflow.nick].workflow_state;
+                    });
+                }
+            });
+        }
+
+    };
+};
+
+export var burgerhaushaltProposalDetailColumnDirective = (
+    adhConfig : AdhConfig.IService,
+    adhPermissions : AdhPermissions.Service
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/BurgerhaushaltProposalDetailColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl", "proposalUrl"]);
+            adhPermissions.bindScope(scope, () => scope.proposalUrl && AdhUtil.parentPath(scope.proposalUrl), "proposalItemOptions");
+        }
+    };
+};
+
+export var burgerhaushaltProposalCreateColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/BurgerhaushaltProposalCreateColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl"]);
+        }
+    };
+};
+
+export var burgerhaushaltProposalEditColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/BurgerhaushaltProposalEditColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl", "proposalUrl"]);
+        }
+    };
+};
+
+export var burgerhaushaltDetailColumnDirective = (
+    adhConfig : AdhConfig.IService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/BurgerhaushaltDetailColumn.html",
+        require: "^adhMovingColumn",
+        link: (scope, element, attrs, column : AdhMovingColumns.MovingColumnController) => {
+            column.bindVariablesAndClear(scope, ["processUrl"]);
+            scope.shared = column.$scope.shared;
+            scope.showMap = (isShowMap) => {
+                scope.shared.isShowMap = isShowMap;
+            };
+        }
+    };
+};
+
+export var registerRoutes = (
+    processType : string = "",
+    context : string = ""
+) => (adhResourceAreaProvider : AdhResourceArea.Provider) => {
+    adhResourceAreaProvider
+        .default(RIBurgerhaushaltProcess, "", processType, context, {
+            space: "content",
+            movingColumns: "is-show-hide-hide"
+        })
+        .default(RIBurgerhaushaltProcess, "create_proposal", processType, context, {
+            space: "content",
+            movingColumns: "is-show-show-hide"
+        })
+        .specific(RIBurgerhaushaltProcess, "create_proposal", processType, context, [
+            "adhHttp", (adhHttp : AdhHttp.Service<any>) => (resource : RIBurgerhaushaltProcess) => {
+                return adhHttp.options(resource.path).then((options : AdhHttp.IOptions) => {
+                    if (!options.POST) {
+                        throw 401;
+                    } else {
+                        return {};
+                    }
+                });
+            }])
+        .defaultVersionable(RIProposal, RIProposalVersion, "edit", processType, context, {
+            space: "content",
+            movingColumns: "is-show-show-hide"
+        })
+        .specificVersionable(RIProposal, RIProposalVersion, "edit", processType, context, [
+            "adhHttp", (adhHttp : AdhHttp.Service<any>) => (item : RIProposal, version : RIProposalVersion) => {
+                return adhHttp.options(item.path).then((options : AdhHttp.IOptions) => {
+                    if (!options.POST) {
+                        throw 401;
+                    } else {
+                        return {
+                            proposalUrl: version.path
+                        };
+                    }
+                });
+            }])
+        .defaultVersionable(RIProposal, RIProposalVersion, "", processType, context, {
+            space: "content",
+            movingColumns: "is-show-show-hide"
+        })
+        .specificVersionable(RIProposal, RIProposalVersion, "", processType, context, [
+            () => (item : RIProposal, version : RIProposalVersion) => {
+                return {
+                    proposalUrl: version.path
+                };
+            }])
+        .defaultVersionable(RIProposal, RIProposalVersion, "comments", processType, context, {
+            space: "content",
+            movingColumns: "is-collapse-show-show"
+        })
+        .specificVersionable(RIProposal, RIProposalVersion, "comments", processType, context, [
+            () => (item : RIProposal, version : RIProposalVersion) => {
+                return {
+                    commentableUrl: version.path,
+                    commentCloseUrl: version.path,
+                    proposalUrl: version.path
+                };
+            }])
+        .defaultVersionable(RIComment, RICommentVersion, "", processType, context, {
+            space: "content",
+            movingColumns: "is-collapse-show-show"
+        })
+        .specificVersionable(RIComment, RICommentVersion, "", processType, context, ["adhHttp", "$q", (
+            adhHttp : AdhHttp.Service<any>,
+            $q : angular.IQService
+        ) => {
+            var getCommentableUrl = (resource) : angular.IPromise<any> => {
+                if (resource.content_type !== RICommentVersion.content_type) {
+                    return $q.when(resource);
+                } else {
+                    var url = resource.data[SIComment.nick].refers_to;
+                    return adhHttp.get(url).then(getCommentableUrl);
+                }
+            };
+
+            return (item : RIComment, version : RICommentVersion) => {
+                return getCommentableUrl(version).then((commentable) => {
+                    return {
+                        commentableUrl: commentable.path,
+                        commentCloseUrl: commentable.path,
+                        proposalUrl: commentable.path
+                    };
+                });
+            };
+        }]);
+};
+
+export var moduleName = "adhMeinBerlinWorkbench";
 
 export var register = (angular) => {
     angular
-        .module(moduleName, []);
+        .module(moduleName, [
+            AdhAbuse.moduleName,
+            AdhComment.moduleName,
+            AdhHttp.moduleName,
+            AdhMeinBerlinBurgerhaushaltProcess.moduleName,
+            AdhMeinBerlinProposal.moduleName,
+            AdhMovingColumns.moduleName,
+            AdhProcess.moduleName,
+            AdhResourceArea.moduleName,
+            AdhTopLevelState.moduleName
+        ])
+        .config(["adhResourceAreaProvider", registerRoutes(RIBurgerhaushaltProcess.content_type)])
+        .config(["adhProcessProvider", (adhProcessProvider : AdhProcess.Provider) => {
+            adhProcessProvider.templateFactories[RIBurgerhaushaltProcess.content_type] = ["$q", ($q : angular.IQService) => {
+                return $q.when("<adh-mein-berlin-burgerhaushalt-workbench></adh-mein-berlin-burgerhaushalt-workbench>");
+            }];
+        }])
+        .directive("adhMeinBerlinBurgerhaushaltWorkbench", ["adhTopLevelState", "adhConfig", "adhHttp", meinBerlinWorkbenchDirective])
+        .directive("adhMeinBerlinBurgerhaushaltProposalDetailColumn", [
+            "adhConfig", "adhPermissions", burgerhaushaltProposalDetailColumnDirective])
+        .directive("adhMeinBerlinBurgerhaushaltProposalCreateColumn", ["adhConfig", burgerhaushaltProposalCreateColumnDirective])
+        .directive("adhMeinBerlinBurgerhaushaltProposalEditColumn", ["adhConfig", burgerhaushaltProposalEditColumnDirective])
+        .directive("adhMeinBerlinBurgerhaushaltDetailColumn", ["adhConfig", burgerhaushaltDetailColumnDirective]);
 };
