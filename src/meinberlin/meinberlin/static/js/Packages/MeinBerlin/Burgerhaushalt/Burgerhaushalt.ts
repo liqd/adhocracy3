c2f814e8d434d76f92ed9b9554d8fd458f6d13a3
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-08-19 19:34:43 +0200

    add burgerhaushalt context
    mostly copied from kiezkassen

--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Burgerhaushalt/Burgerhaushalt.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Burgerhaushalt/Burgerhaushalt.ts
@@ -1,3 +1,4 @@
+import AdhMeinBerlinBurgerhaushaltContext = require("./Context/Context");
 import AdhMeinBerlinBurgerhaushaltProcess = require("./Process/Process");
 import AdhMeinBerlinBurgerhaushaltWorkbench = require("./Workbench/Workbench");
 
@@ -5,11 +6,13 @@ import AdhMeinBerlinBurgerhaushaltWorkbench = require("./Workbench/Workbench");
 export var moduleName = "adhMeinBerlinBurgerhaushalt";
 
 export var register = (angular) => {
+    AdhMeinBerlinBurgerhaushaltContext.register(angular);
     AdhMeinBerlinBurgerhaushaltProcess.register(angular);
     AdhMeinBerlinBurgerhaushaltWorkbench.register(angular);
 
     angular
         .module(moduleName, [
+            AdhMeinBerlinBurgerhaushaltContext.moduleName,
             AdhMeinBerlinBurgerhaushaltProcess.moduleName,
             AdhMeinBerlinBurgerhaushaltWorkbench.moduleName
         ]);
