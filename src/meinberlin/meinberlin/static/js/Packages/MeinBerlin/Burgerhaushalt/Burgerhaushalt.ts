b80edd3cc784dccd37334f127f339371326c8bca
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-08-19 19:21:45 +0200

    add burgerhaushalt process widgets
    mostly copied from kiezkassen

--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Burgerhaushalt/Burgerhaushalt.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Burgerhaushalt/Burgerhaushalt.ts
@@ -1,13 +1,16 @@
+import AdhMeinBerlinBurgerhaushaltProcess = require("./Process/Process");
 import AdhMeinBerlinBurgerhaushaltWorkbench = require("./Workbench/Workbench");
 
 
 export var moduleName = "adhMeinBerlinBurgerhaushalt";
 
 export var register = (angular) => {
+    AdhMeinBerlinBurgerhaushaltProcess.register(angular);
     AdhMeinBerlinBurgerhaushaltWorkbench.register(angular);
 
     angular
         .module(moduleName, [
+            AdhMeinBerlinBurgerhaushaltProcess.moduleName,
             AdhMeinBerlinBurgerhaushaltWorkbench.moduleName
         ]);
 };
