3af03ce0635a90c946a362ef25b99653a5ad8171
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-08-19 10:44:36 +0200

    rename to Burgerhaushalt for consitency

---
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Burgerhaushalt/Burgerhaushalt.ts
@@ -0,0 +1,14 @@
+import AdhMeinBerlinBurgerhaushaltWorkbench = require("./Workbench/Workbench");
+
+export var moduleName = "adhMeinBerlinBurgerhaushalt";
+
+export var register = (angular) => {
+
+
+    angular
+        .module(moduleName, [
+
+        ]);
+};
+
+
--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/MeinBerlin.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/MeinBerlin.ts
@@ -1,7 +1,7 @@
 import AdhMeinBerlinKiezkassen = require("./Kiezkassen/Kiezkassen");
 import AdhMeinBerlinBplaene = require("./Bplaene/Bplaene");
 import AdhMeinBerlinAlexanderplatz = require("./Alexanderplatz/Alexanderplatz");
-import AdhMeinBerlinBuergerhaushalt = require("./Buergerhaushalt/Buergerhaushalt");
+import AdhMeinBerlinBurgerhaushalt = require("./Burgerhaushalt/Burgerhaushalt");
 
 
 export var moduleName = "adhMeinBerlin";
@@ -10,13 +10,13 @@ export var register = (angular) => {
     AdhMeinBerlinKiezkassen.register(angular);
     AdhMeinBerlinBplaene.register(angular);
     AdhMeinBerlinAlexanderplatz.register(angular);
-    AdhMeinBerlinBuergerhaushalt.register(angular);
+    AdhMeinBerlinBurgerhaushalt.register(angular);
 
     angular
         .module(moduleName, [
             AdhMeinBerlinAlexanderplatz.moduleName,
             AdhMeinBerlinBplaene.moduleName,
             AdhMeinBerlinKiezkassen.moduleName,
-            AdhMeinBerlinBuergerhaushalt.moduleName
+            AdhMeinBerlinBurgerhaushalt.moduleName
         ]);
 };
