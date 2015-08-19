464beca439377b3ef79489f03cdb3b6c6b584273
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-08-19 10:46:47 +0200

    Gardening

--- src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Burgerhaushalt/Burgerhaushalt.ts
+++ src/meinberlin/meinberlin/static/js/Packages/MeinBerlin/Burgerhaushalt/Burgerhaushalt.ts
@@ -1,14 +1,10 @@
 import AdhMeinBerlinBurgerhaushaltWorkbench = require("./Workbench/Workbench");
 
+
 export var moduleName = "adhMeinBerlinBurgerhaushalt";
 
 export var register = (angular) => {
-
-
     angular
         .module(moduleName, [
-
         ]);
 };
-
-
