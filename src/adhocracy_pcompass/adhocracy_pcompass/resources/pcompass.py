00f81d56986fa081ec4353e845ca1b2dae754cb6
Pierre Allix <pierre.allix@liqd.de>
2015-09-17 13:48:07 +0200

    Add process resource for policy compass

---
+++ src/adhocracy_pcompass/adhocracy_pcompass/resources/pcompass.py
@@ -0,0 +1,21 @@
+"""Policy compass process resources."""
+
+from adhocracy_core.resources import add_resource_type_to_registry
+from adhocracy_core.resources import process
+from adhocracy_core.resources.proposal import IProposal
+
+
+class IProcess(process.IProcess):
+
+    """Policy Compass participation process."""
+
+process_meta = process.process_meta._replace(
+    iresource=IProcess,
+    element_types=(IProposal,),
+    workflow_name='standard',
+)
+
+
+def includeme(config):
+    """Add resource type to content."""
+    add_resource_type_to_registry(process_meta, config)
