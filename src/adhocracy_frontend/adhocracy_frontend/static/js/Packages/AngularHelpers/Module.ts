55771346f5ccf3080ec6b9edb943f660fafe41b7
Caroline Clifford <me@carolineclifford.com>
2015-10-05 17:03:22 +0200

    Added percentage filter

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/AngularHelpers/Module.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/AngularHelpers/Module.ts
@@ -15,7 +15,7 @@ export var register = (angular) => {
         ])
         .filter("join", () => (list : any[], separator : string = ", ") : string => list.join(separator))
         .filter("signum", () => (n : number) : string => (typeof n === "number") ? (n > 0 ? "+" + n.toString() : n.toString()) : "0")
-        .filter("percentage", () => (i : number, t : number) : string => Math.round((i/t) * 100).toString() + "%")
+        .filter("percentage", () => (i : number, t : number) : string => Math.round((i / t) * 100).toString() + "%")
         .filter("numberOrDash", () => AdhAngularHelpers.numberOrDashFilter)
         .factory("adhRecursionHelper", ["$compile", AdhAngularHelpers.recursionHelper])
         .factory("adhShowError", () => AdhAngularHelpers.showError)
