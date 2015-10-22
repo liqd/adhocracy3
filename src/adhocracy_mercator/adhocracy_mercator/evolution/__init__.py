99140b258f16238402ced0d91d18600b9606fca7
Pierre Allix <pierre.allix@liqd.de>
2015-10-22 14:39:05 +0200

    Add migration making mercator proposals badgeable

--- src/adhocracy_mercator/adhocracy_mercator/evolution/__init__.py
+++ src/adhocracy_mercator/adhocracy_mercator/evolution/__init__.py
@@ -11,8 +11,10 @@ from adhocracy_core.evolution import migrate_new_iresource
 from adhocracy_core.evolution import migrate_new_sheet
 from adhocracy_core.interfaces import search_query
 from adhocracy_core.resources.badge import add_badge_assignments_service
+from adhocracy_core.resources.badge import add_badges_service
 from adhocracy_core.resources.logbook import add_logbook_service
 from adhocracy_core.sheets.badge import IBadgeable
+from adhocracy_core.sheets.badge import IHasBadgesPool
 from adhocracy_core.sheets.logbook import IHasLogbookPool
 from adhocracy_core.sheets.metadata import IMetadata
 from adhocracy_core.utils import get_sheet_field
@@ -142,6 +144,29 @@ def remove_mercator_workflow_assignment_sheet(root):  # pragma: no cover
                       )
 
 
+@log_migration
+def make_mercator_proposals_badgeable(root):  # pragma: no cover
+    """Add badge services processes and make mercator proposals badgeable."""
+    from adhocracy_core.evolution import _search_for_interfaces
+    from adhocracy_mercator.resources.mercator import IProcess
+    catalogs = find_service(root, 'catalogs')
+    proposals = _search_for_interfaces(catalogs, IMercatorProposal)
+    registry = get_current_registry(root)
+    for proposal in proposals:
+        if not IBadgeable.providedBy(proposal):
+            logger.info('add badgeable interface to {0}'.format(proposal))
+            alsoProvides(proposal, IBadgeable)
+        if 'badge_assignments' not in proposal:
+            logger.info('add badge assignments to {0}'.format(proposal))
+            add_badge_assignments_service(proposal, registry, {})
+    processes = _search_for_interfaces(catalogs, IProcess)
+    for process in processes:
+        if not IHasBadgesPool.providedBy(process):
+            logger.info('Add badges service to {0}'.format(process))
+            add_badges_service(process, registry, {})
+            alsoProvides(process, IHasBadgesPool)
+
+
 def includeme(config):  # pragma: no cover
     """Register evolution utilities and add evolution steps."""
     config.add_evolution_step(evolve1_add_ititle_sheet_to_proposals)
@@ -155,3 +180,4 @@ def includeme(config):  # pragma: no cover
     config.add_evolution_step(add_logbook_service_to_proposal_items)
     config.add_evolution_step(add_haslogbookpool_sheet_to_proposal_versions)
     config.add_evolution_step(remove_mercator_workflow_assignment_sheet)
+    config.add_evolution_step(make_mercator_proposals_badgeable)
