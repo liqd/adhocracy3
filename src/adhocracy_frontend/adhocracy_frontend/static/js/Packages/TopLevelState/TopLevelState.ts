8dda53af7fa34517aec7bc176a786c2cdd26e435
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-10-23 14:13:13 +0200

    make header optional

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/TopLevelState/TopLevelState.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/TopLevelState/TopLevelState.ts
@@ -521,6 +521,7 @@ export var pageWrapperDirective = (adhConfig : AdhConfig.IService) => {
         transclude: true,
         templateUrl: adhConfig.pkg_path + pkgLocation + "/templates/" + "Wrapper.html",
         link: (scope) => {
+            scope.hide_header = adhConfig["custom"]["hide_header"];
             scope.headerTemplateUrl = adhConfig.pkg_path + pkgLocation + "/templates/" + "Header.html";
         }
     };
