0cfeaa277ba42a6b1db4dc20353a24f0f7bb8709
Pierre Allix <pierre.allix@liqd.de>
2015-10-26 18:51:17 +0100

    Fix missing reindexing in mercator migrations

--- src/adhocracy_mercator/adhocracy_mercator/evolution/__init__.py
+++ src/adhocracy_mercator/adhocracy_mercator/evolution/__init__.py
@@ -36,6 +36,7 @@ def evolve1_add_ititle_sheet_to_proposals(root):  # pragma: no cover
         & interfaces.eq(IMercatorProposalVersion) \
         & interfaces.noteq(ITitle)
     proposals = query.execute()
+    catalogs = find_service(root, 'catalogs')
     for proposal in proposals:
         logger.info('updating {0}'.format(proposal))
         introduction = get_sheet_field(proposal, IMercatorSubResources,
@@ -43,6 +44,7 @@ def evolve1_add_ititle_sheet_to_proposals(root):  # pragma: no cover
         if introduction == '' or introduction is None:
             continue
         alsoProvides(proposal, ITitle)
+        catalogs.reindex_index(proposal, 'interfaces')
         sheet = registry.content.get_sheet(introduction, IIntroduction)
         if 'title' not in sheet.get().keys():
             continue
