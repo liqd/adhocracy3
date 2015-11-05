2b3fec21aec04c0300fbc02dc6b32d9c14da51b2
Joscha Krutzki <joka@jokasis.de>
2015-11-05 13:57:56 +0100

    smal performance fix for request without authentication, added fixme

--- src/adhocracy_core/adhocracy_core/authentication/__init__.py
+++ src/adhocracy_core/adhocracy_core/authentication/__init__.py
@@ -206,6 +206,8 @@ class TokenHeaderAuthenticationPolicy(CallbackAuthenticationPolicy):
         settings = request.registry.settings
         if not asbool(settings.get('adhocracy.validate_user_token', True)):
             return userid
+        if token is None:
+            raise KeyError
         with statsd_timer('authenticationuser', rate=.1):
             authenticated_userid = \
                 tokenmanager.get_user_id(token, timeout=self.timeout)
@@ -250,6 +252,8 @@ class TokenHeaderAuthenticationPolicy(CallbackAuthenticationPolicy):
         if cached_principals:
             return cached_principals
         if self.authenticated_userid(request) is None:
+            # FIXME this should go to principals.groups_and_roles_finder
+            # to make adhocracy work with other authentication polices.
             return [Everyone, Anonymous]
         principals = super().effective_principals(request)
         request.__cached_principals__ = principals
