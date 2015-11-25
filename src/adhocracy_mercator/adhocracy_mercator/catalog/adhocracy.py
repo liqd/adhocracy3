56133f33a1f38fa0575def1a93f72778d7be99e6
Pierre Allix <pierre.allix@liqd.de>
2015-11-25 17:14:21 +0100

    Add indexes for mercator2

--- src/adhocracy_mercator/adhocracy_mercator/catalog/adhocracy.py
+++ src/adhocracy_mercator/adhocracy_mercator/catalog/adhocracy.py
@@ -8,6 +8,7 @@ from adhocracy_core.utils import get_sheet_field
 from adhocracy_mercator.sheets.mercator import IMercatorSubResources
 from adhocracy_mercator.sheets.mercator import IFinance
 from adhocracy_mercator.sheets.mercator import ILocation
+from adhocracy_mercator import sheets
 
 
 INDEX_EXAMPLE_VALUES['mercator_requested_funding'] = 1
@@ -71,6 +72,49 @@ def index_budget(resource: IResource, default) -> str:
     return ['above_50000']
 
 
+def mercator2_index_location(resource, default) -> list:
+    """Return search index keywords based on the location's fields."""
+    locations = []
+    if get_sheet_field(resource,
+                       sheets.mercator2.ILocation,
+                       'is_online'):
+        locations.append('online')
+    if get_sheet_field(resource,
+                       sheets.mercator2.ILocation,
+                       'has_link_to_ruhr'):
+        locations.append('linked_to_ruhr')
+    if get_sheet_field(resource,
+                       sheets.mercator2.ILocation,
+                       'location') is not '':
+        locations.append('specific')
+    return locations if locations else default
+
+
+def mercator2_index_requested_funding(resource: IResource, default) -> int:
+    """Return search index keyword based on the "requested_funding" field."""
+    requested_funding = get_sheet_field(resource,
+                                        sheets.mercator2.IFinancialPlanning,
+                                        'requested_funding')
+    for limit in BUDGET_INDEX_LIMIT_KEYWORDS:
+        if requested_funding <= limit:
+            return [limit]
+    return default
+
+
+def mercator2_index_budget(resource: IResource, default) -> str:
+    """Return search index keyword based on the "budget" field.
+
+    The returned values are the same values as per the "requested_funding"
+    field, or "above_50000" if the total budget value is more than 50,000 euro.
+    """
+    budget = get_sheet_field(resource,
+                             sheets.mercator2.IFinancialPlanning, 'budget')
+    for limit in BUDGET_INDEX_LIMIT_KEYWORDS:
+        if budget <= limit:
+            return [str(limit)]
+    return ['above_50000']
+
+
 def includeme(config):
     """Register catalog utilities and index functions."""
     config.add_catalog_factory('adhocracy', MercatorCatalogIndexes)
@@ -86,3 +130,18 @@ def includeme(config):
                          catalog_name='adhocracy',
                          index_name='mercator_budget',
                          context=IMercatorSubResources)
+    # this prevent an "AttributeError: 'module' object has no attribute
+    # 'mercator2'" error when executing polytester
+    import adhocracy_mercator.sheets.mercator2  # noqa
+    config.add_indexview(mercator2_index_location,
+                         catalog_name='adhocracy',
+                         index_name='mercator_location',
+                         context=sheets.mercator2.ILocation)
+    config.add_indexview(mercator2_index_requested_funding,
+                         catalog_name='adhocracy',
+                         index_name='mercator_requested_funding',
+                         context=sheets.mercator2.IFinancialPlanning)
+    config.add_indexview(mercator2_index_budget,
+                         catalog_name='adhocracy',
+                         index_name='mercator_budget',
+                         context=sheets.mercator2.IFinancialPlanning)
