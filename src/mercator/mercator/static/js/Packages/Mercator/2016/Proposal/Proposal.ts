d7804e16dc5bbb355878d91cbbd0964b573050c2
Caroline Clifford <me@carolineclifford.com>
2015-12-02 19:20:38 +0100

    updates some fields and validation

--- src/mercator/mercator/static/js/Packages/Mercator/2016/Proposal/Proposal.ts
+++ src/mercator/mercator/static/js/Packages/Mercator/2016/Proposal/Proposal.ts
@@ -24,7 +24,7 @@ export var mercatorProposalFormController2016 = ($scope, $element, $window, adhS
 
     $scope.data = {};
 
-    var topicTotal = 0;
+    var topicTotal = -1;
 
     $scope.topics = [
         "democracy",
@@ -38,6 +38,21 @@ export var mercatorProposalFormController2016 = ($scope, $element, $window, adhS
         "other",
     ];
 
+    var heardFromCheckboxes = [
+        "heard-from-personal",
+        "heard-from-website",
+        "heard-from-newsletter",
+        "heard-from-facebook",
+        "heard-from-twitter",
+        "heard-from-other"
+    ];
+
+    var locationCheckboxes = [
+        "location-location-is-specific",
+        "location-location-is-online",
+        "location-location-is-linked-to-ruhr"
+    ];
+
     $scope.topicChange = (isChecked) => {
         topicTotal = isChecked ? (topicTotal + 1) : (topicTotal - 1);
         var validity = topicTotal > 0 && topicTotal < 3;
@@ -45,7 +60,45 @@ export var mercatorProposalFormController2016 = ($scope, $element, $window, adhS
         $scope.mercatorProposalForm.mercatorProposalBriefForm["introduction-topics"].$setDirty();
     };
 
+    var updateCheckBoxGroupValidity = (form, names : string[]) : boolean => {
+        var valid =  _.some(names, (name) => form[name].$modelValue);
+        _.forOwn(names, (name) => {
+            form[name].$setValidity("groupRequired", valid);
+        });
+        return valid;
+    };
+
     $scope.topicTrString = (topic) => {
         return ("TR__MERCATOR_TOPIC_" + topic.toUpperCase());
     };
+
+    var showCheckboxGroupError = (form, names : string[]) : boolean => {
+        var dirty = $scope.mercatorProposalForm.$submitted || _.some(names, (name) => form[name].$dirty);
+        return !updateCheckBoxGroupValidity(form, names) && dirty;
+    };
+
+    $scope.showLocationError = () : boolean => {
+        return showCheckboxGroupError($scope.mercatorProposalBriefForm, locationCheckboxes);
+    };
+
+    $scope.showStatusError = () : boolean => {
+        return showCheckboxGroupError($scope.mercatorProposalBriefForm, locationCheckboxes);
+    };
+
+    $scope.showFinanceGrantedInfo = () : boolean => {
+        return ($scope.data.finance && $scope.data.finance.other_sources && $scope.data.finance.other_sources !== "");
+    };
+
+    $scope.showHeardFromError = () : boolean => {
+        return showCheckboxGroupError($scope.mercatorProposalCommunityForm, heardFromCheckboxes);
+    };
+
+    // FIXME !
+    $scope.create = "true";
+
+    $scope.submitIfValid = () => {
+        // check validation of topics
+        $scope.topicChange(true);
+    };
+
 };
