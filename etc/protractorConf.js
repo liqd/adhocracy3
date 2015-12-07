36bd03a96cc7e2308539bbb817aa56669a72d9c5
Alexej Starschenko <alexej.starschenko@liqd.de>
2015-12-07 14:28:15 +0100

    Fix and enable project specific protractor tests

--- etc/protractorConf.js
+++ etc/protractorConf.js
@@ -4,9 +4,7 @@ var ini = require("ini");
 
 exports.config = {
     suites: {
-
-        // FIXME: Mercator tests all fail due to missing permissions
-        //mercator: "../src/mercator/mercator/tests/acceptance/*Spec.js",
+        current: "../src/current/current/tests/acceptance/*Spec.js",
         core: "../src/adhocracy_frontend/adhocracy_frontend/tests/acceptance/*Spec.js"
     },
     baseUrl: "http://localhost:9090",
@@ -17,7 +15,8 @@ exports.config = {
     },
     beforeLaunch: function() {
         exec("bin/supervisord");
-        exec("bin/supervisorctl restart adhocracy_test:*");
+        exec("bin/supervisorctl restart  adhocracy_test:test_zeo test_backend_with_ws adhocracy_test:test_autobahn adhocracy_test:test_frontend");
+        exec("src/current/current/tests/acceptance/setup_test.sh");
     },
     afterLaunch: function() {
         exec("bin/supervisorctl stop adhocracy_test:test_zeo test_backend_with_ws adhocracy_test:test_autobahn adhocracy_test:test_frontend");
