ac52dd134f3893f0d63b8d9239bcefd8d394c91b
Tobias Bengfort <tobias.bengfort@liqd.de>
2015-12-07 15:01:37 +0100

    rm implicit params
    depth: 2 and tag: LAST are implcit if contentType ends in 'Version'

--- src/mercator/mercator/static/js/Packages/Mercator/2015/Proposal/Proposal.ts
+++ src/mercator/mercator/static/js/Packages/Mercator/2015/Proposal/Proposal.ts
@@ -912,8 +912,7 @@ export var userListing = (adhConfig : AdhConfig.IService) => {
             scope.poolUrl = adhConfig.rest_url + adhConfig.custom["mercator_platform_path"];
             scope.contentType = RIMercatorProposalVersion.content_type;
             scope.params = {
-                creator: scope.path.replace(adhConfig.rest_url, "").replace(/\/+$/, ""),
-                depth: 2
+                creator: scope.path.replace(adhConfig.rest_url, "").replace(/\/+$/, "")
             };
         }
     };
