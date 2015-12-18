a1ef403a7873ce4eaf9341bcbfea6f2389d320ba
Joscha Krutzki <joka@jokasis.de>
2015-12-18 16:52:08 +0100

    Fix regression test only

--- src/adhocracy_core/adhocracy_core/catalog/test_init.py
+++ src/adhocracy_core/adhocracy_core/catalog/test_init.py
@@ -286,19 +286,19 @@ class TestCatalogsServiceAdhocracy:
         result = inst.search(query._replace(references=[reference]))
         assert list(result.elements) == [referencing]
 
-
-    def test_search_with_two_references(self, registry, pool, service, inst, query):
+    def test_search_with_two_references(self, registry, pool, service, inst,
+                                        query):
         from copy import deepcopy
-        from adhocracy_core.interfaces import ITag
         from adhocracy_core.interfaces import Reference
+        from adhocracy_core.interfaces import IItem
         from adhocracy_core.resources.principal import IUser
         from adhocracy_core import sheets
         from adhocracy_core.utils import get_sheet
         pool['principals'] = service
         pool['principals']['groups'] = deepcopy(service)
-        referenced = pool
         user = self._make_resource(registry, parent=pool, iresource=IUser)
-        referencing = self._make_resource(registry, parent=pool, iresource=ITag)
+        referencing = self._make_resource(registry, parent=pool,
+                                          iresource=IItem)
         sheet = get_sheet(referencing, sheets.metadata.IMetadata)
         sheet.set({'creator': [user]})
         reference = Reference(None, sheets.metadata.IMetadata,
