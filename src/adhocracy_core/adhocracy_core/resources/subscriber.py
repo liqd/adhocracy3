f6b056861ece07167a76254f4a8ff0296bdc2879
Alexej Starschenko <alexej.starschenko@liqd.de>
2015-12-21 10:16:54 +0100

    Fix subscriber unit test

--- src/adhocracy_core/adhocracy_core/resources/subscriber.py
+++ src/adhocracy_core/adhocracy_core/resources/subscriber.py
@@ -236,8 +236,7 @@ def send_activation_mail_or_activate_user(event):
     activation_path = _generate_activation_path()
     user.activation_path = activation_path
     messenger = getattr(event.registry, 'messenger', None)
-    if messenger is not None:  # ease testing
-        messenger.send_registration_mail(user, activation_path)
+    messenger.send_registration_mail(user, activation_path)
 
 
 def _generate_activation_path() -> str:
