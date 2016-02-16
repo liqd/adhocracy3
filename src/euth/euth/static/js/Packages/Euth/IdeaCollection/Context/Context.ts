2b3bc33ee96d489ede9bfa9ccf52437e8d4421c6
Magdalena Noffke <magdalena.noffke@googlemail.com>
2016-02-16 16:14:06 +0100

    this adds the pc proposal to euth

---
+++ src/euth/euth/static/js/Packages/Euth/IdeaCollection/Context/Context.ts
@@ -0,0 +1,11 @@
+import * as AdhConfig from "../../../Config/Config";
+
+var pkgLocation = "/Euth/IdeaCollection/Context";
+
+
+export var areaTemplate = (
+    adhConfig : AdhConfig.IService,
+    $templateRequest : angular.ITemplateRequestService
+) => {
+    return $templateRequest(adhConfig.pkg_path + pkgLocation + "/Template.html");
+};
---
+++ src/euth/euth/static/js/Packages/Euth/IdeaCollection/Context/Module.ts
@@ -0,0 +1,39 @@
+import * as AdhEmbedModule from "../../../Embed/Module";
+import * as AdhPermissionsModule from "../../../Permissions/Module";
+import * as AdhResourceAreaModule from "../../../ResourceArea/Module";
+import * as AdhTopLevelStateModule from "../../../TopLevelState/Module";
+
+import * as AdhEuthWorkbenchModule from "../Workbench/Module";
+
+import * as AdhEmbed from "../../../Embed/Embed";
+import * as AdhResourceArea from "../../../ResourceArea/ResourceArea";
+
+import * as AdhEuthWorkbench from "../Workbench/Workbench";
+
+import RIPeuthProcess from "../../../../Resources_/adhocracy_euth/resources/idea_collection/IProcess";
+
+import * as Context from "./Context";
+
+
+export var moduleName = "adhPcompassContext";
+
+export var register = (angular) => {
+    angular
+        .module(moduleName, [
+            AdhEmbedModule.moduleName,
+            AdhEuthWorkbenchModule.moduleName,
+            AdhPermissionsModule.moduleName,
+            AdhResourceAreaModule.moduleName,
+            AdhTopLevelStateModule.moduleName
+        ])
+        .config(["adhEmbedProvider", (adhEmbedProvider : AdhEmbed.Provider) => {
+            adhEmbedProvider.registerContext("euth");
+        }])
+        .config(["adhResourceAreaProvider", (adhResourceAreaProvider : AdhResourceArea.Provider) => {
+            adhResourceAreaProvider.template("euth", ["adhConfig", "$templateRequest", Context.areaTemplate]);
+            AdhEuthWorkbench.registerRoutes(
+               RIPeuthProcess.content_type,
+                "euth"
+            )(adhResourceAreaProvider);
+        }]);
+};
