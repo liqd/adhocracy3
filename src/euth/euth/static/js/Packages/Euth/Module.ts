41a997a2c998098326defe03df9e29382c108917
Magdalena Noffke <magdalena.noffke@googlemail.com>
2016-02-16 12:56:25 +0100

    move processes to folder

--- src/euth/euth/static/js/Packages/Euth/Module.ts
+++ src/euth/euth/static/js/Packages/Euth/Module.ts
@@ -1,22 +1,12 @@
-import * as AdhDebateWorkbenchModule from "../DebateWorkbench/Module";
-import * as AdhDebateWorkbench from "../DebateWorkbench/DebateWorkbench";
-import * as AdhProcess from "../Process/Process";
-import RIEuthResourcesCollaborativeTextIProcess from "../../Resources_/adhocracy_euth/resources/collaborative_text/IProcess";
+import * as AdhEuthCollaberativeTexteditingModule from "./CollaborativeTextediting/Module";
 
 export var moduleName = "adhEuth";
 
 export var register = (angular) => {
+	AdhEuthCollaberativeTexteditingModule.register(angular);
 
-	AdhDebateWorkbenchModule.register(angular);
-
-    angular
+	angular
         .module(moduleName, [
-			AdhDebateWorkbenchModule.moduleName
-        ])
-		.config(["adhProcessProvider", (adhProcessProvider: AdhProcess.Provider) => {
-			adhProcessProvider.templateFactories[RIEuthResourcesCollaborativeTextIProcess.content_type] = ["$q", ($q: angular.IQService) => {
-				return $q.when("<adh-debate-workbench></adh-debate-workbench>");
-			}];
-		}])
-		.config(["adhResourceAreaProvider", AdhDebateWorkbench.registerRoutes(RIEuthResourcesCollaborativeTextIProcess)]);
-		};
+	           AdhEuthCollaberativeTexteditingModule.moduleName
+	   ]);
+};
