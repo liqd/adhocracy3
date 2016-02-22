6221a3575effa97a907e969e8ea6646cbcfa0633
Pierre Allix <pierre.allix@liqd.de>
2016-02-22 18:15:57 +0100

    Move private workflows tests to core

--- src/adhocracy_core/adhocracy_core/workflows/test_debate_private.py
+++ src/adhocracy_core/adhocracy_core/workflows/test_debate_private.py
@@ -1,11 +1,19 @@
 from pyramid import testing
+from pyramid.threadlocal import manager
+from pyramid.traversal import find_resource
 from pytest import fixture
 from pytest import mark
 from webtest import TestResponse
+import transaction
 
+from adhocracy_core.authorization import acm_to_acl
+from adhocracy_core.authorization import get_acl
+from adhocracy_core.authorization import set_acl
+from adhocracy_core.authorization import set_local_roles
+from adhocracy_core.schema import ACM
+from adhocracy_core.utils import get_root
 from adhocracy_core.utils.testing import add_resources
 from adhocracy_core.utils.testing import do_transition_to
-
 
 @fixture
 def integration(integration):
@@ -17,3 +25,138 @@ def test_includeme(registry):
     from adhocracy_core.workflows import AdhocracyACLWorkflow
     workflow = registry.content.workflows['debate_private']
     assert isinstance(workflow, AdhocracyACLWorkflow)
+
+
+@mark.functional
+class TestDebatePrivateProcess:
+
+    @fixture
+    def process_url_private(self):
+        return '/opin/collaborative_text'
+
+    @fixture(scope='session')
+    def app_settings(self, app_settings) -> dict:
+        """Return settings to start the test wsgi app."""
+        app_settings['adhocracy.skip_registration_mail'] = True
+        return app_settings
+
+    @fixture(scope='class')
+    def app_router(self, app_settings):
+        """Add a `debate_private` workflow to IProcess resources."""
+        from adhocracy_core.testing import make_configurator
+        from adhocracy_core.resources import add_resource_type_to_registry
+        from adhocracy_core.resources.process import process_meta
+        import adhocracy_core
+        configurator = make_configurator(app_settings, adhocracy_core)
+        debate_process_meta = process_meta._replace(workflow_name='debate_private')
+        add_resource_type_to_registry(debate_process_meta, configurator)
+        app_router = configurator.make_wsgi_app()
+        return app_router
+
+    def test_create_resources(self,
+                              datadir,
+                              process_url_private,
+                              app_admin):
+        registry = app_admin.app_router.registry
+        manager.push({'registry': registry})
+        root = get_root(app_admin.app_router)
+        json_file_private = str(datadir.join('private.json'))
+        add_resources(app_admin.app_router, json_file_private)
+        process_private = find_resource(root, process_url_private)
+        set_local_roles(process_private,
+                        {"opin-idea-collection-participants":
+                         {"role:participant"}})
+        old_acl = get_acl(root)
+        acm = ACM().deserialize(
+            {'principals':            ['anonymous', 'authenticated', 'participant', 'moderator',  'creator', 'initiator', 'admin'],
+             'permissions': [['view',  'Allow',      'Deny',          'Allow',       'Allow',      'Allow',   'Allow',     'Allow'],
+             ]})
+        new_acl = acm_to_acl(acm, registry) + old_acl
+        set_acl(root, new_acl)
+        transaction.commit()
+        resp = app_admin.get(process_url_private)
+
+    def set_process_state(self,
+                          process_url,
+                          app_admin,
+                          state):
+        resp = do_transition_to(app_admin,
+                                process_url,
+                                state)
+        assert resp.status_code == 200
+
+    def test_announce_anonymous_cannot_read_private_process(
+            self, process_url_private, app_anonymous, app_admin):
+        self.set_process_state(process_url_private, app_admin, 'announce')
+        resp = app_anonymous.get(process_url_private)
+        assert resp.status_code == 403
+
+    def test_announce_authenticated_cannot_read_private_process(
+            self, process_url_private, app_authenticated):
+        resp = app_authenticated.get(process_url_private)
+        assert resp.status_code == 403
+
+
+    def set_process_participate_state(self,
+                                      process_url,
+                                      app_admin):
+        resp = do_transition_to(app_admin,
+                                process_url,
+                                'participate')
+        assert resp.status_code == 200
+
+    def test_participate_anonymous_cannot_read_private_process(
+            self, process_url_private, app_anonymous, app_admin):
+        self.set_process_state(process_url_private, app_admin, 'participate')
+        resp = app_anonymous.get(process_url_private)
+        assert resp.status_code == 403
+
+    def test_participate_authenticated_cannot_read_private_process(
+            self, process_url_private, app_authenticated, app_admin):
+        resp = app_authenticated.get(process_url_private)
+        assert resp.status_code == 403
+
+    def test_evaluate_anonymous_cannot_read_private_process(
+            self, process_url_private, app_anonymous, app_admin):
+#        import pudb; pudb.set_trace() #  noqa
+        self.set_process_state(process_url_private, app_admin, 'evaluate')
+        resp = app_anonymous.get(process_url_private)
+        assert resp.status_code == 403
+
+    def test_evaluate_authenticated_cannot_read_private_process(
+            self, process_url_private, app_authenticated, app_admin):
+        resp = app_authenticated.get(process_url_private)
+        assert resp.status_code == 403
+
+    def set_process_state(self,
+                          process_url,
+                          app_admin,
+                          state):
+        resp = do_transition_to(app_admin,
+                                process_url,
+                                state)
+        assert resp.status_code == 200
+
+    def test_result_anonymous_cannot_read_private_process(
+            self, process_url_private, app_anonymous, app_admin):
+        self.set_process_state(process_url_private, app_admin, 'result')
+        resp = app_anonymous.get(process_url_private)
+        assert resp.status_code == 403
+
+    def test_result_authenticated_cannot_read_private_process(
+            self, process_url_private, app_authenticated, app_admin):
+        resp = app_authenticated.get(process_url_private)
+        assert resp.status_code == 403
+
+    def test_closed_anonymous_cannot_read_private_process(
+            self, process_url_private, app_anonymous, app_admin):
+        self.set_process_state(process_url_private,
+                               app_admin,
+                               'closed')
+        resp = app_anonymous.get(process_url_private)
+        assert resp.status_code == 403
+
+    def test_closed_authenticated_cannot_read_private_process(
+            self, process_url_private, app_authenticated, app_admin):
+        resp = app_authenticated.get(process_url_private)
+        assert resp.status_code == 403
--- src/adhocracy_core/adhocracy_core/workflows/test_standard.py
+++ src/adhocracy_core/adhocracy_core/workflows/test_standard.py
@@ -1,6 +1,18 @@
+from pyramid.traversal import find_resource
 from pyramid import testing
 from pytest import fixture
 from pytest import mark
+import transaction
+
+from adhocracy_core.authorization import acm_to_acl
+from adhocracy_core.authorization import get_acl
+from adhocracy_core.authorization import set_acl
+from adhocracy_core.authorization import set_local_roles
+from adhocracy_core.schema import ACM
+from adhocracy_core.utils import get_root
+from adhocracy_core.utils.testing import add_resources
+from adhocracy_core.utils.testing import do_transition_to
+
 
 class TestStandardWorkflow:
 
@@ -46,3 +58,214 @@ def test_initiate_and_transition_to_result(registry, context):
     assert workflow.state_of(context) is 'result'
     workflow.transition_to_state(context, request, 'closed')
     assert workflow.state_of(context) is 'closed'
+
+@mark.functional
+class TestStandardPublicWithPrivateProcessesConfig:
+
+    @fixture
+    def process_url_public(self):
+        return '/opin/idea_collection'
+
+
+    @fixture(scope='session')
+    def app_settings(self, app_settings) -> dict:
+        """Return settings to start the test wsgi app."""
+        app_settings['adhocracy.skip_registration_mail'] = True
+        return app_settings
+
+    @fixture(scope='class')
+    def app_router(self, app_settings):
+        """Add a `debate_private` workflow to IProcess resources."""
+        from adhocracy_core.testing import make_configurator
+        from adhocracy_core.resources import add_resource_type_to_registry
+        from adhocracy_core.resources.process import process_meta
+        import adhocracy_core
+        configurator = make_configurator(app_settings, adhocracy_core)
+        standard_process_meta = process_meta._replace(workflow_name='standard')
+        add_resource_type_to_registry(standard_process_meta, configurator)
+        app_router = configurator.make_wsgi_app()
+        return app_router
+
+    def test_create_resources(self,
+                              datadir,
+                              process_url_public,
+                              app_admin):
+        registry = app_admin.app_router.registry
+        from pyramid.threadlocal import manager
+        manager.push({'registry': registry})
+        root = get_root(app_admin.app_router)
+        json_file_public = str(datadir.join('public.json'))
+        add_resources(app_admin.app_router, json_file_public)
+        process_public = find_resource(root, process_url_public)
+        set_local_roles(process_public,
+                        {"opin-idea-collection-participants":
+                         {"role:participant"}})
+        old_acl = get_acl(root)
+        acm = ACM().deserialize(
+            {'principals':            ['anonymous', 'authenticated', 'participant', 'moderator',  'creator', 'initiator', 'admin'],
+             'permissions': [['view',  'Allow',      'Deny',          'Allow',       'Allow',      'Allow',   'Allow',     'Allow'],
+             ]})
+        new_acl = acm_to_acl(acm, registry) + old_acl
+        set_acl(root, new_acl)
+        set_local_roles(process_public,
+                        # this is "group:authenticated" in prod
+                        {"system.Authenticated":
+                         {"role:participant"},})
+        transaction.commit()
+        resp = app_admin.get(process_url_public)
+
+    def set_process_state(self,
+                          process_url,
+                          app_admin,
+                          state):
+        resp = do_transition_to(app_admin,
+                                process_url,
+                                state)
+        assert resp.status_code == 200
+
+    def test_announce_authenticated_can_read_public_process(
+            self, process_url_public, app_authenticated, app_admin):
+        self.set_process_state(process_url_public,
+                               app_admin,
+                               'announce')
+        resp = app_authenticated.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_announce_anonymous_can_read_public_process(
+            self, process_url_public, app_anonymous, request):
+        resp = app_anonymous.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_announce_initiator_can_read_public_process(
+            self, process_url_public, app_initiator):
+        root = get_root(app_initiator.app_router)
+        resp = app_initiator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_announce_moderator_can_read_public_process(
+            self, process_url_public, app_moderator):
+        root = get_root(app_moderator.app_router)
+        resp = app_moderator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_announce_admin_can_read_public_process(
+            self, process_url_public, app_admin):
+        root = get_root(app_admin.app_router)
+        resp = app_admin.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_participate_authenticated_can_read_public_process(
+            self, process_url_public, app_authenticated, app_admin):
+        self.set_process_state(process_url_public, app_admin, 'participate')
+        resp = app_authenticated.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_participate_anonymous_can_read_public_process(
+            self, process_url_public, app_anonymous, request):
+        resp = app_anonymous.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_participate_initiator_can_read_public_process(
+            self, process_url_public, app_initiator):
+        root = get_root(app_initiator.app_router)
+        resp = app_initiator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_participate_moderator_can_read_public_process(
+            self, process_url_public, app_moderator):
+        root = get_root(app_moderator.app_router)
+        resp = app_moderator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_participate_admin_can_read_public_process(
+            self, process_url_public, app_admin):
+        root = get_root(app_admin.app_router)
+        resp = app_admin.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_evaluate_authenticated_can_read_public_process(
+            self, process_url_public, app_authenticated, app_admin):
+        self.set_process_state(process_url_public, app_admin, 'evaluate')
+        resp = app_authenticated.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_evaluate_anonymous_can_read_public_process(
+            self, process_url_public, app_anonymous, request):
+        resp = app_anonymous.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_evaluate_initiator_can_read_public_process(
+            self, process_url_public, app_initiator):
+        root = get_root(app_initiator.app_router)
+        resp = app_initiator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_evaluate_moderator_can_read_public_process(
+            self, process_url_public, app_moderator):
+        root = get_root(app_moderator.app_router)
+        resp = app_moderator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_evaluate_admin_can_read_public_process(
+            self, process_url_public, app_admin):
+        root = get_root(app_admin.app_router)
+        resp = app_admin.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_result_authenticated_can_read_public_process(
+            self, process_url_public, app_authenticated, app_admin):
+        self.set_process_state(process_url_public, app_admin, 'result')
+        resp = app_authenticated.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_result_anonymous_can_read_public_process(
+            self, process_url_public, app_anonymous, request):
+        resp = app_anonymous.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_result_initiator_can_read_public_process(
+            self, process_url_public, app_initiator):
+        root = get_root(app_initiator.app_router)
+        resp = app_initiator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_result_moderator_can_read_public_process(
+            self, process_url_public, app_moderator):
+        root = get_root(app_moderator.app_router)
+        resp = app_moderator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_result_admin_can_read_public_process(
+            self, process_url_public, app_admin):
+        root = get_root(app_admin.app_router)
+        resp = app_admin.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_closed_authenticated_can_read_public_process(
+            self, process_url_public, app_authenticated, app_admin):
+        self.set_process_state(process_url_public, app_admin, 'closed')
+        resp = app_authenticated.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_closed_anonymous_can_read_public_process(
+            self, process_url_public, app_anonymous, request):
+        resp = app_anonymous.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_closed_initiator_can_read_public_process(
+            self, process_url_public, app_initiator):
+        root = get_root(app_initiator.app_router)
+        resp = app_initiator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_closed_moderator_can_read_public_process(
+            self, process_url_public, app_moderator):
+        root = get_root(app_moderator.app_router)
+        resp = app_moderator.get(process_url_public)
+        assert resp.status_code == 200
+
+    def test_closed_admin_can_read_public_process(
+            self, process_url_public, app_admin):
+        root = get_root(app_admin.app_router)
+        resp = app_admin.get(process_url_public)
+        assert resp.status_code == 200
