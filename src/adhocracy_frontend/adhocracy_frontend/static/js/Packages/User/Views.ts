bf81f1429cf778e560e9287bc8e99d8daca129b7
John <john.schmid@liqd.de>
2016-02-26 17:19:21 +0100

    check whether support_url is empty
    I know this fix looks primitive. But I think it basically solves the
    issue.

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/User/Views.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/User/Views.ts
@@ -840,13 +840,15 @@ export var adhUserProfileImageEditDirective = (
 export var adhHelpLinkDirective = (
     adhConfig: AdhConfig.IService
 ) => {
-    return {
-        restrict: "E",
-        templateUrl: adhConfig.pkg_path + pkgLocation + "/HelpLink.html",
-        link: (scope) => {
-            scope.supportUrl = adhConfig.support_url;
-        }
-    };
+    if (adhConfig.support_url.length > 0) {
+        return {
+            restrict: "E",
+            templateUrl: adhConfig.pkg_path + pkgLocation + "/HelpLink.html",
+            link: (scope) => {
+                scope.supportUrl = adhConfig.support_url;
+            }
+        };
+    }
 };
 
 export var registerRoutes = (
