34f392eb1ac04652faee6844eb77ce6f46bac13e
Pierre Allix <pierre.allix@liqd.de>
2016-03-09 11:48:38 +0100

    Convert badge_assignment workflow to YAML

--- src/adhocracy_core/adhocracy_core/workflows/badge_assignment.py
+++ src/adhocracy_core/adhocracy_core/workflows/badge_assignment.py
@@ -6,27 +6,8 @@ allowed to assign them.
 
 """
 
-# flake8: noqa
-
-from adhocracy_core.workflows import add_workflow
-from pyrsistent import freeze
-
-badge_assignment_meta = freeze({
-    'initial_state': 'assign',
-    'states': {
-        'assign': {'title': 'Assignment',
-                  'description': 'Assign badges.',
-                  'acm': {'principals':                         ['anonymous', 'participant', 'moderator', 'creator', 'initiator', 'admin'],
-                          'permissions':
-                          [['assign_badge',                       None,       'Allow',       'Allow',      None,      None,        None],
-                          ]}
-                  },
-        },
-    'transitions': {
-    }
-})
-
 
 def includeme(config):
     """Add workflow."""
-    add_workflow(config.registry, badge_assignment_meta, 'badge_assignment')
+    config.add_workflow('adhocracy_core.workflows:badge_assignment.yaml',
+                        'badge_assignment')
