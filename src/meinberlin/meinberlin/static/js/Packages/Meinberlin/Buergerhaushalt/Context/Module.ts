b357934b8244ee0196d2edf23c472cafe3ede73b
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-05-10 15:40:37 +0200

    restore buergerhaushalt context

--- src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Buergerhaushalt/Context/Module.ts
+++ src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Buergerhaushalt/Context/Module.ts
@@ -1,6 +1,14 @@
 import * as AdhEmbedModule from "../../../Embed/Module";
+import * as AdhResourceAreaModule from "../../../ResourceArea/Module";
+
+import * as AdhMeinberlinIdeaCollectionModule from "../../IdeaCollection/Module";
 
 import * as AdhEmbed from "../../../Embed/Embed";
+import * as AdhResourceArea from "../../../ResourceArea/ResourceArea";
+
+import * as AdhMeinberlinIdeaCollection from "../../IdeaCollection/IdeaCollection";
+
+import RIBuergerhaushaltProcess from "../../../../Resources_/adhocracy_meinberlin/resources/burgerhaushalt/IProcess";
 
 
 export var moduleName = "adhMeinberlinBuergerhaushaltContext";
@@ -8,9 +16,17 @@ export var moduleName = "adhMeinberlinBuergerhaushaltContext";
 export var register = (angular) => {
     angular
         .module(moduleName, [
-            AdhEmbedModule.moduleName
+            AdhEmbedModule.moduleName,
+            AdhMeinberlinIdeaCollectionModule.moduleName,
+            AdhResourceAreaModule.moduleName
         ])
-        .config(["adhEmbedProvider", (adhEmbedProvider: AdhEmbed.Provider) => {
+        .config(["adhEmbedProvider", (adhEmbedProvider : AdhEmbed.Provider) => {
             adhEmbedProvider.registerContext("buergerhaushalt", ["burgerhaushalt"]);
+        }])
+        .config(["adhResourceAreaProvider", (adhResourceAreaProvider : AdhResourceArea.Provider) => {
+            AdhMeinberlinIdeaCollection.registerRoutes(
+                RIBuergerhaushaltProcess.content_type,
+                "buergerhaushalt"
+            )(adhResourceAreaProvider);
         }]);
 };
