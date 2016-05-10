984e724ae7320511eaacb37dbd180b87a6b3b932
John <john.schmid@liqd.de>
2016-05-10 11:35:48 +0200

    rm Buergerhaushalt workbench

--- src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Buergerhaushalt/Context/Module.ts
+++ src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Buergerhaushalt/Context/Module.ts
@@ -1,14 +1,6 @@
 import * as AdhEmbedModule from "../../../Embed/Module";
-import * as AdhResourceAreaModule from "../../../ResourceArea/Module";
-
-import * as AdhMeinberlinBuergerhaushaltWorkbenchModule from "../Workbench/Module";
 
 import * as AdhEmbed from "../../../Embed/Embed";
-import * as AdhResourceArea from "../../../ResourceArea/ResourceArea";
-
-import * as AdhMeinberlinBuergerhaushaltWorkbench from "../Workbench/Workbench";
-
-import RIBuergerhaushaltProcess from "../../../../Resources_/adhocracy_meinberlin/resources/burgerhaushalt/IProcess";
 
 
 export var moduleName = "adhMeinberlinBuergerhaushaltContext";
@@ -16,17 +8,9 @@ export var moduleName = "adhMeinberlinBuergerhaushaltContext";
 export var register = (angular) => {
     angular
         .module(moduleName, [
-            AdhEmbedModule.moduleName,
-            AdhMeinberlinBuergerhaushaltWorkbenchModule.moduleName,
-            AdhResourceAreaModule.moduleName
+            AdhEmbedModule.moduleName
         ])
-        .config(["adhEmbedProvider", (adhEmbedProvider : AdhEmbed.Provider) => {
+        .config(["adhEmbedProvider", (adhEmbedProvider: AdhEmbed.Provider) => {
             adhEmbedProvider.registerContext("buergerhaushalt", ["burgerhaushalt"]);
-        }])
-        .config(["adhResourceAreaProvider", (adhResourceAreaProvider : AdhResourceArea.Provider) => {
-            AdhMeinberlinBuergerhaushaltWorkbench.registerRoutes(
-                RIBuergerhaushaltProcess.content_type,
-                "buergerhaushalt"
-            )(adhResourceAreaProvider);
         }]);
 };
--- src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Buergerhaushalt/Module.ts
+++ src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Buergerhaushalt/Module.ts
@@ -1,6 +1,5 @@
 import * as AdhMeinberlinBuergerhaushaltContextModule from "./Context/Module";
 import * as AdhMeinberlinBuergerhaushaltProcessModule from "./Process/Module";
-import * as AdhMeinberlinBuergerhaushaltWorkbenchModule from "./Workbench/Module";
 
 
 export var moduleName = "adhMeinberlinBuergerhaushalt";
@@ -8,12 +7,11 @@ export var moduleName = "adhMeinberlinBuergerhaushalt";
 export var register = (angular) => {
     AdhMeinberlinBuergerhaushaltContextModule.register(angular);
     AdhMeinberlinBuergerhaushaltProcessModule.register(angular);
-    AdhMeinberlinBuergerhaushaltWorkbenchModule.register(angular);
 
     angular
         .module(moduleName, [
             AdhMeinberlinBuergerhaushaltContextModule.moduleName,
             AdhMeinberlinBuergerhaushaltProcessModule.moduleName,
-            AdhMeinberlinBuergerhaushaltWorkbenchModule.moduleName
+            AdhMeinberlinBuergerhaushaltProcessModule.moduleName
         ]);
 };
