c5beab5fd254fb61c2814b89f52dcc8abd05f7c6
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-05-10 15:22:41 +0200

    Gardening

--- src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Module.ts
+++ src/meinberlin/meinberlin/static/js/Packages/Meinberlin/Module.ts
@@ -2,11 +2,10 @@ import * as AdhMeinberlinAlexanderplatzModule from "./Alexanderplatz/Module";
 import * as AdhMeinberlinBplanModule from "./Bplan/Module";
 import * as AdhMeinberlinBuergerhaushaltModule from "./Buergerhaushalt/Module";
 import * as AdhMeinberlinDeModule from "./MeinberlinDe/Module";
+import * as AdhMeinberlinIdeaCollectionModule from "./IdeaCollection/Module";
 import * as AdhMeinberlinKiezkasseModule from "./Kiezkasse/Module";
 import * as AdhMeinberlinProposalModule from "./Proposal/Module";
 import * as AdhMeinberlinStadtforumModule from "./Stadtforum/Module";
-import * as AdhMeinberlinDe from "./MeinberlinDe/Module";
-import * as AdhMeinberlinIdeaCollectionModule from "./IdeaCollection/Module";
 
 
 export var moduleName = "adhMeinberlin";
@@ -16,21 +15,18 @@ export var register = (angular) => {
     AdhMeinberlinBplanModule.register(angular);
     AdhMeinberlinBuergerhaushaltModule.register(angular);
     AdhMeinberlinDeModule.register(angular);
+    AdhMeinberlinIdeaCollectionModule.register(angular);
     AdhMeinberlinKiezkasseModule.register(angular);
     AdhMeinberlinProposalModule.register(angular);
     AdhMeinberlinStadtforumModule.register(angular);
-    AdhMeinberlinDe.register(angular);
-    AdhMeinberlinIdeaCollectionModule.register(angular);
 
     angular
         .module(moduleName, [
             AdhMeinberlinAlexanderplatzModule.moduleName,
             AdhMeinberlinBplanModule.moduleName,
             AdhMeinberlinBuergerhaushaltModule.moduleName,
-            AdhMeinberlinStadtforumModule.moduleName,
-            AdhMeinberlinDe.moduleName,
-            AdhMeinberlinIdeaCollectionModule.moduleName,
             AdhMeinberlinDeModule.moduleName,
+            AdhMeinberlinIdeaCollectionModule.moduleName,
             AdhMeinberlinKiezkasseModule.moduleName,
             AdhMeinberlinProposalModule.moduleName,
             AdhMeinberlinStadtforumModule.moduleName
