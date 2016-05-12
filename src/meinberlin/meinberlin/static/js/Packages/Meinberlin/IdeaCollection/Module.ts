52b4850bc0d1f61b551f3f58abe703b3e6c1686d
John <john.schmid@liqd.de>
2016-05-12 12:36:14 +0200

    register IdeaCollection

--- src/meinberlin/meinberlin/static/js/Packages/Meinberlin/IdeaCollection/Module.ts
+++ src/meinberlin/meinberlin/static/js/Packages/Meinberlin/IdeaCollection/Module.ts
@@ -4,17 +4,22 @@ import * as AdhHttpModule from "../../Http/Module";
 import * as AdhMovingColumnsModule from "../../MovingColumns/Module";
 import * as AdhTopLevelStateModule from "../../TopLevelState/Module";
 import * as AdhPermissionsModule from "../../Permissions/Module";
+import * as AdhProcess from "../../Process/Process";
+import * as AdhResourceArea from "../../ResourceArea/ResourceArea";
 
 import * as AdhMeinberlinIdeaCollectionProcessModule from "./Process/Module";
 import * as AdhMeinberlinProposalModule from "../Proposal/Module";
 
 import * as IdeaCollection from "./IdeaCollection";
 
+import RIIdeaCollectionProcess from "../../../Resources_/adhocracy_meinberlin/resources/idea_collection/IProcess";
 
 export var moduleName = "adhMeinberlinIdeaCollection";
 
 export var register = (angular) => {
     AdhMeinberlinIdeaCollectionProcessModule.register(angular);
+
+    var processType = RIIdeaCollectionProcess.content_type;
 
     angular
         .module(moduleName, [
@@ -37,5 +42,18 @@ export var register = (angular) => {
         .directive("adhMeinberlinIdeaCollectionDetailColumn", ["adhConfig", IdeaCollection.detailColumnDirective])
         .directive("adhMeinberlinIdeaCollectionEditColumn", ["adhConfig", IdeaCollection.editColumnDirective])
         .directive("adhMeinberlinIdeaCollectionAddProposalButton", [
-            "adhConfig", "adhPermissions", "adhTopLevelState", IdeaCollection.addProposalButtonDirective]);
+            "adhConfig", "adhPermissions", "adhTopLevelState", IdeaCollection.addProposalButtonDirective])
+        .config(["adhResourceAreaProvider", "adhConfig", (adhResourceAreaProvider: AdhResourceArea.Provider, adhConfig) => {
+            var registerRoutes = IdeaCollection.registerRoutesFactory(processType);
+            registerRoutes(processType)(adhResourceAreaProvider);
+
+            var customHeader = adhConfig.pkg_path + IdeaCollection.pkgLocation + "/CustomHeader.html";
+            adhResourceAreaProvider.customHeader(processType, customHeader);
+        }])
+        .config(["adhProcessProvider", (adhProcessProvider: AdhProcess.Provider) => {
+            adhProcessProvider.templateFactories[processType] = ["$q", ($q: angular.IQService) => {
+                return $q.when("<adh-meinberlin-idea-collection-workbench>" +
+                    "</adh-meinberlin-idea-collection-workbench>");
+            }];
+        }]);
 };
