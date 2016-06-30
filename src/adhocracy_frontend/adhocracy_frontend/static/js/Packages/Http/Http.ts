1ff883fd1ff01b631e96604f5e41ad71a29ccb41
John <john.schmid@liqd.de>
2016-06-30 12:46:12 +0200

    remove variable 'R' from within Http.Service

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Http/Http.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Http/Http.ts
@@ -210,7 +210,7 @@ export class Service<R extends ResourcesBase.IResource> {
         path : string,
         params?,
         config : IHttpGetConfig = {}
-    ) : angular.IPromise<R> {
+    ) : angular.IPromise<any> {
         var query = (typeof params === "undefined") ? "" : "?" + $.param(params);
 
         var originalElements = (params || {}).elements || "omit";
@@ -225,7 +225,7 @@ export class Service<R extends ResourcesBase.IResource> {
                 AdhError.logBackendError));
     }
 
-    public putRaw(path : string, obj : R, config : IHttpConfig = {}) : angular.IPromise<any> {
+    public putRaw(path : string, obj : any, config : IHttpConfig = {}) : angular.IPromise<any> {
         if (this.adhPreliminaryNames.isPreliminary(path)) {
             throw "attempt to http-put preliminary path: " + path;
         }
@@ -238,7 +238,7 @@ export class Service<R extends ResourcesBase.IResource> {
         }));
     }
 
-    public put(path : string, obj : R, config : IHttpPutConfig = {}) : angular.IPromise<R> {
+    public put(path : string, obj : any, config : IHttpPutConfig = {}) : angular.IPromise<any> {
         var _self = this;
 
         if (!config.noExport) {
@@ -281,7 +281,7 @@ export class Service<R extends ResourcesBase.IResource> {
         }));
     }
 
-    public postRaw(path : string, obj : R, config : IHttpConfig = {}) : angular.IPromise<any> {
+    public postRaw(path : string, obj : any, config : IHttpConfig = {}) : angular.IPromise<any> {
         var _self = this;
 
         if (_self.adhPreliminaryNames.isPreliminary(path)) {
@@ -307,7 +307,7 @@ export class Service<R extends ResourcesBase.IResource> {
         });
     }
 
-    public post(path : string, obj : R, config : IHttpConfig = {}) : angular.IPromise<R> {
+    public post(path : string, obj : any, config : IHttpConfig = {}) : angular.IPromise<any> {
         var _self = this;
 
         if (!config.noExport) {
@@ -415,9 +415,9 @@ export class Service<R extends ResourcesBase.IResource> {
      */
     public postNewVersionNoFork(
         oldVersionPath : string,
-        obj : R,
+        obj : any,
         rootVersions? : string[]
-    ) : angular.IPromise<{ value: R; parentChanged: boolean; }> {
+    ) : angular.IPromise<{ value: any; parentChanged: boolean; }> {
         var _self = this;
 
         var timeoutRounds : number = 5;
@@ -433,7 +433,7 @@ export class Service<R extends ResourcesBase.IResource> {
             nextOldVersionPath : string,
             parentChanged : boolean,
             roundsLeft : number
-        ) : angular.IPromise<{ value : R; parentChanged : boolean; }> => {
+        ) : angular.IPromise<{ value : any; parentChanged : boolean; }> => {
             if (roundsLeft === 0) {
                 throw "Tried to post new version of " + dagPath + " " + timeoutRounds.toString() + " times, giving up.";
             }
@@ -478,7 +478,7 @@ export class Service<R extends ResourcesBase.IResource> {
         return retry(oldVersionPath, false, timeoutRounds);
     }
 
-    public postToPool(poolPath : string, obj : R) : angular.IPromise<R> {
+    public postToPool(poolPath : string, obj : any) : angular.IPromise<any> {
         return this.post(poolPath, obj);
     }
 
@@ -488,8 +488,8 @@ export class Service<R extends ResourcesBase.IResource> {
      * If you do not know if a reference is already resolved to the
      * corresponding resource you can use this function to be sure.
      */
-    public resolve(path : string) : angular.IPromise<R>;
-    public resolve(resource : R) : angular.IPromise<R>;
+    public resolve(path : string) : angular.IPromise<any>;
+    public resolve(resource : any) : angular.IPromise<any>;
     public resolve(pathOrResource) {
         if (typeof pathOrResource === "string") {
             return this.get(pathOrResource);
