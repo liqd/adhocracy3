8b1fd21c2e3cadc8967304c720f0a6447f642acc
John <john.schmid@liqd.de>
2016-07-07 16:31:33 +0200

    rm euth/IdeaColl/Proposal + /Workbench files
    except for euth/IdeaColl/Workbench/Module.ts

--- src/euth/euth/static/js/Packages/Euth/IdeaCollection/Module.ts
+++ src/euth/euth/static/js/Packages/Euth/IdeaCollection/Module.ts
@@ -1,15 +1,12 @@
-import * as AdhEuthIdeaProposalModule from "./Proposal/Module";
 import * as AdhEuthIdeaWorkbenchModule from "./Workbench/Module";
 
 export var moduleName = "adhEuthIdeaCollection";
 
 export var register = (angular) => {
-    AdhEuthIdeaProposalModule.register(angular);
     AdhEuthIdeaWorkbenchModule.register(angular);
 
     angular
         .module(moduleName, [
-            AdhEuthIdeaProposalModule.moduleName,
-            AdhEuthIdeaWorkbenchModule.moduleName,
+            AdhEuthIdeaWorkbenchModule.moduleName
         ]);
 };
--- src/euth/euth/static/js/Packages/Euth/IdeaCollection/Workbench/Module.ts
+++ src/euth/euth/static/js/Packages/Euth/IdeaCollection/Workbench/Module.ts
@@ -5,11 +5,11 @@ import * as AdhIdeaCollectionModule from "../../../IdeaCollection/Module";
 import * as AdhMovingColumnsModule from "../../../MovingColumns/Module";
 import * as AdhPermissionsModule from "../../../Permissions/Module";
 import * as AdhProcessModule from "../../../Process/Module";
-import * as AdhProposalModule from "../Proposal/Module";
 import * as AdhResourceActionsModule from "../../../ResourceActions/Module";
 import * as AdhResourceAreaModule from "../../../ResourceArea/Module";
 import * as AdhTopLevelStateModule from "../../../TopLevelState/Module";
 
+import * as AdhEmbed from "../../../Embed/Embed";
 import * as AdhIdeaCollectionWorkbench from "../../../IdeaCollection/Workbench/Workbench";
 import * as AdhProcess from "../../../Process/Process";
 import * as AdhResourceArea from "../../../ResourceArea/ResourceArea";
@@ -18,8 +18,6 @@ import RIEuthProcess from "../../../../Resources_/adhocracy_euth/resources/idea_
 import RIEuthPrivateProcess from "../../../../Resources_/adhocracy_euth/resources/idea_collection/IPrivateProcess";
 import RIProposal from "../../../../Resources_/adhocracy_core/resources/proposal/IProposal";
 import RIProposalVersion from "../../../../Resources_/adhocracy_core/resources/proposal/IProposalVersion";
-
-import * as Workbench from "./Workbench";
 
 
 export var moduleName = "adhEuthWorkbench";
@@ -37,11 +35,13 @@ export var register = (angular) => {
             AdhMovingColumnsModule.moduleName,
             AdhPermissionsModule.moduleName,
             AdhProcessModule.moduleName,
-            AdhProposalModule.moduleName,
             AdhResourceActionsModule.moduleName,
             AdhResourceAreaModule.moduleName,
             AdhTopLevelStateModule.moduleName
         ])
+        .config(["adhEmbedProvider", (adhEmbedProvider : AdhEmbed.Provider) => {
+            adhEmbedProvider.registerContext("euth");
+        }])
         .config(["adhResourceAreaProvider", "adhConfig", (adhResourceAreaProvider : AdhResourceArea.Provider, adhConfig) => {
             var registerRoutes1 = AdhIdeaCollectionWorkbench.registerRoutesFactory(
                 RIEuthProcess, RIProposal, RIProposalVersion);
@@ -72,16 +72,5 @@ export var register = (angular) => {
                 proposalClass: RIProposal,
                 proposalVersionClass: RIProposalVersion
             };
-        }])
-        .config(["adhResourceAreaProvider", Workbench.registerRoutes(RIEuthProcess)])
-        .config(["adhResourceAreaProvider", Workbench.registerRoutes(RIEuthPrivateProcess)])
-        .directive("adhPcompassWorkbench", ["adhTopLevelState", "adhConfig", "adhHttp", Workbench.workbenchDirective])
-        .directive("adhPcompassProposalDetailColumn", [
-            "$timeout", "adhConfig", "adhPermissions", "adhTopLevelState", Workbench.proposalDetailColumnDirective])
-        .directive("adhPcompassProposalCreateColumn", ["adhConfig", "adhTopLevelState", Workbench.proposalCreateColumnDirective])
-        .directive("adhPcompassProposalEditColumn", ["adhConfig", "adhTopLevelState", Workbench.proposalEditColumnDirective])
-        .directive("adhPcompassProposalImageColumn", [
-            "adhConfig", "adhTopLevelState", "adhResourceUrlFilter", "adhParentPathFilter", Workbench.proposalImageColumnDirective])
-        .directive("adhPcompassProcessDetailColumn", [
-            "adhConfig", "adhPermissions", "adhTopLevelState", Workbench.processDetailColumnDirective]);
+        }]);
 };
