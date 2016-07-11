7ed9f35b4006672bcfd8ec7111e3dc96c9951547
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-07-11 17:27:09 +0200

    refactor

--- src/euth/euth/static/js/Packages/Euth/IdeaCollection/Module.ts
+++ src/euth/euth/static/js/Packages/Euth/IdeaCollection/Module.ts
@@ -13,11 +13,8 @@ import RIEuthPrivateProcess from "../../../Resources_/adhocracy_euth/resources/i
 import RIProposal from "../../../Resources_/adhocracy_core/resources/proposal/IProposal";
 import RIProposalVersion from "../../../Resources_/adhocracy_core/resources/proposal/IProposalVersion";
 
-
 export var moduleName = "adhEuthIdeaCollection";
 
-var processType1 = RIEuthProcess.content_type;
-var processType2 = RIEuthPrivateProcess.content_type;
 
 export var register = (angular) => {
     angular
@@ -31,34 +28,26 @@ export var register = (angular) => {
             adhEmbedProvider.registerContext("euth");
         }])
         .config(["adhResourceAreaProvider", "adhConfig", (adhResourceAreaProvider : AdhResourceArea.Provider, adhConfig) => {
-            var registerRoutes1 = AdhIdeaCollectionWorkbench.registerRoutesFactory(
-                RIEuthProcess, RIProposal, RIProposalVersion);
-            registerRoutes1()(adhResourceAreaProvider);
-            registerRoutes1("euth")(adhResourceAreaProvider);
-            var registerRoutes2 = AdhIdeaCollectionWorkbench.registerRoutesFactory(
-                RIEuthPrivateProcess, RIProposal, RIProposalVersion);
-            registerRoutes2()(adhResourceAreaProvider);
-            registerRoutes2("euth")(adhResourceAreaProvider);
             var processHeaderSlot = adhConfig.pkg_path + AdhIdeaCollectionWorkbench.pkgLocation + "/ProcessHeaderSlot.html";
-            adhResourceAreaProvider.processHeaderSlots[processType1] = processHeaderSlot;
-            adhResourceAreaProvider.processHeaderSlots[processType2] = processHeaderSlot;
+
+            _.forEach([RIEuthProcess, RIEuthPrivateProcess], (processType) => {
+                var registerRoutes = AdhIdeaCollectionWorkbench.registerRoutesFactory(
+                    processType, RIProposal, RIProposalVersion);
+                registerRoutes()(adhResourceAreaProvider);
+                registerRoutes("euth")(adhResourceAreaProvider);
+                adhResourceAreaProvider.processHeaderSlots[processType.content_type] = processHeaderSlot;
+            });
         }])
         .config(["adhProcessProvider", (adhProcessProvider : AdhProcess.Provider) => {
-            adhProcessProvider.templates[processType1] =
-                "<adh-idea-collection-workbench data-process-options=\"processOptions\">" +
-                "</adh-idea-collection-workbench>";
-            adhProcessProvider.processOptions[processType1] = {
-                hasImage: true,
-                proposalClass: RIProposal,
-                proposalVersionClass: RIProposalVersion
-            };
-            adhProcessProvider.templates[processType2] =
-                "<adh-idea-collection-workbench data-process-options=\"processOptions\">" +
-                "</adh-idea-collection-workbench>";
-            adhProcessProvider.processOptions[processType2] = {
-                hasImage: true,
-                proposalClass: RIProposal,
-                proposalVersionClass: RIProposalVersion
-            };
+            _.forEach([RIEuthProcess, RIEuthPrivateProcess], (processType) => {
+                adhProcessProvider.templates[processType.content_type] =
+                    "<adh-idea-collection-workbench data-process-options=\"processOptions\">" +
+                    "</adh-idea-collection-workbench>";
+                adhProcessProvider.processOptions[processType.content_type] = {
+                    hasImage: true,
+                    proposalClass: RIProposal,
+                    proposalVersionClass: RIProposalVersion
+                };
+            });
         }]);
 };
