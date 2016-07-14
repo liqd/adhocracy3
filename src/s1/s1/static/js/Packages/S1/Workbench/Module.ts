43411b4573e3e748387caaa1b14c51ee04c53d4d
Nicolas Dietrich <nidi@mailbox.org>
2016-07-14 17:17:19 +0200

    Show S1 process title and description on landing page

--- src/s1/s1/static/js/Packages/S1/Workbench/Module.ts
+++ src/s1/s1/static/js/Packages/S1/Workbench/Module.ts
@@ -1,5 +1,6 @@
 import * as AdhCommentModule from "../../Comment/Module";
 import * as AdhHttpModule from "../../Http/Module";
+import * as AdhMarkdownModule from "../../Markdown/Module";
 import * as AdhProcessModule from "../../Process/Module";
 import * as AdhResourceAreaModule from "../../ResourceArea/Module";
 import * as AdhTopLevelStateModule from "../../TopLevelState/Module";
@@ -20,6 +21,7 @@ export var register = (angular) => {
         .module(moduleName, [
             AdhCommentModule.moduleName,
             AdhHttpModule.moduleName,
+            AdhMarkdownModule.moduleName,
             AdhProcessModule.moduleName,
             AdhResourceAreaModule.moduleName,
             AdhTopLevelStateModule.moduleName
@@ -36,7 +38,7 @@ export var register = (angular) => {
             adhProcessProvider.templates[RIS1Process.content_type] = "<adh-s1-workbench></adh-s1-workbench>";
         }])
         .directive("adhS1Workbench", ["adhConfig", "adhTopLevelState", Workbench.s1WorkbenchDirective])
-        .directive("adhS1Landing", ["$translate", "adhConfig", "adhTopLevelState", Workbench.s1LandingDirective])
+        .directive("adhS1Landing", ["$translate", "adhConfig", "adhHttp", "adhTopLevelState", Workbench.s1LandingDirective])
         .directive("adhS1CurrentColumn", ["adhConfig", "adhHttp", "adhTopLevelState", Workbench.s1CurrentColumnDirective])
         .directive("adhS1NextColumn", ["adhConfig", "adhHttp", "adhTopLevelState", Workbench.s1NextColumnDirective])
         .directive("adhS1ArchiveColumn", ["adhConfig", "adhHttp", "adhTopLevelState", Workbench.s1ArchiveColumnDirective])
