8a4a953c66b0dff97b97ee1c7b3945dafc3a7369
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-07-25 11:23:30 +0200

    fix image upload in pcompass

--- src/pcompass/pcompass/static/js/Adhocracy.ts
+++ src/pcompass/pcompass/static/js/Adhocracy.ts
@@ -13,6 +13,7 @@ import * as angularCache from "angularCache";  if (angularCache) { ; };
 import * as angularTranslate from "angularTranslate";  if (angularTranslate) { ; };
 import * as angularTranslateLoader from "angularTranslateLoader";  if (angularTranslateLoader) { ; };
 import * as angularElastic from "angularElastic";  if (angularElastic) { ; };
+import * as angularFlow from "angularFlow";  if (angularFlow) { ; };
 
 import * as modernizr from "modernizr";
 import * as moment from "moment";
@@ -88,6 +89,7 @@ export var init = (config : AdhConfig.IService, metaApi) => {
         "ngAnimate",
         "ngAria",
         "ngMessages",
+        "flow",
         AdhCommentModule.moduleName,
         AdhConfigModule.moduleName,
         AdhCrossWindowMessagingModule.moduleName,
