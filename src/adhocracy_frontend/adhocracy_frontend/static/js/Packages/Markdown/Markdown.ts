574afe298caf82e7497ac4980eb3b9a655ae0f45
Martin Häcker <spamfaenger@gmx.de>
2016-08-04 17:22:51 +0200

    Establish a two way binding to a nested dict in code, so the multiple in between scopes in the template don’t interfere with the data transport.

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Markdown/Markdown.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Markdown/Markdown.ts
@@ -43,8 +43,21 @@ export var inlineEditableMarkdownDirective = (
         templateUrl: adhConfig.pkg_path + pkgLocation + "/InlineEditableMarkdown.html",
         link: (scope) => {
             scope.isEditing = false;
+            // Sorry for the hubhub with the watches here, but the template has multiple levels of child 
+            // scopes (due to nested ng-if's), so we need a way to ensure that we can get a notification back
+            // here when the value is changed deep inside. data = {} is that way.
+            // REFACT would be nice to enhance scope with $bind()...
+            scope.data = { parsetext: scope.parsetext };
             scope.$watch("parsetext", (parsetext) => {
-                scope.isEmpty = parsetext.trim().length === 0;
+                scope.isEmpty = 0 === parsetext.trim().length;
+                if (scope.data.parsetext !== parsetext) {
+                    scope.data.parsetext = parsetext;
+                }
+            });
+            scope.$watch("data.parsetext", (parsetext) => {
+                if (scope.parsetext !== parsetext) {
+                    scope.parsetext = parsetext;
+                }
             });
             scope.startEditing = () => {
                 scope.originalMarkdown = scope.parsetext;
