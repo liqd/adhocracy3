8f2cea2078ad1e969fb6f0aef25299789fa5ded2
John <john.schmid@liqd.de>
2016-09-15 17:25:49 +0200

    create RAProvider.names + listing-counter

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/ResourceArea/ResourceArea.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/ResourceArea/ResourceArea.ts
@@ -33,12 +33,14 @@ export class Provider implements angular.IServiceProvider {
         type? : string;
     }};
     public processHeaderSlots : {[processType : string]: string};
+    public names : {[resourceType : string]: string};
 
     constructor() {
         var self = this;
         this.defaults = {};
         this.specifics = {};
         this.processHeaderSlots = {};
+        this.names = {};
         this.$get = [
             "$q",
             "$injector",
@@ -287,6 +289,10 @@ export class Service implements AdhTopLevelState.IAreaInput {
                 return false;
             }
         });
+    }
+
+    public getName(resourceType : string) : string {
+        return this.provider.names[resourceType];
     }
 
     public getTemplate() : angular.IPromise<string> {
