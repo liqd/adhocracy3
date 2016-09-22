9fb3f5cd4f89138eb60506b14251868bce85b55e
John <john.schmid@liqd.de>
2016-09-22 14:46:22 +0200

    fixup: use Names.getName instead of RA.getName + Process.getName

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Process/Module.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Process/Module.ts
@@ -18,7 +18,7 @@ export var register = (angular) => {
         .directive("adhWorkflowSwitch", [
             "adhConfig", "adhHttp", "adhPermissions", "$q", "$translate", "$window", AdhProcess.workflowSwitchDirective])
         .directive("adhProcessView", ["adhTopLevelState", "adhProcess", "$compile", AdhProcess.processViewDirective])
-        .directive("adhProcessListItem", ["adhConfig", "adhHttp", "adhProcess", AdhProcess.listItemDirective])
+        .directive("adhProcessListItem", ["adhConfig", "adhHttp", "adhNames", AdhProcess.listItemDirective])
         .directive("adhProcessListing", ["adhConfig", AdhProcess.listingDirective])
         .directive("adhCurrentProcessTitle", ["adhTopLevelState", "adhHttp", AdhProcess.currentProcessTitleDirective]);
 };
