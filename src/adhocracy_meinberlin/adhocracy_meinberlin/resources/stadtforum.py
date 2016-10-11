7ad64cd959e638703054f7578301c2bead39bc83
Alexej Starschenko <alexej.starschenko@liqd.de>
2016-10-11 14:16:55 +0200

    Add embed sheet and adapter to stadtforum polls

--- src/adhocracy_meinberlin/adhocracy_meinberlin/resources/stadtforum.py
+++ src/adhocracy_meinberlin/adhocracy_meinberlin/resources/stadtforum.py
@@ -1,7 +1,13 @@
 """Stadtforum process resources."""
+from pyramid.interfaces import IRequest
+
+from adhocracy_core.interfaces import IResource
 from adhocracy_core.resources import add_resource_type_to_registry
 from adhocracy_core.resources import process
 from adhocracy_core.resources import proposal
+from adhocracy_core.sheets.embed import IEmbed
+from adhocracy_core.sheets.embed import IEmbedCodeConfig
+from adhocracy_core.sheets.embed import embed_code_config_adapter
 
 
 class IPoll(proposal.IProposal):
@@ -12,7 +18,8 @@ poll_meta = proposal.proposal_meta._replace(
     iresource=IPoll,
     default_workflow='stadtforum_poll',
     autonaming_prefix='poll_',
-)
+)._add(extended_sheets=(IEmbed,
+                        ))
 
 
 class IProcess(process.IProcess):
@@ -29,7 +36,25 @@ process_meta = process.process_meta._replace(
 )
 
 
+def embed_code_config_poll_adapter(context: IResource,
+                                   request: IRequest) -> {}:
+    """Return config to render `adhocracy_core:templates/embed_code.html`."""
+    mapping = embed_code_config_adapter(context, request)
+    version_path = mapping['path'] + 'VERSION_0000000/'
+    mapping.update({'widget': 'meinberlin-stadtforum-proposal-detail',
+                    'autoresize': 'false',
+                    'autourl': 'false',
+                    'nocenter': 'true',
+                    'style': 'height: 650px',
+                    'path': version_path
+                    })
+    return mapping
+
+
 def includeme(config):
     """Add resource type to registry."""
     add_resource_type_to_registry(process_meta, config)
     add_resource_type_to_registry(poll_meta, config)
+    config.registry.registerAdapter(embed_code_config_poll_adapter,
+                                    (IPoll, IRequest),
+                                    IEmbedCodeConfig)
