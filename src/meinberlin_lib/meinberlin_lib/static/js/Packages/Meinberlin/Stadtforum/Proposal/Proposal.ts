1ab57cc644e14ecb5ed9bbc139163680dabd997b
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-10-11 17:05:38 +0200

    implement edit

--- src/meinberlin_lib/meinberlin_lib/static/js/Packages/Meinberlin/Stadtforum/Proposal/Proposal.ts
+++ src/meinberlin_lib/meinberlin_lib/static/js/Packages/Meinberlin/Stadtforum/Proposal/Proposal.ts
@@ -121,6 +121,23 @@ var postCreate = (
     return adhHttp.deepPost([proposal, proposalVersion]);
 };
 
+var postEdit = (
+    adhHttp : AdhHttp.Service,
+    adhPreliminaryNames : AdhPreliminaryNames.Service
+) => (
+    scope : IScope,
+    oldVersion : RIProposalVersion
+) => {
+    var proposalVersion = new RIProposalVersion({preliminaryNames: adhPreliminaryNames});
+    proposalVersion.parent = AdhUtil.parentPath(oldVersion.path);
+    proposalVersion.data[SIVersionable.nick] = new SIVersionable.Sheet({
+        follows: [oldVersion.path]
+    });
+    fill(scope, proposalVersion);
+
+    return adhHttp.deepPost([proposalVersion]);
+};
+
 
 export var detailDirective = (
     adhConfig : AdhConfig.IService,
@@ -179,6 +196,49 @@ export var createDirective = (
 
             scope.cancel = () => {
                 var fallback = adhResourceUrlFilter(scope.poolPath);
+                adhTopLevelState.goToCameFrom(fallback);
+            };
+        }
+    };
+};
+
+export var editDirective = (
+    adhConfig : AdhConfig.IService,
+    adhHttp : AdhHttp.Service,
+    adhPermissions : AdhPermissions.Service,
+    adhPreliminaryNames : AdhPreliminaryNames.Service,
+    adhRate : AdhRate.Service,
+    adhResourceUrlFilter,
+    adhShowError,
+    adhSubmitIfValid,
+    adhTopLevelState : AdhTopLevelState.Service,
+    adhGetBadges : AdhBadge.IGetBadgeAssignments,
+    $location : angular.ILocationService,
+    $q : angular.IQService
+) => {
+    return {
+        restrict: "E",
+        templateUrl: adhConfig.pkg_path + pkgLocation + "/Create.html",
+        scope: {
+            path: "@"
+        },
+        link: (scope, element) => {
+            scope.errors = [];
+            scope.data = {};
+            scope.showError = adhShowError;
+            bindPath(adhHttp, adhPermissions, adhRate, adhTopLevelState, adhGetBadges, $q)(scope);
+
+            scope.submit = () => {
+                return adhSubmitIfValid(scope, element, scope.proposalForm, () => {
+                    return postEdit(adhHttp, adhPreliminaryNames)(scope, scope.resource)
+                        .then((result) => {
+                            $location.url(adhResourceUrlFilter(AdhUtil.parentPath(result[0].path)));
+                        });
+                });
+            };
+
+            scope.cancel = () => {
+                var fallback = adhResourceUrlFilter(AdhUtil.parentPath(scope.path));
                 adhTopLevelState.goToCameFrom(fallback);
             };
         }
