dd6944550dd8c38b09c0ec2635502ffe32936c89
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-10-12 19:45:53 +0200

    add empty Page Package

---
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Core/Page/Module.ts
@@ -0,0 +1,9 @@
+import * as Page from "./Page";
+
+export var moduleName = "adhPage";
+
+
+export var register = (angular) => {
+    angular.module(moduleName, [
+    ]);
+};
---
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Core/Page/Page.ts
@@ -0,0 +1,1 @@
+var pkgLocation = "/Core/Page";
