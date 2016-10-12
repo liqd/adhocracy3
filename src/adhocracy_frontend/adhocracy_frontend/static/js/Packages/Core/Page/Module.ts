73fd1775f60e78d46e2e26ea0f36fb423f6e0731
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-10-12 19:48:27 +0200

    add page directive

--- src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Core/Page/Module.ts
+++ src/adhocracy_frontend/adhocracy_frontend/static/js/Packages/Core/Page/Module.ts
@@ -1,3 +1,7 @@
+import * as AdhHttpModule from "../Http/Module";
+import * as AdhMarkdownModule from "../Markdown/Module";
+import * as AdhTopLevelStateModule from "../TopLevelState/Module";
+
 import * as Page from "./Page";
 
 export var moduleName = "adhPage";
@@ -5,5 +9,9 @@ export var moduleName = "adhPage";
 
 export var register = (angular) => {
     angular.module(moduleName, [
-    ]);
+        AdhHttpModule.moduleName,
+        AdhMarkdownModule.moduleName,
+        AdhTopLevelStateModule.moduleName,
+    ])
+    .directive("adhPage", ["adhConfig", "adhHttp", "adhTopLevelState", Page.pageDirective]);
 };
