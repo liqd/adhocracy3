dfe3933a69eed391d3746d2de39432b17e750aae
John <john.schmid@liqd.de>
2016-10-25 14:14:22 +0200

    add + manage "counter" in s1 proposal listing
    if undefined, let "counter" be true. this is counter-intuitive, I admit.
    an alternative would be to replace "counter" with "nocounter"
    everywhere.

--- src/s1/s1/static/js/Packages/S1/Proposal/Proposal.ts
+++ src/s1/s1/static/js/Packages/S1/Proposal/Proposal.ts
@@ -336,7 +336,8 @@ export var listingDirective = (
             sorts: "=?",
             state: "@?",
             decisionDate: "@?",
-            creator: "@?"
+            creator: "@?",
+            counter: "=?"
         },
         link: (scope) => {
             scope.contentType = RIProposalVersion.content_type;
@@ -355,6 +356,7 @@ export var listingDirective = (
             if (scope.decisionDate) {
                 scope.params.decision_date = scope.decisionDate;
             }
+            scope.counter = typeof scope.counter === "undefined" ? true : scope.counter;
         }
     };
 };
