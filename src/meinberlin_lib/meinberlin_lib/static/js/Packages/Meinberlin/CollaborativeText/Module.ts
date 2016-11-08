8807547d14f1c6a078568f8435f1f0a80b27bb37
John <john.schmid@liqd.de>
2016-11-08 16:40:45 +0100

    rm unused embed
    see #2840.

--- src/meinberlin_lib/meinberlin_lib/static/js/Packages/Meinberlin/CollaborativeText/Module.ts
+++ src/meinberlin_lib/meinberlin_lib/static/js/Packages/Meinberlin/CollaborativeText/Module.ts
@@ -1,7 +1,6 @@
 import * as AdhAbuseModule from "../../Core/Abuse/Module";
 import * as AdhCommentModule from "../../Core/Comment/Module";
 import * as AdhDocumentModule from "../../Core/Document/Module";
-import * as AdhEmbedModule from "../../Core/Embed/Module";
 import * as AdhHttpModule from "../../Core/Http/Module";
 import * as AdhMovingColumnsModule from "../../Core/MovingColumns/Module";
 import * as AdhPermissionsModule from "../../Core/Permissions/Module";
@@ -10,7 +9,6 @@ import * as AdhResourceAreaModule from "../../Core/ResourceArea/Module";
 import * as AdhTopLevelStateModule from "../../Core/TopLevelState/Module";
 
 import * as AdhDocument from "../../Core/Document/Document";
-import * as AdhEmbed from "../../Core/Embed/Embed";
 import * as AdhIdeaCollectionWorkbench from "../../Core/IdeaCollection/Workbench/Workbench";
 import * as AdhNames from "../../Core/Names/Names";
 import * as AdhProcess from "../../Core/Process/Process";
@@ -30,7 +28,6 @@ export var register = (angular) => {
             AdhAbuseModule.moduleName,
             AdhCommentModule.moduleName,
             AdhDocumentModule.moduleName,
-            AdhEmbedModule.moduleName,
             AdhHttpModule.moduleName,
             AdhMovingColumnsModule.moduleName,
             AdhPermissionsModule.moduleName,
@@ -38,12 +35,6 @@ export var register = (angular) => {
             AdhResourceAreaModule.moduleName,
             AdhTopLevelStateModule.moduleName
         ])
-        // the (document-focused) IdeaCollection is also registered under the directive name
-        // "adhDebateWorkbench" such as not to break currently running embeds.
-        .config(["adhEmbedProvider", (adhEmbedProvider : AdhEmbed.Provider) => {
-            adhEmbedProvider
-                .registerDirective("debate-workbench");
-        }])
         .config(["adhResourceAreaProvider", "adhConfig", (adhResourceAreaProvider: AdhResourceArea.Provider, adhConfig) => {
             var registerRoutes = (context? : string) => (provider) => {
                 AdhIdeaCollectionWorkbench.registerCommonRoutesFactory(
